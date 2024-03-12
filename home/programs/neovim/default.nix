3bb90c6dfa61a9eeb7bf273cdd60051902c46a8d
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-12 01:10:54 +0100

    Add comment on symlinking

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -15,6 +15,7 @@
     withPython3 = true;
   };
   # home.file.".config/nvim" = { source = ./.; };
+  # This is one way to create symlinks outside of the nix store.
   systemd.user.tmpfiles.rules = [
     "L ${config.home.homeDirectory}/.config/nvim - - - - ${config.home.homeDirectory}/projects/dotfiles/home/programs/neovim"
   ];
