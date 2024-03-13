802e08ad38d1a38f598574cb7cee28fd48e9d9de
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-13 12:32:19 +0100

    Set LD_LIBRARY_PATH

--- machines/home-jarle-wsl/default.nix
+++ machines/home-jarle-wsl/default.nix
@@ -3,4 +3,7 @@ if hostname != "Jarle" then
   { }
 else {
   home.packages = with pkgs; [ xdg-utils ];
+  home.sessionVariables = {
+    LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu";
+  };
 }
