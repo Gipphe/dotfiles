2334882fa53c15204ca1871336c84ca25e3512d9
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-20 22:10:16 +0100

    fix: add bootloader to nixos-vm

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -2,6 +2,11 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
   hardware.opengl = {
     driSupport = true;
     driSupport32Bit = true;
