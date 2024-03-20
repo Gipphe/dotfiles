34d718da77d93598b1be4240ee244045d7cefb56
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-20 16:41:25 +0100

    chore: disable workspace switch bindings

--- home/rice/hyprland/binds.nix
+++ home/rice/hyprland/binds.nix
@@ -4,24 +4,26 @@ let
   modshift = "${mod}SHIFT";
   mouse_lmb = 272;
   mouse_rmb = 273;
-  # binds $mod + [shift +] {1..10} to [move to] workspace {1..10} (stolen from fufie) (re-stolen from sioodmy)
-  workspaces = builtins.concatLists (
-    builtins.genList (
-      x:
-      let
-        ws =
-          let
-            c = (x + 1) / 10;
-          in
-          builtins.toString (x + 1 - (c * 10));
-      in
-      [
-        "${mod}, ${ws}, workspace ${toString (x + 1)}"
-        "${mod} SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-      ]
-    ) 10
-  );
 in
+# binds $mod + [shift +] {1..10} to [move to] workspace {1..10} (stolen from fufie) (re-stolen from sioodmy)
+# workspaces =
+#   with builtins;
+#   concatLists (
+#     genList (
+#       x:
+#       let
+#         ws =
+#           let
+#             c = (x + 1) / 10;
+#           in
+#           toString (x + 1 - (c * 10));
+#       in
+#       [
+#         "${mod}, ${ws}, workspace ${toString (x + 1)}"
+#         "${mod} SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+#       ]
+#     ) 10
+#   );
 {
   wayland.windowManager.hyprland.settings = {
     bind = [
@@ -57,7 +59,7 @@ in
       "${mod},Period,exec, tofi-emoji"
 
       "${modshift},L,exec,hyprlock"
-    ] ++ workspaces;
+    ]; # ++ workspaces;
 
     bindm = [
       "${mod},mouse:${mouse_lmb},movewindow"
