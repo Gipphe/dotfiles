705a6617d3767732b0b2186cef8aa01e0a3d775e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-20 16:12:34 +0100

    fix: misplaced systemd settings

--- system/core/system.nix
+++ system/core/system.nix
@@ -107,15 +107,15 @@
         "autovt@tty1".enable = false;
         "getty@tty7".enable = false;
         "autovt@tty7".enable = false;
-
-        # Systemd OOMd
-        # Fedora enables these options by deafult. See the 10-oomd-* files here:
-        # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
-        oomd.enableRootSlice = true;
-
-        # TODO: channels-to-flakes
-        tmpfiles.rules = [ "D /nix/var/nix/profiles/per-user/root 755 root root - -" ];
       };
+
+      # Systemd OOMd
+      # Fedora enables these options by deafult. See the 10-oomd-* files here:
+      # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
+      oomd.enableRootSlice = true;
+
+      # TODO: channels-to-flakes
+      tmpfiles.rules = [ "D /nix/var/nix/profiles/per-user/root 755 root root - -" ];
     };
 
   programs = {
