1594a01db99b6d05093ced97c7cb9e5c28f114c0
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-21 23:17:27 +0100

    move config from nixos-vm/configuration.nix to nixos-vm/default.nix

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -5,6 +5,29 @@
     ./hardware-configuration.nix
   ];
 
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.x11 = true;
+
+  services.xserver = {
+    virtualScreen = { x = 1920; y = 940; };
+    monitorSection = ''
+      Modeline "1920x940_60.00"  149.45  1920 2032 2240 2560  940 941 944 973  -HSync +Vsync  
+    '';
+    deviceSection = ''
+      Option "ModeValidation"
+    '';
+  };
+
+  services.openssh.enable = true;
+
   # hardware.opengl = {
   #   driSupport = true;
   #   driSupport32Bit = true;
