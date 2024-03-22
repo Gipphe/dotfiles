4d409caa6f10b7e7c00ad298af8d40b73ed1de2e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-22 10:40:32 +0100

    merge configuration and default nixos-vm configs

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -1,29 +1,111 @@
 { pkgs, lib, ... }:
 {
   imports = [
-    ./configuration.nix
     ./hardware-configuration.nix
   ];
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
 
+  # Enable flakes and nix-command features to use Nix in the modern age
+  nix.settings.extra-experimental-features = [
+    "flakes"
+    "nix-command"
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.groups.gipphe = { };
+  users.users.gipphe = {
+    isNormalUser = true;
+    description = "Victor Nascimento Bakke";
+    home = lib.mkDefault "/home/gipphe";
+    group = "gipphe";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      firefox
+      kate
+      #  thunderbird
+    ];
+  };
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
+  # Enable VirtualBox additions to make shared clipboard and other niceties work
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.x11 = true;
 
   services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+
+    # Enable automatic login for the user.
+    displayManager.autoLogin.enable = true;
+    displayManager.autoLogin.user = "gipphe";
+
+    # Configure keymap in X11
+    layout = "no";
+    xkbVariant = "";
+
+    # Attempt to set the screen resolution
     virtualScreen = { x = 1920; y = 940; };
+
+    # Add custom screen resolution that works nicely in VirtualBox
     monitorSection = ''
       Modeline "1920x940_60.00"  149.45  1920 2032 2240 2560  940 941 944 973  -HSync +Vsync  
     '';
     deviceSection = ''
       Option "ModeValidation"
     '';
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   services.openssh.enable = true;
@@ -72,4 +154,31 @@
     #   path = "/home/gipphe/.ssh/codeberg.ssh";
     # } // user;
   #};
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
