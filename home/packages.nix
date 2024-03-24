cc3da0a4e60e1efeb4f87e68ede468e2b6b960ae
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-24 16:58:11 +0100

    Add various programs

--- home/packages.nix
+++ home/packages.nix
@@ -2,10 +2,30 @@
 {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
+
+    # Programs
     _1password-gui
     ledger-live-desktop
     ledger_agent
-    pulseaudio
+    discord
+    cool-retro-term
+    lutris
+    betterdiscord-installer
+
+    # Utils
     dconf
+    git
+    catimg
+    curl
+    xflux
+
+    # Misc
+    cava
+    neofetch
+    mpc-cli
+    gnome.nautilus
+    gnome.zenity
+    gnome.gnome-tweaks
+    gnome.eog
   ];
 }
