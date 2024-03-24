44c2d88a2310ce92845c9396e8281ca2aa0b975e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-03-24 20:02:03 +0100

    Remove unused module

--- nixos/secrets.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, lib, ...}: let
-  host = ["nixos-vm"];
-  key = ["gitlab.ssh", "github.ssh", "codeberg.ssh"];
-  inherit (lib.attrsets) genAttrs cartesianProductOfSets;
-  secretNames = builtins.map (x: "${x.host}-${x.key}") (cartesianProductOfSets { inherit host key; });
-  secrets = genAttrs (sn: { file = ../secrets/${sn}.age; owner = "gipphe"; group = "gipphe"; }) secretNames;
-in {
-  age.secrets = secrets;
-}
