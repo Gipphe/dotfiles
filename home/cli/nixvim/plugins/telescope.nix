f5c55d868ea9a20e675c4bd72169848242ffeda5
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-04 14:16:38 +0200

    Add remaining telescope keymaps

--- home/cli/nixvim/plugins/telescope.nix
+++ home/cli/nixvim/plugins/telescope.nix
@@ -25,65 +25,86 @@ in
       };
     };
     extraConfigLuaPre = ''
-      function get_cwd()
+      function get_current_dir()
         local cwd = vim.fn.expand('%:p:h:~:.')
         return cwd:gsub("^oil://", "")
       end
     '';
     keymaps = [
       (k "n" "<leader>," "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>" {
-        desc = "Switch Buffer";
+        desc = "Switch buffer";
       })
-      # (kv "n" "<leader>/" "LazyVim.telescope('live_grep')" { desc = "Grep (Root Dir)"; })
-      (k "n" "<leader>:" "<cmd>Telescope command_history<cr>" { desc = "Command History"; })
-      # (kv "n" "<leader><space>" "LazyVim.telescope('files')" { desc = "Find Files (Root Dir)"; })
+      (kv "n" "<leader>/"
+        "function() require('telescope.builtin').live_grep({ cwd = get_current_dir() }) end"
+        { desc = "Grep (current dir)"; }
+      )
+      (k "n" "<leader>:" "<cmd>Telescope command_history<cr>" { desc = "Command history"; })
+      (k "n" "<leader><space>" "<cmd>Telescope find_files<cr>" { desc = "Find files (cwd)"; })
 
       # find
       (k "n" "<leader>fb" "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>" {
         desc = "Buffers";
       })
-      # (kv "n" "<leader>fc" "LazyVim.telescope.config_files()" { desc = "Find Config File"; })
-      # (kv "n" "<leader>ff" "LazyVim.telescope('files')" { desc = "Find Files (Root Dir)"; })
-      # (kv "n" "<leader>fF" "LazyVim.telescope('files' { cwd = false })" { desc = "Find Files (cwd)"; })
-      (k "n" "<leader>fg" "<cmd>Telescope git_files<cr>" { desc = "Find Files (git-files)"; })
-      (k "n" "<leader>fr" "<cmd>Telescope oldfiles<cr>" { desc = "Recent"; })
-      (kv "n" "<leader>fR" "function() require('telescope').oldfiles({ cwd = get_cwd() }) end" {
-        desc = "Recent (cwd)";
-      })
-      # (kv "n" "<leader>fR" LazyVim.telescope("oldfiles" { cwd = vim.uv.cwd() }) { desc = "Recent (cwd)"; })
+      (kv "n" "<leader>ff" ''
+        function()
+          require('telescope.builtin').files({ cwd = get_current_dir() })
+        end
+      '' { desc = "Find files (current dir)"; })
+      (k "n" "<leader>fF" "<cmd>Telescope find_files<cr>" { desc = "Find files (cwd)"; })
+      (k "n" "<leader>fg" "<cmd>Telescope git_files<cr>" { desc = "Find files (git-files)"; })
+      (kv "n" "<leader>fr" ''
+        function()
+          require('telescope.builtin').oldfiles({ cwd = get_current_dir() }) 
+        end
+      '' { desc = "Recent (current dir)"; })
+      (k "n" "<leader>fR" "<cmd>Telescope oldfiles<cr>" { desc = "Recent (cwd)"; })
 
       # git
-      (k "n" "<leader>gc" "<cmd>Telescope git_commits<CR>" { desc = "Commits"; })
-      (k "n" "<leader>gs" "<cmd>Telescope git_status<CR>" { desc = "Status"; })
+      (k "n" "<leader>gc" "<cmd>Telescope git_commits<cr>" { desc = "Commits"; })
+      (k "n" "<leader>gs" "<cmd>Telescope git_status<cr>" { desc = "Status"; })
 
       # search
       (k "n" "<leader>s\"" "<cmd>Telescope registers<cr>" { desc = "Registers"; })
-      (k "n" "<leader>sa" "<cmd>Telescope autocommands<cr>" { desc = "Auto Commands"; })
+      (k "n" "<leader>sa" "<cmd>Telescope autocommands<cr>" { desc = "Auto commands"; })
       (k "n" "<leader>sb" "<cmd>Telescope current_buffer_fuzzy_find<cr>" { desc = "Buffer"; })
-      (k "n" "<leader>sc" "<cmd>Telescope command_history<cr>" { desc = "Command History"; })
+      (k "n" "<leader>sc" "<cmd>Telescope command_history<cr>" { desc = "Command history"; })
       (k "n" "<leader>sC" "<cmd>Telescope commands<cr>" { desc = "Commands"; })
-      (k "n" "<leader>sd" "<cmd>Telescope diagnostics bufnr=0<cr>" { desc = "Document Diagnostics"; })
-      (k "n" "<leader>sD" "<cmd>Telescope diagnostics<cr>" { desc = "Workspace Diagnostics"; })
+      (k "n" "<leader>sd" "<cmd>Telescope diagnostics bufnr=0<cr>" { desc = "Document diagnostics"; })
+      (k "n" "<leader>sD" "<cmd>Telescope diagnostics<cr>" { desc = "Workspace diagnostics"; })
       (kv "n" "<leader>sg" ''
         function()
-          require('telescope.builtin').live_grep({ cwd = get_cwd() })
+          require('telescope.builtin').live_grep({ cwd = get_current_dir() })
         end
       '' { desc = "Grep (current dir)"; })
-      # (k "n" "<leader>sg" LazyVim.telescope("live_grep") {desc = "Grep (Root Dir)"; })
       (k "n" "<leader>sG" "<cmd>Telescope live_grep<cr>" { desc = "Grep (cwd)"; })
-      # (k "n" "<leader>sG", LazyVim.telescope("live_grep" { cwd = false }) {desc = "Grep (cwd)"; })
-      (k "n" "<leader>sh" "<cmd>Telescope help_tags<cr>" { desc = "Help Pages"; })
-      (k "n" "<leader>sH" "<cmd>Telescope highlights<cr>" { desc = "Search Highlight Groups"; })
-      (k "n" "<leader>sk" "<cmd>Telescope keymaps<cr>" { desc = "Key Maps"; })
-      (k "n" "<leader>sM" "<cmd>Telescope man_pages<cr>" { desc = "Man Pages"; })
-      (k "n" "<leader>sm" "<cmd>Telescope marks<cr>" { desc = "Jump to Mark"; })
+      (k "n" "<leader>sh" "<cmd>Telescope help_tags<cr>" { desc = "Help pages"; })
+      (k "n" "<leader>sH" "<cmd>Telescope highlights<cr>" { desc = "Search highlight groups"; })
+      (k "n" "<leader>sk" "<cmd>Telescope keymaps<cr>" { desc = "Key maps"; })
+      (k "n" "<leader>sM" "<cmd>Telescope man_pages<cr>" { desc = "Man pages"; })
+      (k "n" "<leader>sm" "<cmd>Telescope marks<cr>" { desc = "Jump to mark"; })
       (k "n" "<leader>so" "<cmd>Telescope vim_options<cr>" { desc = "Options"; })
       (k "n" "<leader>sR" "<cmd>Telescope resume<cr>" { desc = "Resume"; })
-      # (k "n" "<leader>sw" ''LazyVim.telescope("grep_string" { word_match = "-w" })'' {desc = "Word (Root Dir)"; })
-      # (k "n" "<leader>sW" ''LazyVim.telescope("grep_string" { cwd = false, word_match = "-w" })'' {desc = "Word (cwd)"; })
-      # (k "v" "<leader>sw" ''LazyVim.telescope("grep_string")'' {desc = "Selection (Root Dir)"; })
-      # (k "v" "<leader>sW" ''LazyVim.telescope("grep_string" { cwd = false })'' {desc = "Selection (cwd)"; })
-      # (k "n" "<leader>uC" ''LazyVim.telescope("colorscheme" { enable_preview = true })'' {desc = "Colorscheme with Preview"; })
+      (kv "n" "<leader>sw" ''
+        function()
+          require('telescope.builtin').grep_string({ word_match = '-w', cwd = get_durrent_dir() })
+        end
+      '' { desc = "Word (current dir)"; })
+      (kv "n" "<leader>sW" ''
+        function()
+          require('telescope.builtin').grep_string({ word_match = '-w' })
+        end
+      '' { desc = "Word (cwd)"; })
+      (kv "v" "<leader>sw" ''
+        function()
+          require('telescope.builtin').grep_string({ cwd = get_current_dir() })
+        end
+      '' { desc = "Selection (current dir)"; })
+      (k "v" "<leader>sW" "<cmd>Telescope grep_string<cr>" { desc = "Selection (cwd)"; })
+      (kv "n" "<leader>uC" ''
+        function()
+          require('telescope.builtin').colorscheme({ enable_preview = true })
+        end
+      '' { desc = "Colorscheme with Preview"; })
     ];
   };
 }
