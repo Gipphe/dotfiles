1b84e8615cafe246e75a3d3853e2ed4430783b33
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-04 09:24:43 +0200

    Add vim-illuminate

---
+++ home/cli/nixvim/plugins/vim-illuminate.nix
@@ -0,0 +1,34 @@
+{ config, ... }:
+let
+  inherit (import ../util.nix) kv;
+  inherit (config.nixvim) helpers;
+  km = np: ''function() require('illuminate').goto_${np}_reference(false) end'';
+in
+{
+  programs.nixvim = {
+    plugins.illuminate = {
+      delay = 200;
+      largeFileCutoff = 2000;
+      largeFileOverrides = {
+        providers = [ "lsp" ];
+      };
+    };
+    keymaps = [
+      (kv "n" "]]" (km "next") { desc = "Next reference"; })
+      (kv "n" "[[" (km "prev") { desc = "Prev reference"; })
+    ];
+
+    autoCmd = [
+      {
+        event = "FileType";
+        callback = helpers.mkRaw ''
+          function()
+            local buffer = vim.api.nvim_get_current_buf()
+            vim.keymap.set("n", "]]", ${km "next"}, { desc = "Next reference"; buffer = buffer })
+            vim.keymap.set("n", "[[", ${km "prev"}, { desc = "Prev reference"; buffer = buffer })
+          end
+        '';
+      }
+    ];
+  };
+}
