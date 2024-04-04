7b6bed489622134e9761c656a6c23ffb4347b7c4
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-04 10:41:00 +0200

    Reapply "Add miniindentscope autocommand"
    This reverts commit e2a7213473ec73666d4135dfb719518729bb38af.

--- home/cli/nixvim/plugins/mini.nix
+++ home/cli/nixvim/plugins/mini.nix
@@ -62,5 +62,24 @@ in
         };
       };
     };
+    autoCmd = [
+      {
+        event = "FileType";
+        pattern = [
+          "help"
+          "alpha"
+          "dashboard"
+          "neo-tree"
+          "Trouble"
+          "trouble"
+          "lazy"
+          "mason"
+          "notify"
+          "toggleterm"
+          "lazyterm"
+        ];
+        callback = ''function() vim.b.miniindentscope_disable = true end'';
+      }
+    ];
   };
 }
