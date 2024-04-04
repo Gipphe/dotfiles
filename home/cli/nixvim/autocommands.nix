41f2303c1977239a550e9bf2908dacb94e6a52e5
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-04 09:35:06 +0200

    Add highlight on yank autocmd

--- home/cli/nixvim/autocommands.nix
+++ home/cli/nixvim/autocommands.nix
@@ -1,9 +1,13 @@
-{ config, ...}: 
-let inherit (config.nixvim) helpers;
+{ config, ... }:
+let
+  inherit (config.nixvim) helpers;
 in
 {
   programs.nixvim = {
     autoGroups = {
+      highlight_yank = {
+        clear = true;
+      };
       nvim-metals = {
         clear = true;
       };
@@ -26,6 +30,17 @@ in
             }
             config.init_options.statusBarProvider = "on"
             metals.initialize_or_attach(config)
+          end
+        '';
+      }
+
+      # Highlight on yank
+      {
+        event = "TextYankPost";
+        group = "highlight_yank";
+        callback = helpers.mkRaw ''
+          function()
+            vim.hightlight.on_yank()
           end
         '';
       }
