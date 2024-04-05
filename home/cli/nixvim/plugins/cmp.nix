8b11a4b0c5368a37d541437528564eba78253c85
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-05 08:30:03 +0200

    Fix highlight on cmp ghost text

--- home/cli/nixvim/plugins/cmp.nix
+++ home/cli/nixvim/plugins/cmp.nix
@@ -3,43 +3,59 @@ let
   inherit (config.nixvim) helpers;
 in
 {
-  programs.nixvim.plugins = {
-    cmp-nvim-lsp.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp = {
-      enable = true;
-      settings = {
-        completion = {
-          completeopt = "menu,menuone,noinsert";
+  programs.nixvim = {
+    plugins = {
+      cmp-nvim-lsp.enable = true;
+      cmp-buffer.enable = true;
+      cmp-path.enable = true;
+      cmp = {
+        enable = true;
+        settings = {
+          completion = {
+            completeopt = "menu,menuone,noinsert";
+          };
+          mapping = helpers.mkRaw ''
+            cmp.mapping.preset.insert({
+                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<S-CR>"] = cmp.mapping.confirm({
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = true,
+                }),
+                ["<C-CR>"] = function(fallback)
+                  cmp.abort()
+                  fallback()
+                end,
+              })
+          '';
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
+          # formatting.format = ''
+          #   function(_, item)
+          #     if icons.kinds[item.kind] then
+          #       item.kind = icons.kinds[item.kind] .. item.kind
+          #     end
+          #     return item
+          #   end
+          # '';
+          experimental.ghost_text.hl_group = "CmpGhostText";
         };
-        mapping = helpers.mkRaw ''
-          cmp.mapping.preset.insert({
-              ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-              ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-              ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-              ["<C-f>"] = cmp.mapping.scroll_docs(4),
-              ["<C-Space>"] = cmp.mapping.complete(),
-              ["<C-e>"] = cmp.mapping.abort(),
-              -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-              ["<CR>"] = cmp.mapping.confirm({ select = true }),
-              -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-              ["<S-CR>"] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
-              }),
-              ["<C-CR>"] = function(fallback)
-                cmp.abort()
-                fallback()
-              end,
-            })
-        '';
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
-        experimental.ghost_text.hl_group = "CmpGhostText";
+      };
+    };
+    highlight = {
+      CmpGhostText = {
+        link = "Comment";
+        default = true;
       };
     };
   };
