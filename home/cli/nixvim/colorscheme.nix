f7989e99a76a4c824b845e8620c5c62d994012a2
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-05 09:17:06 +0200

    Enable catppuccin integrations

--- home/cli/nixvim/colorscheme.nix
+++ home/cli/nixvim/colorscheme.nix
@@ -1,10 +1,31 @@
-{...}: {
+{ ... }:
+{
   programs.nixvim = {
     colorscheme = "catppuccin";
     colorschemes.catppuccin = {
       enable = true;
       flavour = "mocha";
       showBufferEnd = true;
+      integrations = {
+        illuminate.enabled = true;
+        indent_blankline = {
+          enabled = true;
+          colored_indent_levels = true;
+        };
+        mini.enabled = true;
+        navic.enabled = true;
+        telescope.enabled = true;
+        native_lsp.enabled = true;
+        NormalNvim = true;
+        cmp = true;
+        flash = true;
+        gitsigns = true;
+        noice = true;
+        notify = true;
+        treesitter = true;
+        treesitter_context = true;
+        which_key = true;
+      };
     };
   };
 }
