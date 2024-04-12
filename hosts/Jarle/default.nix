ab662b40d9e5894214f453cb4be273125ef661b7
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-12 07:27:18 +0000

    Fix docker integration for NixOS-WSL

--- hosts/Jarle/default.nix
+++ hosts/Jarle/default.nix
@@ -5,7 +5,13 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ inputs, config, lib, pkgs, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   imports = [
@@ -18,14 +24,33 @@
   wsl = {
     enable = true;
     defaultUser = "gipphe";
-    docker-desktop.enable = true;
+    docker-desktop.enable = false;
     interop = {
       includePath = true;
       # register = true;
       # startMenuLaunchers = true;
     };
+    extraBin = with pkgs; [
+      # Binaries for Docker Desktop wsl-distro-proxy
+      { src = "${coreutils}/bin/mkdir"; }
+      { src = "${coreutils}/bin/cat"; }
+      { src = "${coreutils}/bin/whoami"; }
+      { src = "${coreutils}/bin/ls"; }
+      { src = "${busybox}/bin/addgroup"; }
+      { src = "${su}/bin/groupadd"; }
+      { src = "${su}/bin/usermod"; }
+    ];
   };
-  
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+
+  systemd.services.docker-desktop-proxy.script = lib.mkForce ''
+    ${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"
+  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
