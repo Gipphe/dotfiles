5d2563cb062f65e36ab6a57f75ff5c3b7118d046
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-17 11:06:03 +0000

    Fix nvim-metals plugin

--- home/cli/nixvim/plugins/metals.nix
+++ home/cli/nixvim/plugins/metals.nix
@@ -9,51 +9,50 @@ in
       plenary-nvim
       nvim-metals
     ];
-    extraConfigLua = ''
-      local setup_nvim_metals = function()
-        local metals = require('metals')
-        local config = metals.bare_config()
-        local gradleScript = os.getenv('GRADLE_SCRIPT')
-        config.find_root_dir_max_project_nesting = 3
-        config.settings = {
-          showImplicitArguments = true,
-          excludedPackages = { "akka.actor.typed.javads1", "com.github.swagger.akka.javads1" },
-          metalsBinaryPath = "${pkgs.metals}/bin/metals",
-        }
-        if (gradleScript ~= nil) then
-          config.settings.gradleScript = gradleScript
-        end
-
-        config.init_options.statusBarProvider = 'on'
-        config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-        metals.setup(config)
-      end
-    '';
     autoGroups = {
-      nvim-metals = {
+      metals = {
         clear = true;
       };
     };
     autoCmd = [
       {
         event = "FileType";
-        group = "nvim-metals";
         pattern = [
           "scala"
           "sbt"
           "java"
         ];
-        callback = helpers.mkRaw ''
-          function()
-            local metals = require('metals')
-            local config = metals.bare_config()
-            config.settings = {
-              showImplicitArguments = true,
-            }
-            config.init_options.statusBarProvider = "on"
-            metals.initialize_or_attach(config)
-          end
-        '';
+        group = "metals";
+        callback =
+          let
+            settings = {
+              showImplicitArguments = true;
+              excludedPackages = [
+                "akka.actor.typed.javads1"
+                "com.github.swagger.akka.javads1"
+              ];
+              metalsBinaryPath = "${pkgs.metals}/bin/metals";
+            };
+          in
+          helpers.mkRaw ''
+            function()
+              local metals = require('metals')
+              local config = metals.bare_config()
+
+              config.find_root_dir_max_project_nesting = 3
+              config.settings = ${helpers.toLuaObject settings}
+
+              local gradleScript = os.getenv('GRADLE_SCRIPT')
+              if (gradleScript ~= nil) then
+                config.settings.gradleScript = gradleScript
+              end
+
+              config.init_options.statusBarProvider = 'on'
+              config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+              metals.initialize_or_attach(config)
+            end
+          '';
       }
     ];
   };
