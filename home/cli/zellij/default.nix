c635d59a7f0324344a68fa5cdb61955e561cb79e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-22 11:04:16 +0000

    Tweak zellij layout

--- home/cli/zellij/default.nix
+++ home/cli/zellij/default.nix
@@ -13,9 +13,15 @@ in
     enable = lib.mkEnableOption "zellij";
   };
   config = lib.mkIf cfg.enable {
-    home.file."zellij-plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
-      url = "https://github.com/karimould/zellij-forgot/releases/download/0.3.0/zellij_forgot.wasm";
-      hash = "sha256-JNQ4KXb6VzjSF0O4J8Tvq3FXUYBBabQb9ZitcR3kZFw=";
+    home.file = {
+      "zellij-plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
+        url = "https://github.com/karimould/zellij-forgot/releases/download/0.3.0/zellij_forgot.wasm";
+        hash = "sha256-JNQ4KXb6VzjSF0O4J8Tvq3FXUYBBabQb9ZitcR3kZFw=";
+      };
+      "zellij-plugins/zjstatus.wasm".source = pkgs.fetchurl {
+        url = "https://github.com/dj95/zjstatus/releases/download/v0.14.0/zjstatus.wasm";
+        hash = "sha256-TlPnvSz1ROusMXP0CeQJcugR0RdiKTqwMxpUDiP0SRU=";
+      };
     };
     xdg.configFile = {
       "zellij/layouts".source = ./layouts;
