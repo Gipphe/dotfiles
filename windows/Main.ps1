56ea17c635d0de04c57f5b8802d4e2a483bbaf9f
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-30 22:09:41 +0000

    debug: windows

--- windows/Main.ps1
+++ windows/Main.ps1
@@ -1,31 +1,31 @@
 #Requires -Version 7.3
 
-param()
+param ()
 
 $ErrorActionPreference = "Stop"
 
 $Mod = "$PSScriptRoot/Modules"
 Import-Module $Mod/Config.psm1
-Import-Module $Mod/Games.psm1
-Import-Module $Mod/Programs.psm1
-Import-Module $Mod/Registry.psm1
-Import-Module $Mod/SD.psm1
-Import-Module $Mod/WSL.psm1
+# Import-Module $Mod/Games.psm1
+# Import-Module $Mod/Programs.psm1
+# Import-Module $Mod/Registry.psm1
+# Import-Module $Mod/SD.psm1
+# Import-Module $Mod/WSL.psm1
 
 function Initialize-Main {
   $Config = New-Config
-  $Games = New-Games
-  $Programs = New-Programs
-  $Registry = New-Registry
-  $SD = New-SD
-  $WSL = New-WSL
+  # $Games = New-Games
+  # $Programs = New-Programs
+  # $Registry = New-Registry
+  # $SD = New-SD
+  # $WSL = New-WSL
 
-  $Programs.Install()
   $Config.Install()
-  $Registry.SetEntries()
-  $Games.InstallFS22Mods()
-  $SD.Install()
-  $WSL.Install()
+  # $Games.InstallFS22Mods()
+  # $Programs.Install()
+  # $Registry.SetEntries()
+  # $SD.Install()
+  # $WSL.Install()
 }
 
 Initialize-Main
