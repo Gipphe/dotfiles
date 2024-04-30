b0e5c45cbbe2c68f9f36c3fd31675d88aa724b7b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-30 23:28:20 +0000

    debug: windows

--- windows/Modules/SD.psm1
+++ windows/Modules/SD.psm1
@@ -15,7 +15,7 @@ class SD {
   [Void] Install() {
     $SDDir = "$($this.Dirname)\_temp"
     Remove-Item -Force -Recurse -ErrorAction 'SilentlyContinue' -Path $SDDir
-    Invoke-Native { git clone https://codeberg.org/Gipphe/sd.git $SDDir } 
+    Invoke-Native { git clone "https://codeberg.org/Gipphe/sd.git" "$SDDir" }
     . "$SDDir\sd.ps1" 
     Remove-Item -Force -Recurse -ErrorAction 'SilentlyContinue' -Path $SDDir
   }
