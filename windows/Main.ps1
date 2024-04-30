1b542dc7ac6886d564cb00e06a00bb7bbeb48a0a
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-30 22:46:41 +0000

    debug: windows

--- windows/Main.ps1
+++ windows/Main.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 
 $Mod = "$PSScriptRoot/Modules"
 Import-Module $Mod/Config.psm1
-# Import-Module $Mod/Games.psm1
+Import-Module $Mod/Games.psm1
 # Import-Module $Mod/Programs.psm1
 # Import-Module $Mod/Registry.psm1
 # Import-Module $Mod/SD.psm1
@@ -14,14 +14,14 @@ Import-Module $Mod/Config.psm1
 
 function Initialize-Main {
   $Config = New-Config
-  # $Games = New-Games
+  $Games = New-Games
   # $Programs = New-Programs
   # $Registry = New-Registry
   # $SD = New-SD
   # $WSL = New-WSL
 
   $Config.Install()
-  # $Games.InstallFS22Mods()
+  $Games.InstallFS22Mods()
   # $Programs.Install()
   # $Registry.SetEntries()
   # $SD.Install()
