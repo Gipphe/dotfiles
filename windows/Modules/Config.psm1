b09470f4c90244a8720947755c3cdfc83b02c839
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-30 22:21:22 +0000

    debug: windows

--- windows/Modules/Config.psm1
+++ windows/Modules/Config.psm1
@@ -3,18 +3,17 @@
 $ErrorActionPreference = "Stop"
 
 class Config {
-  [String]$Dirname
+  [String]$CfgDir
 
   Config([String]$Dirname) {
-    $this.Dirname = "$Dirname/../Config"
+    $this.CfgDir = "$Dirname/../Config"
   }
 
   # [Void] Install() {
-  #   $cfgdir = "$($this.Dirname)\config"
   #   $Items = @(
-  #     @("$cfgDir\.vimrc", "$HOME\.vimrc"),
-  #     @("$cfgDir\.wezterm.lua", "$HOME\.wezterm.lua"),
-  #     @("$cfgDir\wsl.conf", "$HOME\wsl.conf")
+  #     @("$($this.cfgDir)\.vimrc", "$HOME\.vimrc"),
+  #     @("$($this.cfgDir)\.wezterm.lua", "$HOME\.wezterm.lua"),
+  #     @("$($this.cfgDir)\wsl.conf", "$HOME\wsl.conf")
   #   )
   #
   #   $Items | ForEach-Object { Copy-Item -Path $_[0] -Destination $_[1] }
