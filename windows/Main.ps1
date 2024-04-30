6fcb8ca1839e98ebfdd4c60f9f8843cfa165dc3e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-30 23:24:16 +0000

    debug: windows

--- windows/Main.ps1
+++ windows/Main.ps1
@@ -9,7 +9,7 @@ $Mod = "$PSScriptRoot/Modules"
 Import-Module $Mod/Config.psm1
 Import-Module $Mod/Games.psm1
 Import-Module $Mod/Programs.psm1
-# Import-Module $Mod/Registry.psm1
+Import-Module $Mod/Registry.psm1
 # Import-Module $Mod/SD.psm1
 # Import-Module $Mod/WSL.psm1
 
@@ -17,14 +17,14 @@ function Initialize-Main {
   $Config = New-Config
   $Games = New-Games
   $Programs = New-Programs
-  # $Registry = New-Registry
+  $Registry = New-Registry
   # $SD = New-SD
   # $WSL = New-WSL
 
   $Config.Install()
   $Games.InstallFS22Mods()
   $Programs.Install()
-  # $Registry.SetEntries()
+  $Registry.SetEntries()
   # $SD.Install()
   # $WSL.Install()
 }
