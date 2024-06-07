e2cf5ef3a97f447d8385e0cab48e15f24dcb9d24
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-07 10:44:44 +0200

    refactor: move some gui apps to desktop

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,1 +1,30 @@
-{ lib, flags, ... }: lib.optionalAttrs flags.desktop { imports = [ ./hyprland ]; }
+{
+  lib,
+  flags,
+  pkgs,
+  ...
+}:
+lib.optionalAttrs flags.desktop {
+  imports = [
+    ./hyprland
+    ./gaming.nix
+    ./filen.nix
+    ./term.nix
+  ];
+  home.packages = with pkgs; [
+    _1password-gui
+    ledger-live-desktop
+    ledger_agent
+    discord
+    cool-retro-term
+    betterdiscord-installer
+    slack
+    vivaldi
+
+    xflux
+    gnome.nautilus
+    gnome.zenity
+    gnome.gnome-tweaks
+    gnome.eog
+  ];
+}
