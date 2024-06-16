fc0106623747de007e3a3f108d58942afbb1546b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-16 14:46:39 +0200

    fix: IDEA plugin hashes

--- home/work/idea/plugins.nix
+++ home/work/idea/plugins.nix
@@ -8,7 +8,7 @@ let
     "1347-scala" = {
       name = "Scala";
       url = "https://plugins.jetbrains.com/files/1347/544339/scala-intellij-bin-2024.1.21.zip";
-      hash = "sha256-6ibo1vdwO4olQTCWpWAefT3QCwgtzTo1ojilDes8Rvg=";
+      hash = "sha256-X7gAfpzhnTOsz3z170ahZ1ddqMHND8+qMzoXU51R/S4=";
     };
     "164-ideavim" = {
       name = "IdeaVim";
@@ -23,7 +23,7 @@ let
     "23029-catppuccin-icons" = {
       name = "Catppuccin Icons";
       url = "https://plugins.jetbrains.com/files/23029/542152/Catppuccin_Icons-1.5.0.zip";
-      hash = "sha256-AlxMNYxS84BvQalaj3o14zHEXONuRNE5EOaJO7kNqAQ=";
+      hash = "sha256-Mm3V0mlW59EJyCzr75dhKFsOfUPnsVnU+D26+sFAnQ4=";
     };
     "12062-vscode-keymap" = {
       name = "VSCode Keymap";
