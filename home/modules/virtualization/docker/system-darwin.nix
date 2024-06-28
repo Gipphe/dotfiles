e42c627308342f63971591ac909096ec0de5a3cb
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-28 10:51:34 +0200

    fix: docker

--- home/modules/virtualization/docker/system-darwin.nix
+++ home/modules/virtualization/docker/system-darwin.nix
@@ -1,6 +1,4 @@
 { lib, config, ... }:
 {
-  config = lib.mkIf (config.homebrew.enable && config.gipphe.virtualisation.docker.enable) {
-    homebrew.casks = [ "docker" ];
-  };
+  config = lib.mkIf config.gipphe.virtualisation.docker.enable { homebrew.casks = [ "docker" ]; };
 }
