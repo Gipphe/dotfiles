2f5a2519d61b8d89b010c0ab327ebf252722e1e1
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-28 10:34:10 +0200

    fix: homebrew

--- home/profiles/darwin.nix
+++ home/profiles/darwin.nix
@@ -4,6 +4,7 @@
   config = lib.mkIf config.gipphe.profiles.darwin.enable {
     gipphe.programs = {
       alt-tab.enable = true;
+      homebrew.enable = true;
       karabiner-elements.enable = true;
       linearmouse.enable = true;
       xnviewmp.enable = true;
