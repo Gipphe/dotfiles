22cb1b3a0b4c6e6b046b5c9610832f04828e907a
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-30 23:43:45 +0200

    feat: set profiles for nixos-vm and trond-arne

--- home/machines/nixos-vm/default.nix
+++ home/machines/nixos-vm/default.nix
@@ -9,11 +9,22 @@
   options.gipphe.machines.nixos-vm.enable = lib.mkEnableOption "nixos-vm machine config";
   config = lib.mkIf config.gipphe.machines.nixos-vm.enable {
     gipphe.profiles = {
-      core.enable = true;
-      systemd.enable = true;
+      nixos = {
+        audio.enable = true;
+        boot-legacy.enable = true;
+        devices.enable = true;
+        system.enable = true;
+        zramswap.enable = true;
+      };
+      cli.enable = true;
       audio.enable = true;
+      core.enable = true;
+      desktop-normal.enable = true;
       desktop.enable = true;
       fonts.enable = true;
+      secrets.enable = true;
+      systemd.enable = true;
+      vm-guest.enable = true;
     };
   };
 }
--- home/machines/trond-arne/default.nix
+++ home/machines/trond-arne/default.nix
@@ -5,16 +5,62 @@
   ...
 }:
 {
+  # flags = {
+  #   user = {
+  #     username = "gipphe";
+  #     homeDirectory = "/home/gipphe";
+  #   };
+  #   system = {
+  #     type = "nixos";
+  #     homeFonts = true;
+  #     secrets = true;
+  #     systemd = true;
+  #   };
+  #   use-case = {
+  #     work = false;
+  #     cli = true;
+  #     personal = {
+  #       enable = true;
+  #       gaming = true;
+  #     };
+  #   };
+  #   desktop = {
+  #     enable = true;
+  #     hyprland = false;
+  #     plasma = true;
+  #     wayland = false;
+  #   };
+  #   virtualisation = {
+  #     wsl = false;
+  #     virtualbox = false;
+  #   };
+  #   bootloader.type = "efi";
+  #   aux = {
+  #     networkmanager = true;
+  #     audio = true;
+  #     printer = false;
+  #     nvidia = false;
+  #   };
+  # };
   imports = lib.optional flags.isSystem ./system-all.nix;
   options.gipphe.machines.trond-arne.enable = lib.mkEnableOption "trond-arne machine config";
   config = lib.mkIf config.gipphe.machines.trond-arne.enable {
     gipphe.profiles = {
-      core.enable = true;
-      systemd.enable = true;
+      nixos = {
+        audio.enable = true;
+        boot-efi.enable = true;
+        devices.enable = true;
+        system.enable = true;
+      };
       audio.enable = true;
+      cli.enable = true;
+      core.enable = true;
+      desktop-normal.enable = true;
       desktop.enable = true;
-      gaming.enable = true;
       fonts.enable = true;
+      gaming.enable = true;
+      secrets.enable = true;
+      systemd.enable = true;
     };
   };
 }
