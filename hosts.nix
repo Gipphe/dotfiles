5e5e597cdbab31c2ce5bd8326c7badc5905f1f64
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-30 23:49:25 +0200

    refactor: use simplified format for machine definitions

--- hosts.nix
+++ hosts.nix
@@ -1,75 +1,10 @@
 { nixpkgs, self, ... }@inputs:
 let
   machines = {
-    Jarle = {
-      system = "x86_64-linux";
-    };
-    nixos-vm = {
-      system = "x86_64-linux";
-      gipphe.machine = "nixos-vm";
-      flags = {
-        user = {
-          username = "gipphe";
-          homeDirectory = "/home/gipphe";
-        };
-        system = {
-          type = "nixos";
-          secrets = true;
-        };
-        use-case = {
-          work = false;
-          personal.enable = false;
-        };
-        bootloader.type = "grub";
-        virtualisation = {
-          virtualbox = true;
-        };
-      };
-    };
-    trond-arne = {
-      system = "x86_64-linux";
-      gipphe.machine = "trond-arne";
-      flags = {
-        user = {
-          username = "gipphe";
-          homeDirectory = "/home/gipphe";
-        };
-        system = {
-          type = "nixos";
-          homeFonts = true;
-          secrets = true;
-          systemd = true;
-        };
-        use-case = {
-          work = false;
-          cli = true;
-          personal = {
-            enable = true;
-            gaming = true;
-          };
-        };
-        desktop = {
-          enable = true;
-          hyprland = false;
-          plasma = true;
-          wayland = false;
-        };
-        virtualisation = {
-          wsl = false;
-          virtualbox = false;
-        };
-        bootloader.type = "efi";
-        aux = {
-          networkmanager = true;
-          audio = true;
-          printer = false;
-          nvidia = false;
-        };
-      };
-    };
-    VNB-MB-Pro = {
-      system = "aarch64-darwin";
-    };
+    Jarle.system = "x86_64-linux";
+    nixos-vm.system = "x86_64-linux";
+    trond-arne.system = "x86_64-linux";
+    VNB-MB-Pro.system = "aarch64-darwin";
   };
   inherit (nixpkgs.lib.attrsets) filterAttrs mapAttrs;
   inherit (nixpkgs.lib) nixosSystem hasSuffix;
