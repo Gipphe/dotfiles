8823d0dc7ceb291a0568f7ed8530442463333ccc
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-30 23:47:28 +0200

    refactor: udisks2

--- home/modules/system/udisks2/system-nixos.nix
+++ home/modules/system/udisks2/system-nixos.nix
@@ -7,6 +7,6 @@
 {
   config = lib.mkIf config.gipphe.system.udisks2.enable {
     services.udisks2.enable = true;
-    dbus = lib.mkIf config.gipphe.system.dbus.enable { packages = with pkgs; [ udisks2 ]; };
+    services.dbus.packages = with pkgs; [ udisks2 ];
   };
 }
