b9bfb80ee3b40d447f18dea87c1d74f569e5a793
Victor Nascimento Bakke <gipphe@gmail.com>
2024-06-30 23:44:12 +0200

    fix: screen

--- home/modules/core/screen/nixos-vm.nix
+++ home/modules/core/screen/nixos-vm.nix
@@ -1,17 +1,20 @@
+{ lib, config, ... }:
 {
-  services.xserver = {
-    # Attempt to set the screen resolution
-    virtualScreen = {
-      x = 1920;
-      y = 940;
-    };
+  config = lib.mkIf config.gipphe.machines.nixos-vm.enable {
+    services.xserver = {
+      # Attempt to set the screen resolution
+      virtualScreen = {
+        x = 1920;
+        y = 940;
+      };
 
-    # Add custom screen resolution that works nicely in VirtualBox
-    monitorSection = ''
-      Modeline "1920x940_60.00"  149.45  1920 2032 2240 2560  940 941 944 973  -HSync +Vsync  
-    '';
-    deviceSection = ''
-      Option "ModeValidation"
-    '';
+      # Add custom screen resolution that works nicely in VirtualBox
+      monitorSection = ''
+        Modeline "1920x940_60.00"  149.45  1920 2032 2240 2560  940 941 944 973  -HSync +Vsync  
+      '';
+      deviceSection = ''
+        Option "ModeValidation"
+      '';
+    };
   };
 }
--- home/modules/core/screen/system-nixos.nix
+++ home/modules/core/screen/system-nixos.nix
@@ -1,11 +1,1 @@
-{ config, ... }:
-let
-  inherit (config.networking) hostname;
-  systems = {
-    nixos-vm = ./nixos-vm.nix;
-  };
-  imports = if builtins.hasAttr hostname systems then [ systems.${hostname} ] else [ ];
-in
-{
-  inherit imports;
-}
+{ imports = [ ./nixos-vm.nix ]; }
