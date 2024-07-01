b7c2d738034e27b3788ad6320994c7bff10bd73e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-01 13:39:06 +0200

    chore: add highlighting to devhsell scripts

--- devShell/default.nix
+++ devShell/default.nix
@@ -4,54 +4,57 @@
     {
       help = "Rebuild the system using nh, darwin-rebuild or home-manager, whichever is applicable";
       name = "sw";
-      command = ''
-        if command -v nixos-rebuild &>/dev/null; then
-          nh os switch
-        elif command -v darwin-rebuild &>/dev/null; then
-          darwin-rebuild build --flake $(pwd)
-          nvd diff /run/current-system result
-          darwin-rebuild switch --flake $(pwd)
-        elif command -v home-manager &>/dev/null; then
-          nh home switch
-        fi
-      '';
+      command = # bash
+        ''
+          if command -v nixos-rebuild &>/dev/null; then
+            nh os switch
+          elif command -v darwin-rebuild &>/dev/null; then
+            darwin-rebuild build --flake $(pwd)
+            nvd diff /run/current-system result
+            darwin-rebuild switch --flake $(pwd)
+          elif command -v home-manager &>/dev/null; then
+            nh home switch
+          fi
+        '';
       category = "build";
     }
     {
       help = "Rebuild the system using nh os switch --ask";
       name = "swa";
-      command = ''
-        if command -v nixos-rebuild &> /dev/null; then
-          nh os switch --ask
-        elif command -v darwin-rebuild &>/dev/null; then
-          darwin-rebuild build --flake $(pwd)
+      command = # bash
+        ''
+          if command -v nixos-rebuild &> /dev/null; then
+            nh os switch --ask
+          elif command -v darwin-rebuild &>/dev/null; then
+            darwin-rebuild build --flake $(pwd)
 
-          nvd diff /run/current-system result
+            nvd diff /run/current-system result
 
-          read -p "Apply these changes? (y/N) " -n 1 REPLY
-          echo
+            read -p "Apply these changes? (y/N) " -n 1 REPLY
+            echo
 
-          case "$REPLY" in
-            y|Y) darwin-rebuild switch --flake $(pwd) ;;
-          esac
-        else
-          echo "This is not a NixOS system" >&2
-          exit 1
-        fi
-      '';
+            case "$REPLY" in
+              y|Y) darwin-rebuild switch --flake $(pwd) ;;
+            esac
+          else
+            echo "This is not a NixOS system" >&2
+            exit 1
+          fi
+        '';
       category = "build";
     }
     {
       help = "Rebuild the system using nh os boot";
       name = "boot";
-      command = ''
-        if command -v nixos-rebuild &> /dev/null; then
-          nh os boot
-        else
-          echo "This is not a NixOS system" >&2
-          exit 1
-        fi
-      '';
+      command = # bash
+        ''
+          if command -v nixos-rebuild &> /dev/null; then
+            nh os boot
+          else
+            echo "This is not a NixOS system" >&2
+            exit 1
+          fi
+        '';
       category = "build";
     }
 
@@ -59,7 +62,8 @@
     {
       help = "Format the source tree with treefmt";
       name = "fmt";
-      command = "treefmt";
+      command = # bash
+        "treefmt";
       category = "formatter";
     }
 
@@ -67,7 +71,8 @@
     {
       help = "Update flake inputs and commit changes";
       name = "update";
-      command = ''nix flake update && git commit flake.lock -m "chore: update flake inputs"'';
+      command = # bash
+        ''nix flake update && git commit flake.lock -m "chore: update flake inputs"'';
       category = "utils";
     }
 
@@ -75,20 +80,22 @@
     {
       help = "Track distribution of PR";
       name = "nix:pr";
-      command = ''
-        if test -z "$1"; then
-          echo "Usage: nix:pr <pr number>"
-          exit 1
-        fi
-        prg=$(command -v open || command -v xdg-open)
-        eval "$prg 'https://nixpk.gs/pr-tracker.html?pr=$1'"
-      '';
+      command = # bash
+        ''
+          if test -z "$1"; then
+            echo "Usage: nix:pr <pr number>"
+            exit 1
+          fi
+          prg=$(command -v open || command -v xdg-open)
+          eval "$prg 'https://nixpk.gs/pr-tracker.html?pr=$1'"
+        '';
       category = "nix utils";
     }
     {
       help = "View store path sizes";
       name = "nix:du";
-      command = "nix path-info -rS /run/current-system | sort -nk2";
+      command = # bash
+        "nix path-info -rS /run/current-system | sort -nk2";
       category = "nix utils";
     }
 
@@ -96,25 +103,29 @@
     {
       help = "Check .nix files with statix";
       name = "lint:statix";
-      command = "statix check";
+      command = # bash
+        "statix check";
       category = "lint";
     }
     {
       help = "Check .nix files with statix, watching";
       name = "lint:statix:watch";
-      command = "find . -type f | entr static check";
+      command = # bash
+        "find . -type f | entr static check";
       category = "lint";
     }
     {
       help = "Check .nix files with deadnix";
       name = "lint:deadnix";
-      command = "deadnix --exclude ./system/*/hardware-configuration/*.nix";
+      command = # bash
+        "deadnix --exclude ./system/*/hardware-configuration/*.nix";
       category = "lint";
     }
     {
       help = "Check .nix files with deadnix, watching";
       name = "lint:deadnix:watch";
-      command = "find . -type f | entr deadnix --exclude ./system/*/hardware-configuration/*.nix";
+      command = # bash
+        "find . -type f | entr deadnix --exclude ./system/*/hardware-configuration/*.nix";
       category = "lint";
     }
   ];
