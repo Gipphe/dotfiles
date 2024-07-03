ee022578e5f9d406d57a0dda28f47dab588225e6
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-03 22:10:14 +0200

    fix: 1password GUI

--- home/modules/programs/_1password-gui/home-manager.nix
+++ home/modules/programs/_1password-gui/home-manager.nix
@@ -2,11 +2,10 @@
   lib,
   config,
   pkgs,
-  flags,
   ...
 }:
 {
-  config = lib.mkIf (config.gipphe.programs._1password-gui.enable && flags.isNixos) {
+  config = lib.mkIf (config.gipphe.programs._1password-gui.enable && pkgs.stdenv.isLinux) {
     home.packages = with pkgs; [ _1password-gui ];
   };
 }
