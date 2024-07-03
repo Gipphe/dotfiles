e2b2f23fe6a63b44a3d94fac6ed8873e9972e4cc
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-03 21:50:55 +0200

    tweak: move cyberduck to work profile

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -5,7 +5,6 @@ util.mkProfile "desktop" {
       _1password-gui.enable = true;
       appimage.enable = true;
       cool-retro-term.enable = true;
-      cyberduck.enable = true;
       filen.enable = true;
       gimp.enable = true;
       kitty.enable = true;
--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -1,10 +1,11 @@
 { util, ... }:
 util.mkProfile "work" {
   gipphe.programs = {
+    cyberduck.enable = true;
     idea-ultimate.enable = true;
     neo4j-desktop.enable = true;
     notion.enable = true;
-    slack.enable = true;
     openvpn-connect.enable = true;
+    slack.enable = true;
   };
 }
