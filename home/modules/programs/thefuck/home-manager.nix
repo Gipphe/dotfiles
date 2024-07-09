fbc0166d33c75571152bcdacdaf853dc02eb0f37
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-10 00:56:26 +0200

    fix: thefuck build error

--- home/modules/programs/thefuck/home-manager.nix
+++ home/modules/programs/thefuck/home-manager.nix
@@ -1,4 +1,13 @@
-{ lib, config, ... }:
 {
-  config = lib.mkIf config.gipphe.programs.thefuck.enable { programs.thefuck.enable = true; };
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf config.gipphe.programs.thefuck.enable {
+    programs.thefuck.enable = true;
+    programs.thefuck.package = inputs.nixpkgs-master.legacyPackages.${pkgs.system}.thefuck;
+  };
 }
