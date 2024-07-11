3290ecb7a7e3befcf1523e2ddf04c0add861c6eb
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-11 11:24:48 +0200

    fix: rm aliases

--- home/modules/programs/fish/home-manager.nix
+++ home/modules/programs/fish/home-manager.nix
@@ -47,6 +47,8 @@
 
           # Must be declared in shellInit because shellAbbrs does not escape "?"
           abbr --add -- '?' 'mods --role shell -q'
+          abbr --add -- "rm -rf" "rm -rfi";
+          abbr --add -- "rm -fr" "rm -fri";
         '';
         package = pkgs.fish;
         shellAbbrs = {
@@ -65,8 +67,6 @@
           docker_pull_images = "docker images --format '{{.Repository}}:{{.Tag}}' | xargs -n 1 -P 1 docker pull";
           # Taken from https://discourse.nixos.org/t/list-and-delete-nixos-generations/29637/6
           prune-gens = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/sytem --older-than";
-          "rm -rf" = "rm -rfi";
-          "rm -fr" = "rm -fri";
         };
         plugins = [
           {
