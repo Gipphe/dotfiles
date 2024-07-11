df3133f0f73e4254d681c0fb5e015d450d769ab1
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-11 10:53:34 +0200

    fix: ? abbr

--- home/modules/programs/fish/home-manager.nix
+++ home/modules/programs/fish/home-manager.nix
@@ -44,6 +44,9 @@
 
           init_ssh_agent
           add_ssh_keys_to_agent
+
+          # Must be declared in shellInit because shellAbbrs does not escape "?"
+          abbr --add -- '?' 'mods --role shell -q'
         '';
         package = pkgs.fish;
         shellAbbrs = {
@@ -62,7 +65,6 @@
           docker_pull_images = "docker images --format '{{.Repository}}:{{.Tag}}' | xargs -n 1 -P 1 docker pull";
           # Taken from https://discourse.nixos.org/t/list-and-delete-nixos-generations/29637/6
           prune-gens = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/sytem --older-than";
-          "?" = "mods --role shell -q";
           "rm -rf" = "rm -rfi";
           "rm -fr" = "rm -fri";
         };
