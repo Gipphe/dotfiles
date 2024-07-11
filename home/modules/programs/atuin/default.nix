157a121b69ba5096eec1584e99cabb5a66340c3e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-11 11:22:31 +0200

    tweak: customize atuin

--- home/modules/programs/atuin/default.nix
+++ home/modules/programs/atuin/default.nix
@@ -6,5 +6,17 @@
 }:
 util.mkModule {
   options.gipphe.programs.atuin.enable = lib.mkEnableOption "atuin";
-  hm = lib.mkIf config.gipphe.programs.atuin.enable { programs.atuin.enable = true; };
+  hm = lib.mkIf config.gipphe.programs.atuin.enable {
+    programs.atuin = {
+      enable = true;
+      settings = {
+        style = "compact";
+        keymap_cursor = {
+          emacs = "blink-bar";
+          vim_insert = "blink-bar";
+          vim_normal = "blink-block";
+        };
+      };
+    };
+  };
 }
