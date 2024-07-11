d8509ffdefb65182975e615df7a2f95c779a34f5
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-11 10:38:03 +0200

    feat: supply mods OpenAI key

--- home/modules/programs/mods/default.nix
+++ home/modules/programs/mods/default.nix
@@ -1,1 +1,28 @@
-{ util, ... }: util.mkSimpleProgramModule "mods"
+{
+  util,
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+util.mkModule {
+  options.gipphe.programs.mods.enable = lib.mkEnableOption "mods";
+
+  hm = lib.mkIf config.gipphe.programs.mods.enable {
+    home.packages = [ pkgs.mods ];
+    xdg.configFile."mods/mods.yaml".source = ./mods.yaml;
+    programs.fish.shellInit = ''
+      set -gx OPENAI_API_KEY ''$(''$XDG_CONFIG_HOME/mods/key)
+    '';
+  };
+
+  system-nixos = lib.mkIf config.gipphe.programs.mods.enable {
+    age.secrets = lib.mkIf (with config.gipphe.environment.secrets; enable && importSecrets) {
+      "mods-cli-openai-api-key.age" = {
+        file = ../../../../secrets/mods-cli-openai-api-key.age;
+        path = "${config.home-manager.users.${config.gipphe.username}.xdg.configHome}/mods/key";
+      };
+    };
+  };
+}
