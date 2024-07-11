eac2f694184b57cb7859b39ac83e81181661a3dc
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-11 11:03:32 +0200

    fix: use personal mods api key

--- home/modules/programs/mods/default.nix
+++ home/modules/programs/mods/default.nix
@@ -5,12 +5,15 @@
   pkgs,
   ...
 }:
+let
+  username = config.gipphe.username;
+in
 util.mkModule {
   options.gipphe.programs.mods.enable = lib.mkEnableOption "mods";
 
   hm = lib.mkIf config.gipphe.programs.mods.enable {
     home.packages = [ pkgs.mods ];
-    xdg.configFile."mods/mods.yaml".source = ./mods.yaml;
+    xdg.configFile."mods/mods.yml".source = ./mods.yaml;
     programs.fish.shellInit = ''
       set -gx OPENAI_API_KEY ''$(cat ''$XDG_CONFIG_HOME/mods/key)
     '';
@@ -21,6 +24,9 @@ util.mkModule {
       "mods-cli-openai-api-key.age" = {
         file = ../../../../secrets/mods-cli-openai-api-key.age;
         path = "${config.home-manager.users.${config.gipphe.username}.xdg.configHome}/mods/key";
+        owner = username;
+        group = username;
+        mode = "400";
       };
     };
   };
