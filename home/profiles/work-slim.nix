4ea971accc5d8964bd48824654fccc79ce653b4c
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-22 14:30:27 +0200

    feat: add gcloud

--- home/profiles/work-slim.nix
+++ home/profiles/work-slim.nix
@@ -2,6 +2,7 @@
 util.mkProfile "work-slim" {
   gipphe.programs = {
     dataform.enable = true;
+    gcloud.enable = true;
     idea-ultimate.enable = true;
   };
 }
--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -3,6 +3,7 @@ util.mkProfile "work" {
   gipphe.programs = {
     cyberduck.enable = true;
     dataform.enable = true;
+    gcloud.enable = true;
     idea-ultimate.enable = true;
     neo4j-desktop.enable = true;
     notion.enable = true;
