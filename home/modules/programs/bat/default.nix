a4f17ef3ce48c24608dd729f64c38f94c97af106
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-23 01:16:54 +0200

    feat: alias cat to bat

--- home/modules/programs/bat/default.nix
+++ home/modules/programs/bat/default.nix
@@ -1,1 +1,10 @@
-{ util, ... }: util.mkSimpleProgramModule "bat"
+{
+  util,
+  lib,
+  config,
+  ...
+}:
+{
+  imports = [ (util.mkSimpleProgramModule "bat") ];
+  config = lib.mkIf (config.gipphe.programs.bat.enable) { programs.fish.shellAbbrs.cat = "bat"; };
+}
