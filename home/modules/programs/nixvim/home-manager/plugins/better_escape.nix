37d3c6da9a1a6f162c877e27385c5fcfe6ba070f
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-23 11:10:38 +0200

    fix: better-escape mappings

--- home/modules/programs/nixvim/home-manager/plugins/better_escape.nix
+++ home/modules/programs/nixvim/home-manager/plugins/better_escape.nix
@@ -5,16 +5,27 @@ in
 {
   programs.nixvim.plugins.better-escape = {
     enable = true;
-    clearEmptyLines = true;
-    keys = helpers.mkRaw ''
-      function()
-        return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<ESC>l' or '<ESC>'
-      end
-    '';
-    timeout = 100;
-    mapping = [
-      "jk"
-      "kj"
-    ];
+    extraOptions = {
+      timeout = 100;
+      mappings =
+        let
+          escape = helpers.mkRaw ''
+            function()
+              return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<ESC>l' or '<ESC>'
+            end
+          '';
+          jk = {
+            j.k = escape;
+            k.j = escape;
+          };
+        in
+        {
+          i = jk;
+          c = jk;
+          t = jk;
+          v = jk;
+          s = jk;
+        };
+    };
   };
 }
