c97e40c41aded695a3054ffa396d22db83ff62cd
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-24 15:17:44 +0200

    refactor: install cool-retro-term from brew on darwin

--- home/modules/programs/cool-retro-term/default.nix
+++ home/modules/programs/cool-retro-term/default.nix
@@ -1,4 +1,17 @@
-{ util, ... }:
 {
-  imports = util.mkSimpleProgramImports "cool-retro-term";
+  config,
+  inputs,
+  lib,
+  pkgs,
+  util,
+  ...
+}:
+util.mkModule {
+  options.gipphe.programs.cool-retro-term.enable = lib.mkEnableOption "cool-retro-term";
+  hm = lib.mkIf config.gipphe.programs.cool-retro-term.enable {
+    home.packages = [
+      (lib.mkIf pkgs.stdenv.isLinux pkgs.cool-retro-term)
+      (lib.mkIf pkgs.stdenv.isDarwin inputs.brew-nix.packages.${pkgs.system}.cool-retro-term)
+    ];
+  };
 }
