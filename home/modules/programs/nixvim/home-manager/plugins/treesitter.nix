f3c98bb3a89736fa4b2a9edea147840329d32a7d
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-25 11:37:06 +0200

    feat: set treesitter-textobjects movement commands

--- home/modules/programs/nixvim/home-manager/plugins/treesitter.nix
+++ home/modules/programs/nixvim/home-manager/plugins/treesitter.nix
@@ -30,7 +30,32 @@ in
           maxLines = 3;
         };
       };
-      treesitter-textobjects.enable = true;
+      treesitter-textobjects = {
+        enable = true;
+        move = {
+          enable = true;
+          gotoNextStart = {
+            "]f" = "@function.outer";
+            "]c" = "@class.outer";
+            "]a" = "@parameter.inner";
+          };
+          gotoNextEnd = {
+            "]F" = "@function.outer";
+            "]C" = "@class.outer";
+            "]A" = "@parameter.inner";
+          };
+          gotoPreviousStart = {
+            "[f" = "@function.outer";
+            "[c" = "@class.outer";
+            "[a" = "@parameter.inner";
+          };
+          gotoPreviousEnd = {
+            "[F" = "@function.outer";
+            "[C" = "@class.outer";
+            "[A" = "@parameter.inner";
+          };
+        };
+      };
       treesitter-refactor = {
         enable = true;
         highlightCurrentScope.enable = true;
