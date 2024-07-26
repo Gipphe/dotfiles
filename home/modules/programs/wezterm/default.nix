4462802078c461a69ac9aad93f6efe39e81e3685
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-26 15:43:30 +0200

    tweak(windows): wezterm size and font

--- home/modules/programs/wezterm/default.nix
+++ home/modules/programs/wezterm/default.nix
@@ -35,6 +35,9 @@ util.mkModule {
             end
 
             return {
+              font_size = 9.0,
+              initial_rows = 40,
+              initial_cols = 200,
               default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
               keys = {
                 { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
--- windows/Config/wezterm/windows-config.lua
+++ windows/Config/wezterm/windows-config.lua
@@ -10,6 +10,9 @@ function M.config()
   end
 
   return {
+    font_size = 9.0,
+    initial_rows = 40,
+    initial_cols = 200,
     default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' },
     keys = {
       { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
