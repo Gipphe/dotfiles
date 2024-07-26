c15675ac80a87c5d9bd4a3003b7350b0b7348ecf
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-26 12:11:18 +0200

    fix: Env windows

--- windows/Modules/Env.psm1
+++ windows/Modules/Env.psm1
@@ -4,12 +4,11 @@ $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
 
 class Env {
-  [HashTable]$EnvVars = @{
-    'HOME' = $Env:USERPROFILE
-    'XDG_CONFIG_HOME' = "$Env:USERPROFILE/.config"
-  }
-
   [Void] Install() {
+    $EnvVars = @{
+      'HOME' = $Env:USERPROFILE
+      'XDG_CONFIG_HOME' = "$Env:USERPROFILE/.config"
+    }
     $EnvVars | ForEach-Object {
       $key = $_
       $val = $EnvVars[$_]
