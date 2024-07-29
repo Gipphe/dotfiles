6f764c811dcd4290fc9de6e9e21d20f4038debba
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-29 13:06:06 +0200

    chore: remove superfluous flake input override

--- flake.nix
+++ flake.nix
@@ -112,7 +112,6 @@
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     catppuccin = {
