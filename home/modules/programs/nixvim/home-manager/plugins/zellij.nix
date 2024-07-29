9e73c2d69c7ff3da673d37a94376cb89b535aade
Victor Nascimento Bakke <gipphe@gmail.com>
2024-07-29 12:57:44 +0200

    feat: switch from zellij.nvim to zellij-nav.nvim

--- home/modules/programs/nixvim/home-manager/plugins/zellij.nix
+++ home/modules/programs/nixvim/home-manager/plugins/zellij.nix
@@ -5,12 +5,12 @@
   ...
 }:
 {
-  programs.nixvim = lib.mkIf config.programs.zellij.enable {
-    extraPlugins = [ pkgs.vimPlugins.zellij-nvim ];
-    extraConfigLua = ''
-      require('zellij').setup({
-        vimTmuxNavigatorKeybinds = true,
-      })
-    '';
-  };
+  # programs.nixvim = lib.mkIf config.programs.zellij.enable {
+  #   extraPlugins = [ pkgs.vimPlugins.zellij-nvim ];
+  #   extraConfigLua = ''
+  #     require('zellij').setup({
+  #       vimTmuxNavigatorKeybinds = true,
+  #     })
+  #   '';
+  # };
 }
