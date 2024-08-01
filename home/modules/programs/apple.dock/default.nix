7ef853c551fc6d62ea33021094c5a961f151003c
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-01 14:10:59 +0200

    feat: add more settings to apple.dock

--- home/modules/programs/apple.dock/default.nix
+++ home/modules/programs/apple.dock/default.nix
@@ -10,6 +10,10 @@ util.mkModule {
     system.defaults.CustomUserPreferences."com.apple.dock" = {
       autohide-delay = 0.0;
       autohide-time-modifier = 0.5;
+      magnification = false;
+      mineffect = "scale";
+      show-process-indicators = true;
+      show-recents = false;
       static-only = true;
     };
   };
