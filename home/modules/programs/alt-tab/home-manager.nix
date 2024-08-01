e62bcdd209496fd5efe6f1a91b87f11b85eaa664
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-01 13:20:16 +0200

    chore: remove alt-tab config
    Is not handled well in this manner.

--- home/modules/programs/alt-tab/home-manager.nix
+++ home/modules/programs/alt-tab/home-manager.nix
@@ -7,9 +7,6 @@
 }:
 {
   config = lib.mkIf config.gipphe.programs.alt-tab.enable {
-    home.activation.import-alt-tab-config = ''
-      run /usr/bin/defaults import com.lwouis.alt-tab-macos ${./alt-tab.xml}
-    '';
     home.packages = with inputs.brew-nix.packages.${pkgs.system}; [ alt-tab ];
   };
 }
