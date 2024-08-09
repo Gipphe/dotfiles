114143aa3168ebe870e8fb9136b004b179a99b3b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-09 21:31:54 +0200

    refactor: simplify zellij plugins

--- home/modules/programs/zellij/default.nix
+++ home/modules/programs/zellij/default.nix
@@ -5,22 +5,60 @@
   pkgs,
   ...
 }:
+let
+  zj_forgot = pkgs.fetchurl {
+    url = "https://github.com/karimould/zellij-forgot/releases/download/0.3.0/zellij_forgot.wasm";
+    hash = "sha256-JNQ4KXb6VzjSF0O4J8Tvq3FXUYBBabQb9ZitcR3kZFw=";
+  };
+in
 util.mkModule {
   options.gipphe.programs.zellij.enable = lib.mkEnableOption "zellij";
+
   hm = lib.mkIf config.gipphe.programs.zellij.enable {
-    home.file = {
-      "zellij-plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
-        url = "https://github.com/karimould/zellij-forgot/releases/download/0.3.0/zellij_forgot.wasm";
-        hash = "sha256-JNQ4KXb6VzjSF0O4J8Tvq3FXUYBBabQb9ZitcR3kZFw=";
-      };
-      "zellij-plugins/zjstatus.wasm".source = pkgs.fetchurl {
-        url = "https://github.com/dj95/zjstatus/releases/download/v0.14.0/zjstatus.wasm";
-        hash = "sha256-TlPnvSz1ROusMXP0CeQJcugR0RdiKTqwMxpUDiP0SRU=";
-      };
-    };
     xdg.configFile = {
       "zellij/layouts".source = ./layouts;
-      "zellij/config.kdl".source = ./config.kdl;
+      "zellij/config.kdl".text = # kdl
+        ''
+          // copy_command "xclip -in -sel clip"
+          copy_on_select true
+          theme "catppuccin-macchiato"
+          keybinds {
+            unbind "Ctrl q"
+            unbind "Ctrl o"
+
+            move {
+              bind "Ctrl e" { SwitchToMode "Normal"; }
+            }
+            shared_except "move" "locked" {
+              bind "Ctrl e" { SwitchToMode "Move"; }
+            }
+
+            session {
+              bind "Ctrl x" { SwitchToMode "Normal"; }
+            }
+            shared_except "session" "locked" {
+              bind "Ctrl x" { SwitchToMode "Session"; }
+            }
+
+            shared_except "locked" {
+              bind "Ctrl h" { MoveFocusOrTab "Left"; }
+              bind "Ctrl l" { MoveFocusOrTab "Right"; }
+              bind "Ctrl j" { MoveFocus "Down"; }
+              bind "Ctrl k" { MoveFocus "Up"; }
+
+              bind "Ctrl y" {
+                LaunchOrFocusPlugin "file:${zj_forgot}" {
+                  floating true
+                }
+              }
+            }
+          }
+          ui {
+            pane_frames {
+              rounded_corners false
+            }
+          }
+        '';
     };
     programs = {
       zellij.enable = true;
