d37992d5d7aac28b03d607ba67975a0b4e3eab89
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-12 10:15:54 +0200

    fix: set hash on vim-jjdescription plugin

--- home/modules/programs/nixvim/home-manager/plugins/vim-jjdescription.nix
+++ home/modules/programs/nixvim/home-manager/plugins/vim-jjdescription.nix
@@ -7,7 +7,7 @@
         owner = "avm99963";
         repo = "vim-jjdescription";
         rev = "ca14325202f3cd894d01ba833451017624249222";
-        hash = "";
+        hash = "sha256-gocAHJU/nrO2Ebmefs1MrrJ4UtfgcvY23TGkBGNzA/k=";
       };
     })
   ];
