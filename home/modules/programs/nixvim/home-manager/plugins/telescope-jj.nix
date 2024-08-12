5d86cbf9d4b683319898370c6752b303f257e709
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-12 11:57:03 +0200

    feat: add telescope-jj.nvim

---
+++ home/modules/programs/nixvim/home-manager/plugins/telescope-jj.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+let
+  inherit (import ../util.nix) kv;
+in
+{
+  programs.nixvim = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "telescope-jj.nvim";
+        src = pkgs.fetchFromGitHub {
+          owner = "zschreur";
+          repo = "telescope-jj.nvim";
+          rev = "9527e39f30eded7950ca127698422ec412d633c4";
+          hash = "sha256-hAXW5IbGs2Z46o/1DEO+Y0HhWu/wduzOLk1JWcNO7EU=";
+        };
+      })
+    ];
+    extraConfigLua = ''
+      function setup_telescope_jj()
+        local telescope = require('telescope')
+        telescope.load_extension("jj")
+      end
+    '';
+    keymaps = [
+      (kv "n" "<leader>jj" # lua
+        ''
+          function(opts)
+            local telescope = require("telescope")
+            local jj_pick_status, jj_res = pcall(telescope.extensions.jj.files, opts)
+            if jj_pick_status then
+              return
+            end
+
+            local builtin = require("telescope.builtin")
+            local git_files_status, git_res = pcall(builtin.git_files, opts)
+            if not git_files_status then
+              error("Could not launch jj/git files: \n" .. jj_res .. "\n" .. git_res)
+            end
+          end
+        ''
+        { desc = "Find files (jj)"; }
+      )
+    ];
+  };
+}
