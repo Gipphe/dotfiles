3aa30a671df04dca21715a8f48072a3ba4f03be8
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-14 23:32:54 +0200

    chore: add haskell.nix binary caches

--- home/modules/programs/nixvim/home-manager/plugins/haskell/default.nix
+++ home/modules/programs/nixvim/home-manager/plugins/haskell/default.nix
@@ -10,5 +10,11 @@
       conform-nvim.formattersByFt.haskell = [ "fourmolu" ];
       lsp.servers.hls.enable = true;
     };
+
+    # Required to prevent building GHC multiple times with haskell.nix
+    nix.settings = {
+      trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+      substituters = [ "https://cache.iog.io" ];
+    };
   };
 }
