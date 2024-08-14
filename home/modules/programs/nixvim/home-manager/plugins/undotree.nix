dbcc19b1d6ec1e347c7717169c3edc5d25e97a21
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-14 08:36:31 +0200

    feat: add keymap for undotree

--- home/modules/programs/nixvim/home-manager/plugins/undotree.nix
+++ home/modules/programs/nixvim/home-manager/plugins/undotree.nix
@@ -1,1 +1,9 @@
-{ programs.nixvim.plugins.undotree.enable = true; }
+let
+  inherit (import ../util.nix) k;
+in
+{
+  programs.nixvim = {
+    plugins.undotree.enable = true;
+    keymaps = [ (k "n" "<leader>cu" "<cmd>UndotreeToggle<cr>" { desc = "Toggle Undotree"; }) ];
+  };
+}
