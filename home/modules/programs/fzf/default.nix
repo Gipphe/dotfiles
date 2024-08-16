a454f1aece447749f0294b5296cbc579401829f0
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-16 14:23:13 +0200

    fix: fzf config mkModule for real this time

--- home/modules/programs/fzf/default.nix
+++ home/modules/programs/fzf/default.nix
@@ -1,5 +1,10 @@
-{ lib, config, ... }:
 {
+  lib,
+  config,
+  util,
+  ...
+}:
+util.mkModule {
   options.gipphe.programs.fzf.enable = lib.mkEnableOption "fzf";
   hm = lib.mkIf config.gipphe.programs.fzf.enable { programs.fzf.enable = true; };
 }
