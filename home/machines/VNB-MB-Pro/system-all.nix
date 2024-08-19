f69703ea999d816ea1c6a24e4b181b6377135d6e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-19 13:47:19 +0200

    fix: disable store path optimisation on darwin

--- home/machines/VNB-MB-Pro/system-all.nix
+++ home/machines/VNB-MB-Pro/system-all.nix
@@ -1,4 +1,7 @@
 { lib, config, ... }:
 {
-  config = lib.mkIf config.gipphe.machines.VNB-MB-Pro.enable { networking.hostName = "VNB-MB-Pro"; };
+  config = lib.mkIf config.gipphe.machines.VNB-MB-Pro.enable {
+    networking.hostName = "VNB-MB-Pro";
+    nix.settings.auto-optimise-store = lib.mkForce false;
+  };
 }
