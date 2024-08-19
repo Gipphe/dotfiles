498fa8856ca7a4707a0238ed6b0e3f55b30c7c29
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-19 15:19:47 +0200

    fix: pin jnv version to previous, working nixpkgs set

--- home/modules/programs/jnv/default.nix
+++ home/modules/programs/jnv/default.nix
@@ -1,1 +1,14 @@
-{ util, ... }: util.mkSimpleProgramModule "jnv"
+{
+  util,
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+util.mkProgram {
+  name = "jnv";
+  hm = lib.mkIf config.gipphe.programs.jnv.enable {
+    home.packages = [ inputs.nixpkgs-agenix-not-broken.legacyPackages.${pkgs.system}.jnv ];
+  };
+}
