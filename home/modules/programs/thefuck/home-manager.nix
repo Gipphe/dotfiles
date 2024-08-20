69bb99e26097c2dba2f25ef7ead6d8c1405f4d9e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-20 15:28:39 +0200

    chore: use thefuck from nixos-unstable

--- home/modules/programs/thefuck/home-manager.nix
+++ home/modules/programs/thefuck/home-manager.nix
@@ -1,13 +1,4 @@
+{ lib, config, ... }:
 {
-  lib,
-  config,
-  inputs,
-  pkgs,
-  ...
-}:
-{
-  config = lib.mkIf config.gipphe.programs.thefuck.enable {
-    programs.thefuck.enable = true;
-    programs.thefuck.package = inputs.nixpkgs-master.legacyPackages.${pkgs.system}.thefuck;
-  };
+  config = lib.mkIf config.gipphe.programs.thefuck.enable { programs.thefuck.enable = true; };
 }
