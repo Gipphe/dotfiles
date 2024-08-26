940c2fe461141c9bfd12cf3d606cabdd42f975ed
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 15:47:46 +0200

    refactor: merge run-as-service config

--- home/modules/programs/run-as-service/default.nix
+++ home/modules/programs/run-as-service/default.nix
@@ -1,4 +1,36 @@
-{ lib, flags, ... }:
 {
-  imports = [ ./options.nix ] ++ lib.optional flags.isHm ./home-manager.nix;
+  lib,
+  flags,
+  config,
+  pkgs,
+  util,
+  ...
+}:
+let
+  apply-hm-env = pkgs.writeShellScript "apply-hm-env" ''
+    ${lib.optionalString (config.home.sessionPath != [ ]) ''
+      export PATH=${builtins.concatStringsSep ":" config.home.sessionPath}:$PATH
+    ''}
+    ${builtins.concatStringsSep "\n" (
+      lib.mapAttrsToList (k: v: ''
+        export ${k}=${builtins.toString v}
+      '') config.home.sessionVariables
+    )}
+    ${config.home.sessionVariablesExtra}
+    exec "$@"
+  '';
+
+  # runs processes as systemd transient services
+  run-as-service = pkgs.writeShellScriptBin "run-as-service" ''
+    exec ${pkgs.systemd}/bin/systemd-run \
+      --slice=app-manual.slice \
+      --property=ExitType=cgroup \
+      --user \
+      --wait \
+      bash -lc "exec ${apply-hm-env} $@"
+  '';
+in
+util.mkProgram {
+  name = "run-as-service";
+  hm = lib.mkIf (!flags.isNixDarwin) { home.packages = [ run-as-service ]; };
 }
--- home/modules/programs/run-as-service/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.programs.run-as-service.enable = lib.mkEnableOption "run-as-service";
-}
