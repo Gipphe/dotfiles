8fc1c61b4c15583f71a397921546c4f60dd7ccac
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 21:19:03 +0200

    refactor: merge plasma config

--- home/modules/environment/desktop/plasma/default.nix
+++ home/modules/environment/desktop/plasma/default.nix
@@ -1,4 +1,40 @@
-{ lib, flags, ... }:
 {
-  imports = [ ./options.nix ] ++ lib.optional flags.isNixos ./system-nixos.nix;
-}
+  util,
+  config,
+  pkgs,
+  ...
+}:
+util.mkToggledModule
+  [
+    "environment"
+    "desktop"
+  ]
+  {
+    name = "plasma";
+    system-nixos = {
+      services = {
+        xserver = {
+          # Enable the X11 windowing system.
+          enable = true;
+
+          # Enable the Plasma (KDE) Desktop Environment.
+          displayManager.sddm.enable = true;
+          desktopManager.plasma5.enable = true;
+        };
+
+        dbus.packages = [ pkgs.gcr ];
+
+        # Enable automatic login for the user.
+        xserver.displayManager.autoLogin = {
+          enable = true;
+          user = config.gipphe.username;
+        };
+      };
+
+      # Workaround for GNOME autologin issue
+      systemd.services = {
+        "getty@tty1".enable = false;
+        "autovt@tty1".enable = false;
+      };
+    };
+  }
--- home/modules/environment/desktop/plasma/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.environment.desktop.plasma.enable = lib.mkEnableOption "plasma";
-}
