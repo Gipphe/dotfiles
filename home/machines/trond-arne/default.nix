8ea7dd1eae54724e5e4e5cc536c4d1d39b3f18e0
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 16:20:01 +0200

    refactor: use mkToggledModule in trond-arne config

--- home/machines/trond-arne/default.nix
+++ home/machines/trond-arne/default.nix
@@ -1,31 +1,30 @@
-{ lib, config, ... }:
-{
-  options.gipphe.machines.trond-arne.enable = lib.mkEnableOption "trond-arne machine config";
-  config = lib.mkIf config.gipphe.machines.trond-arne.enable {
-    gipphe = {
-      username = "gipphe";
-      homeDirectory = "/home/gipphe";
-      hostName = "trond-arne";
-      profiles = {
-        nixos = {
-          audio.enable = true;
-          boot-efi.enable = true;
-          devices.enable = true;
-          system.enable = true;
-        };
+{ util, ... }:
+util.mkToggledModule [ "machines" ] {
+  name = "trond-arne";
+
+  shared.gipphe = {
+    username = "gipphe";
+    homeDirectory = "/home/gipphe";
+    hostName = "trond-arne";
+    profiles = {
+      nixos = {
         audio.enable = true;
-        cli.enable = true;
-        core.enable = true;
-        desktop-normal.enable = true;
-        desktop.enable = true;
-        fonts.enable = true;
-        gaming.enable = true;
-        gc.enable = true;
-        networkmanager.enable = true;
-        rice.enable = true;
-        secrets.enable = true;
-        systemd.enable = true;
+        boot-efi.enable = true;
+        devices.enable = true;
+        system.enable = true;
       };
+      audio.enable = true;
+      cli.enable = true;
+      core.enable = true;
+      desktop-normal.enable = true;
+      desktop.enable = true;
+      fonts.enable = true;
+      gaming.enable = true;
+      gc.enable = true;
+      networkmanager.enable = true;
+      rice.enable = true;
+      secrets.enable = true;
+      systemd.enable = true;
     };
   };
 }
