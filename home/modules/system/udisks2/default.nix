04fa3d9e81675684a45e88c2fc10d234226b4e66
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 21:01:38 +0200

    refactor: merge udisks2 config

--- home/modules/system/udisks2/default.nix
+++ home/modules/system/udisks2/default.nix
@@ -1,4 +1,8 @@
-{ lib, flags, ... }:
-{
-  imports = [ ./options.nix ] ++ lib.optional flags.isNixos ./system-nixos.nix;
+{ util, pkgs, ... }:
+util.mkToggledModule [ "system" ] {
+  name = "udisks2";
+  system-nixos = {
+    services.udisks2.enable = true;
+    services.dbus.packages = with pkgs; [ udisks2 ];
+  };
 }
--- home/modules/system/udisks2/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.system.udisks2.enable = lib.mkEnableOption "udisks2";
-}
