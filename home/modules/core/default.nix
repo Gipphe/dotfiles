7927e336f9e347d9aab6e1cb016461d6bc37dacf
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 16:11:15 +0200

    refactor: merge core config

--- home/modules/core/default.nix
+++ home/modules/core/default.nix
@@ -1,14 +1,73 @@
-{ lib, flags, ... }:
 {
-  imports =
-    lib.optionals flags.isHm [
-      ./xdg.nix
-      ./home.nix
-    ]
-    ++ [
-      ./fontconfig
-      ./screen
-    ]
-    ++ lib.optional flags.isNixDarwin ./system-darwin.nix
-    ++ lib.optional flags.isNixos ./system-nixos.nix;
+  inputs,
+  config,
+  pkgs,
+  util,
+  ...
+}:
+util.mkModule {
+  shared.imports = [
+    ./fontconfig
+    ./screen
+  ];
+
+  hm = {
+    home = {
+      inherit (config.gipphe) username homeDirectory;
+      # NO TOUCHY!
+      stateVersion = "23.11";
+      sessionVariables = {
+        FLAKE = "${config.home.homeDirectory}/projects/dotfiles";
+        # Help dynamically linked libraries and other libraries depending upon the
+        # c++ stdenv find their stuff
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      };
+      packages = with pkgs; [ xdg-utils ];
+    };
+
+    # Manage home-manager executable
+    programs.home-manager.enable = true;
+
+    # Manage XDG directories and configs
+    xdg.enable = true;
+  };
+
+  system-nixos = {
+    documentation.dev.enable = false;
+
+    # Auto-upgrading with nixos-unstable is risky
+    system.autoUpgrade.enable = false;
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It's perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.11"; # Did you read the comment?
+  };
+
+  system-darwin = {
+    imports = [ inputs.mac-app-util.darwinModules.default ];
+
+    # home-manager.users.${config.gipphe.username}.imports = [
+    #   inputs.mac-app-util.homeManagerModules.default
+    # ];
+
+    # Need to have zsh to do much on darwin
+    programs.zsh.enable = true;
+
+    # Auto upgrade nix package and the daemon service.
+    services.nix-daemon.enable = true;
+
+    # Set Git commit hash for darwin-version.
+    system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 4;
+
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = "aarch64-darwin";
+  };
 }
--- home/modules/core/xdg.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  # Manage XDG directories and configs
-  xdg.enable = true;
-  home.packages = with pkgs; [ xdg-utils ];
-}
