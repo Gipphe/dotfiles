eb7d41c6d391cf139615e25369da8f451649141a
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 15:31:48 +0200

    refactor: merge obsidian config

--- home/modules/programs/obsidian/default.nix
+++ home/modules/programs/obsidian/default.nix
@@ -1,4 +1,18 @@
-{ lib, flags, ... }:
 {
-  imports = [ ./options.nix ] ++ lib.optional flags.isHm ./home-manager.nix;
+  lib,
+  pkgs,
+  inputs,
+  util,
+  ...
+}:
+util.mkProgram {
+  name = "obsidian";
+  hm = (
+    lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isLinux { home.packages = [ pkgs.obsidian ]; })
+      (lib.mkIf pkgs.stdenv.isDarwin {
+        home.packages = [ inputs.brew-nix.packages.${pkgs.system}.obsidian ];
+      })
+    ]
+  );
 }
--- home/modules/programs/obsidian/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.programs.obsidian.enable = lib.mkEnableOption "obsidian";
-}
