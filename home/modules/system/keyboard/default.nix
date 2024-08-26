3e9c6af3ee78e7e27734ab9b633ca02967f6cda6
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 21:06:22 +0200

    refactor: merge keyboard config

--- home/modules/system/keyboard/default.nix
+++ home/modules/system/keyboard/default.nix
@@ -1,4 +1,14 @@
-{ lib, flags, ... }:
-{
-  imports = [ ./options.nix ] ++ lib.optional flags.isNixos ./system-nixos.nix;
+{ util, ... }:
+util.mkToggledModule [ "system" ] {
+  name = "keyboard";
+  system-nixos = {
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "no";
+      variant = "";
+    };
+
+    # Configure console keymap
+    console.keyMap = "no";
+  };
 }
--- home/modules/system/keyboard/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.system.keyboard.enable = lib.mkEnableOption "keyboard";
-}
