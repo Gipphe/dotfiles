12a38df7e4ba3167f721c0b79126d9e655420f56
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 16:16:57 +0200

    refactor: merge VNB-MB-Pro config

--- home/machines/VNB-MB-Pro/default.nix
+++ home/machines/VNB-MB-Pro/default.nix
@@ -1,32 +1,29 @@
-{
-  lib,
-  config,
-  flags,
-  ...
-}:
-{
-  imports = lib.optional flags.isSystem ./system-all.nix;
-  options.gipphe.machines.VNB-MB-Pro.enable = lib.mkEnableOption "VNB-MB-Pro machine config";
-  config = lib.mkIf config.gipphe.machines.VNB-MB-Pro.enable {
-    gipphe = {
-      username = "victor";
-      homeDirectory = "/Users/victor";
-      hostName = "VNB-MB-Pro";
-      profiles = {
-        ai.enable = true;
-        cli.enable = true;
-        containers.enable = true;
-        core.enable = true;
-        darwin.enable = true;
-        desktop.enable = true;
-        k8s.enable = true;
-        kvm.enable = true;
-        logitech.enable = true;
-        music.enable = true;
-        rice.enable = true;
-        secrets.enable = true;
-        work.enable = true;
-      };
+{ lib, util, ... }:
+util.mkToggledModule [ "machines" ] {
+  name = "VNB-MB-Pro";
+
+  shared.gipphe = {
+    username = "victor";
+    homeDirectory = "/Users/victor";
+    hostName = "VNB-MB-Pro";
+    profiles = {
+      ai.enable = true;
+      cli.enable = true;
+      containers.enable = true;
+      core.enable = true;
+      darwin.enable = true;
+      desktop.enable = true;
+      k8s.enable = true;
+      kvm.enable = true;
+      logitech.enable = true;
+      music.enable = true;
+      rice.enable = true;
+      secrets.enable = true;
+      work.enable = true;
     };
+  };
+
+  system-all = {
+    nix.settings.auto-optimise-store = lib.mkForce false;
   };
 }
--- home/machines/VNB-MB-Pro/system-all.nix
+++
@@ -1,6 +0,0 @@
-{ lib, config, ... }:
-{
-  config = lib.mkIf config.gipphe.machines.VNB-MB-Pro.enable {
-    nix.settings.auto-optimise-store = lib.mkForce false;
-  };
-}
