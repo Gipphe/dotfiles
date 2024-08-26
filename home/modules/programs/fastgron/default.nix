39f34257377d1ee0a0bea8bff717a1a77083a7f2
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 15:28:49 +0200

    refactor: merge fastgron config

--- home/modules/programs/fastgron/default.nix
+++ home/modules/programs/fastgron/default.nix
@@ -1,4 +1,5 @@
-{ lib, flags, ... }:
-{
-  imports = [ ./options.nix ] ++ lib.optional flags.isHm ./home-manager.nix;
+{ util, pkgs, ... }:
+util.mkProgram {
+  name = "fastgron";
+  hm.home.packages = with pkgs; [ fastgron ];
 }
--- home/modules/programs/fastgron/options.nix
+++
@@ -1,4 +0,0 @@
-{ lib, ... }:
-{
-  options.gipphe.programs.fastgron.enable = lib.mkEnableOption "fastgron";
-}
