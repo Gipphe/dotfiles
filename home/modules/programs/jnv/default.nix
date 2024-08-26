46e061451fedf5cf222133784a9d16ea51461ffa
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 21:57:27 +0200

    refactor: remove unnecessary condition in jnv config

--- home/modules/programs/jnv/default.nix
+++ home/modules/programs/jnv/default.nix
@@ -1,14 +1,10 @@
 {
   util,
   inputs,
-  lib,
-  config,
   pkgs,
   ...
 }:
 util.mkProgram {
   name = "jnv";
-  hm = lib.mkIf config.gipphe.programs.jnv.enable {
-    home.packages = [ inputs.nixpkgs-before-rust-1-80-breaking.legacyPackages.${pkgs.system}.jnv ];
-  };
+  hm.home.packages = [ inputs.nixpkgs-before-rust-1-80-breaking.legacyPackages.${pkgs.system}.jnv ];
 }
