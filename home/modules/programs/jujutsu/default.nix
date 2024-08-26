b28c71cef0a9d14c3e3b998daac7bc6ee6cb51ab
Victor Nascimento Bakke <gipphe@gmail.com>
2024-08-26 15:18:22 +0200

    refactor: merge jujutsu config

--- home/modules/programs/jujutsu/default.nix
+++ home/modules/programs/jujutsu/default.nix
@@ -1,4 +1,58 @@
-{ lib, flags, ... }:
 {
-  imports = [ ./options.nix ] ++ lib.optional flags.isHm ./home-manager.nix;
+  util,
+  config,
+  pkgs,
+  ...
+}:
+util.mkProgram {
+  name = "jujutsu";
+
+  hm = {
+    programs.jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Victor Nascimento Bakke";
+          email = "gipphe@gmail.com";
+        };
+        ui = {
+          editor = "nvim";
+          default-command = "lol";
+        };
+        git = {
+          auto-local-branch = true;
+        };
+        signing = {
+          sign-all = true;
+          backend = "gpg";
+          program = if pkgs.stdenv.isLinux then config.programs.gpg.package else "gpg";
+          key = "23723701395B436C";
+        };
+        aliases = {
+          lol = [
+            "log"
+            "-r"
+            "all()"
+          ];
+          sync = [
+            "branch"
+            "set"
+            "-r"
+            "@-"
+          ];
+        };
+      };
+    };
+    xdg.configFile."fish/functions/jj.fish".text = # fish
+      ''
+        function jj
+          set -l re "\/strise\/"
+          if string match -qr $re $PWD
+            ${config.programs.jujutsu.package}/bin/jj --config-toml='signing.key="B4C7E23DDC6AE725"' $argv
+          else
+            ${config.programs.jujutsu.package}/bin/jj $argv
+          end
+        end
+      '';
+  };
 }
