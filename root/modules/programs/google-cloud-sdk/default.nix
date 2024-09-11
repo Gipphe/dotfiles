0a908df0a8298bb293e92d5013cccd7a4201c494
Victor Nascimento Bakke <gipphe@gmail.com>
2024-09-11 22:20:49 +0200

    feat: add gcloud clusters

--- root/modules/programs/google-cloud-sdk/default.nix
+++ root/modules/programs/google-cloud-sdk/default.nix
@@ -1,13 +1,122 @@
-{ util, pkgs, ... }:
+{
+  util,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  region = "europe-west1-b";
+  mkCommon = alias: {
+    name = alias;
+    inherit alias region;
+    project = "strise-common";
+
+  };
+  mkCosmos = alias: {
+    inherit alias region;
+    name = "cosmos";
+    project = alias;
+  };
+
+  common_ns = [
+    "jenkins"
+    "shared"
+    "janitor"
+    "knowledge"
+  ];
+  cosmos = [
+    "strise-common"
+    "strise-stage"
+    "strise-prod"
+  ];
+  inherit (builtins) map concatLists;
+  default_clusters = concatLists [
+    (map mkCommon common_ns)
+    (map mkCosmos cosmos)
+  ];
+
+  gcloud = pkgs.google-cloud-sdk.withExtraComponents (
+    with pkgs.google-cloud-sdk.components;
+    [
+      bq
+      gke-gcloud-auth-plugin
+      kubectl
+    ]
+  );
+
+  addClusterIfMissing = util.writeFishApplication rec {
+    name = "add_cluster_if_missing";
+    runtimeInputs = [
+      pkgs.kubectx
+      gcloud
+    ];
+    runtimeEnv = {
+      script_name = name;
+    };
+    text = # fish
+      ''
+        argparse --name $script_name 'p/project=' 'n/name=' 'r/region=' 'a/alias=' -- $argv
+        or return
+
+        set -l aliases $(kubectx)
+        string match -rq $_flag_alias $aliases
+        if test $status != 0
+          gcloud container clusters get-credentials "$_flag_name" --internal-ip --project "$_flag_project" --region "$_flag_region"
+          kubectx "$_flag_alias"=gke_"$_flag_project"_"$_flag_region"_"$_flag_name"
+        end
+      '';
+  };
+  addClusters = lib.pipe config.gipphe.programs.google-cloud-sdk.clusters [
+    (map (
+      cluster:
+      pkgs.writeShellScriptBin "add_${cluster.alias}_cluster" ''
+        ${lib.getExe addClusterIfMissing} --project '${cluster.project}' --name '${cluster.name}' --region '${cluster.region}' --alias '${cluster.alias}'
+      ''
+    ))
+    (map lib.getExe)
+    (lib.concatStringsSep "; ")
+    (pkgs.writeShellScriptBin "add_clusters")
+  ];
+in
 util.mkProgram {
   name = "google-cloud-sdk";
-  hm.home.packages = with pkgs; [
-    (google-cloud-sdk.withExtraComponents (
-      with google-cloud-sdk.components;
-      [
-        gke-gcloud-auth-plugin
-        kubectl
-      ]
-    ))
-  ];
+  options.gipphe.programs.google-cloud-sdk.clusters = lib.mkOption {
+    description = "clusters";
+    type = lib.types.listOf (
+      lib.types.submodule {
+        options = {
+          alias = lib.mkOption {
+            description = "Alias for the cluster.";
+            default = throw "Missing alias for cluster.";
+            type = lib.types.str;
+          };
+          name = lib.mkOption {
+            description = "Name of the cluster.";
+            default = throw "Missing name for cluster.";
+            type = lib.types.str;
+          };
+          project = lib.mkOption {
+            description = "Project name of the cluster.";
+            default = throw "Missing project for cluster.";
+            type = lib.types.str;
+          };
+          region = lib.mkOption {
+            description = "Region of the cluster.";
+            default = throw "Missing region for the cluster.";
+            type = lib.types.str;
+          };
+        };
+      }
+    );
+    default = default_clusters;
+  };
+  hm = {
+    home = {
+      packages = [ gcloud ];
+      activation.google-cloud-sdk-credentials = lib.hm.dag.entryAfter [ "onFilesChange" ] ''
+        run ${lib.getExe addClusters}
+      '';
+    };
+  };
 }
