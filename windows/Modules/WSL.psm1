90fa389a89647af61c17c21dce8e4d844b2c094b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-09-27 10:16:48 +0200

    fix(windows): add missing newline escapes

--- windows/Modules/WSL.psm1
+++ windows/Modules/WSL.psm1
@@ -25,7 +25,7 @@ class WSL {
     $this.Stamp.Register("install-nixos-wsl", {
       $ChildLogger.Info($(Invoke-WebRequest `
         -Uri "https://github.com/nix-community/NixOS-WSL/releases/download/2311.5.3/nixos-wsl.tar.gz" `
-        -OutFile "$HOME\Downloads\nixos-wsl.tar.gz"
+        -OutFile "$HOME\Downloads\nixos-wsl.tar.gz" `
       ))
       $ChildLogger.Info($(Invoke-Native { wsl --import "NixOS" "$HOME\NixOS\" "$HOME\Downloads\nixos-wsl.tar.gz" }))
       $ChildLogger.Info($(Invoke-Native { wsl --set-default "NixOS" }))
@@ -37,7 +37,7 @@ class WSL {
           ! test -s '$HOME/projects/dotfiles' `
           '&&' nix-shell -p git --run '"git clone https://codeberg.org/Gipphe/dotfiles.git"' '"$HOME/projects/dotfiles"' `
           '&&' cd '$HOME/projects/dotfiles' `
-          '&&' nixos-rebuild --extra-experimental-features 'flakes nix-command' switch --flake '"$(pwd)#Jarle"'
+          '&&' nixos-rebuild --extra-experimental-features 'flakes nix-command' switch --flake '"$(pwd)#Jarle"' `
         ))
       }
     })
