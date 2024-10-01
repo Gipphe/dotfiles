22aa436fdc5dea473cde1d2a2ae56d5a2b5dc4f2
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-01 12:17:04 +0200

    feat: add cursor to work-slim profile

--- profiles/work-slim.nix
+++ profiles/work-slim.nix
@@ -1,6 +1,7 @@
 { util, ... }:
 util.mkProfile "work-slim" {
   gipphe.programs = {
+    cursoride.enable = true;
     google-cloud-sdk.enable = true;
     idea-ultimate.enable = true;
     kubectl.enable = true;
