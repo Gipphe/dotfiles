7c0832ef76b09eab60813efb8d0cf24525f06b7b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-01 12:22:53 +0200

    fix: cursor attr name

--- modules/programs/code-cursor/default.nix
+++ modules/programs/code-cursor/default.nix
@@ -6,6 +6,6 @@
 }:
 util.mkProgram {
   name = "code-cursor";
-  hm.home.packages = lib.mkIf pkgs.stdenv.isLinux [ pkgs.cursoride ];
+  hm.home.packages = lib.mkIf pkgs.stdenv.isLinux [ pkgs.code-cursor ];
   system-darwin.homebrew.casks = [ "cursor" ];
 }
