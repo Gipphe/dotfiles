eefad1ac9465b69bcbaa81d493667e79c32d4429
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-01 12:17:52 +0200

    refactor: work implies work-slim

--- profiles/work.nix
+++ profiles/work.nix
@@ -1,14 +1,13 @@
 { util, ... }:
 util.mkProfile "work" {
-  gipphe.programs = {
-    cyberduck.enable = true;
-    google-cloud-sdk.enable = true;
-    idea-ultimate.enable = true;
-    kubectl.enable = true;
-    kubectx.enable = true;
-    neo4j-desktop.enable = true;
-    notion.enable = true;
-    openvpn-connect.enable = true;
-    slack.enable = true;
+  gipphe = {
+    profiles.work-slim.enable = true;
+    programs = {
+      cyberduck.enable = true;
+      neo4j-desktop.enable = true;
+      notion.enable = true;
+      openvpn-connect.enable = true;
+      slack.enable = true;
+    };
   };
 }
