4fd42b76e84909c6c0668b66dd89b89c3bef90ea
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-08 16:14:26 +0200

    feat: manage cursor config

--- modules/programs/code-cursor/default.nix
+++ modules/programs/code-cursor/default.nix
@@ -6,10 +6,14 @@
 }:
 util.mkProgram {
   name = "code-cursor";
-  hm.home.packages = lib.mkIf pkgs.stdenv.isLinux [
-    (pkgs.writeShellScriptBin "cursor" ''
-      ${pkgs.code-cursor}/bin/cursor "$@" &>/dev/null &
-    '')
-  ];
+  hm.home = {
+    file."Library/Application\ Support/Cursor/User/settings.json".source = lib.mkIf pkgs.stdenv.isDarwin ./config.json;
+    xdg.configFile."Cursor/User/settings.json".source = lib.mkIf pkgs.stdenv.isLinux ./config.json;
+    packages = lib.mkIf pkgs.stdenv.isLinux [
+      (pkgs.writeShellScriptBin "cursor" ''
+        ${pkgs.code-cursor}/bin/cursor "$@" &>/dev/null &
+      '')
+    ];
+  };
   system-darwin.homebrew.casks = [ "cursor" ];
 }
