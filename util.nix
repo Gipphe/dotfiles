616cc267af898751f5bdb0f888ba8a8965abc251
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-10 14:59:55 +0200

    fix: redundant run in mkCopyActivationScript

--- util.nix
+++ util.nix
@@ -34,8 +34,8 @@ let
     writeShellApplication {
       inherit name;
       text = ''
-        run rm -rf "${toDir}"
-        run cp -rL "${fromDir}" "${toDir}"
+        rm -rf "${toDir}"
+        cp -rL "${fromDir}" "${toDir}"
       '';
     };
 
