61b97ca2e518c57096ffa40684056e2bb0bec797
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-11 00:55:37 +0200

    fix: re-add `run` to zoxide activation script

--- windows/configs/.config/zoxide.ps1
+++ windows/configs/.config/zoxide.ps1
@@ -1,1 +1,1 @@
-echo "Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })" > "/home/gipphe/projects/dotfiles/windows/Config/zoxide.ps1"
+run echo "Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })" > "/home/gipphe/projects/dotfiles/windows/Config/zoxide.ps1"
