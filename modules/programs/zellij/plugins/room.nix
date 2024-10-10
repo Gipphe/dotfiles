9df5d87239310dfc847c0f3f085675ce95f82c1f
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-10 11:28:40 +0200

    feat: add room zellij plugin

---
+++ modules/programs/zellij/plugins/room.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, ... }:
+let
+  room = pkgs.fetchurl {
+    url = "https://github.com/rvcas/room/releases/download/v1.1.1/room.wasm";
+    hash = "sha256-wCGnvFaoaoyH6QFkIqaDj0j0lGe1DOAX4ZmUQOyT/eY=";
+  };
+  inherit (import ../helpers.nix { inherit lib; }) bind shared_except;
+in
+{
+  xdg.configFile."zellij/plugins/room.wasm".source = room;
+  programs.zellij.settings.keybinds = shared_except [ "locked" ] (
+    bind "Ctrl l" {
+      "LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/room.wasm\"" = {
+        floating = true;
+        ignore_case = true;
+        quick_jump = true;
+      };
+    }
+  );
+}
