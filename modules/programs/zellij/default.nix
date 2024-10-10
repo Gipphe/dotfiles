fa7d4d4f5b61471871ec616168b1257c281e516e
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-10 10:21:12 +0200

    refactor: manage zellij config in nix lang

--- modules/programs/zellij/default.nix
+++ modules/programs/zellij/default.nix
@@ -10,6 +10,14 @@ let
     url = "https://github.com/karimould/zellij-forgot/releases/download/0.3.0/zellij_forgot.wasm";
     hash = "sha256-JNQ4KXb6VzjSF0O4J8Tvq3FXUYBBabQb9ZitcR3kZFw=";
   };
+  unbind = keys: {
+    "unbind \"${keys}\"" = [ ];
+  };
+  bind = keys: opts: { "bind \"${keys}\"" = opts; };
+  section = section: opts: { "${section}" = opts; };
+  shared_except = modes: opts: {
+    "shared_except \"${lib.concatStringsSep "\" \"" modes}\"" = opts;
+  };
 in
 util.mkProgram {
   name = "zellij";
@@ -17,51 +25,47 @@ util.mkProgram {
   hm = {
     xdg.configFile = {
       "zellij/layouts".source = ./layouts;
-      "zellij/config.kdl".text = # kdl
-        ''
-          // copy_command "xclip -in -sel clip"
-          copy_on_select true
-          theme "catppuccin-macchiato"
-          keybinds {
-            unbind "Ctrl q"
-            unbind "Ctrl o"
-
-            move {
-              bind "Ctrl e" { SwitchToMode "Normal"; }
-            }
-            shared_except "move" "locked" {
-              bind "Ctrl e" { SwitchToMode "Move"; }
-            }
-
-            session {
-              bind "Ctrl x" { SwitchToMode "Normal"; }
-            }
-            shared_except "session" "locked" {
-              bind "Ctrl x" { SwitchToMode "Session"; }
-            }
-
-            shared_except "locked" {
-              bind "Ctrl h" { MoveFocusOrTab "Left"; }
-              bind "Ctrl l" { MoveFocusOrTab "Right"; }
-              bind "Ctrl j" { MoveFocus "Down"; }
-              bind "Ctrl k" { MoveFocus "Up"; }
-
-              bind "Ctrl y" {
-                LaunchOrFocusPlugin "file:${zj_forgot}" {
-                  floating true
-                }
-              }
-            }
-          }
-          ui {
-            pane_frames {
-              rounded_corners false
-            }
-          }
-        '';
     };
     programs = {
       zellij.enable = true;
+
+      settings = {
+        # copy_command = "xclip -in -sel clip";
+        copy_on_select = true;
+        theme = "catppuccin-macchiato";
+        ui = {
+          pane_frames = {
+            rounded_corners = false;
+          };
+        };
+        keybinds = lib.mergeAttrsList [
+          (section "move" (bind "Ctrl e" { SwitchToMode = "Normal"; }))
+          (unbind "Ctrl q")
+          (unbind "Ctrl o")
+          (shared_except [
+            "move"
+            "locked"
+          ] (bind "Ctrl e" { SwitchToMode = "Move"; }))
+          (section "session" (bind "Ctrl x" { SwitchToMode = "Normal"; }))
+          (shared_except [
+            "session"
+            "locked"
+          ] (bind "Ctrl x" { SwitchToMode = "Session"; }))
+          (shared_except [ "locked" ] (
+            lib.mergeAttrsList [
+              (bind "Ctrl h" { MoveFocusOrTab = "Left"; })
+              (bind "Ctrl l" { MoveFocusOrTab = "Right"; })
+              (bind "Ctrl j" { MoveFocusOrTab = "Down"; })
+              (bind "Ctrl k" { MoveFocusOrTab = "Up"; })
+              (bind "Ctrl y" {
+                "LaunchOrFocusPlugin \"file:${zj_forgot}\"" = {
+                  floating = true;
+                };
+              })
+            ]
+          ))
+        ];
+      };
 
       fish.shellAbbrs = {
         zq = "${lib.getExe config.programs.zellij.package} kill-session $ZELLIJ_SESSION_NAME";
