d3bf9145a691b8784b4e02cc3edfd71b19c5d03a
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-10 10:28:06 +0200

    fix: nesting

--- modules/programs/zellij/default.nix
+++ modules/programs/zellij/default.nix
@@ -26,44 +26,46 @@ util.mkProgram {
       "zellij/plugins/zj_forgot.wasm".source = zj_forgot;
     };
     programs = {
-      zellij.enable = true;
+      zellij = {
+        enable = true;
 
-      settings = {
-        # copy_command = "xclip -in -sel clip";
-        copy_on_select = true;
-        theme = "catppuccin-macchiato";
-        ui = {
-          pane_frames = {
-            rounded_corners = false;
+        settings = {
+          # copy_command = "xclip -in -sel clip";
+          copy_on_select = true;
+          theme = "catppuccin-macchiato";
+          ui = {
+            pane_frames = {
+              rounded_corners = false;
+            };
           };
+          keybinds = lib.mergeAttrsList [
+            (section "move" (bind "Ctrl e" { SwitchToMode = "Normal"; }))
+            (unbind "Ctrl q")
+            (unbind "Ctrl o")
+            (shared_except [
+              "move"
+              "locked"
+            ] (bind "Ctrl e" { SwitchToMode = "Move"; }))
+            (section "session" (bind "Ctrl x" { SwitchToMode = "Normal"; }))
+            (shared_except [
+              "session"
+              "locked"
+            ] (bind "Ctrl x" { SwitchToMode = "Session"; }))
+            (shared_except [ "locked" ] (
+              lib.mergeAttrsList [
+                (bind "Ctrl h" { MoveFocusOrTab = "Left"; })
+                (bind "Ctrl l" { MoveFocusOrTab = "Right"; })
+                (bind "Ctrl j" { MoveFocusOrTab = "Down"; })
+                (bind "Ctrl k" { MoveFocusOrTab = "Up"; })
+                (bind "Ctrl y" {
+                  "LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/zj_forgot.wasm\"" = {
+                    floating = true;
+                  };
+                })
+              ]
+            ))
+          ];
         };
-        keybinds = lib.mergeAttrsList [
-          (section "move" (bind "Ctrl e" { SwitchToMode = "Normal"; }))
-          (unbind "Ctrl q")
-          (unbind "Ctrl o")
-          (shared_except [
-            "move"
-            "locked"
-          ] (bind "Ctrl e" { SwitchToMode = "Move"; }))
-          (section "session" (bind "Ctrl x" { SwitchToMode = "Normal"; }))
-          (shared_except [
-            "session"
-            "locked"
-          ] (bind "Ctrl x" { SwitchToMode = "Session"; }))
-          (shared_except [ "locked" ] (
-            lib.mergeAttrsList [
-              (bind "Ctrl h" { MoveFocusOrTab = "Left"; })
-              (bind "Ctrl l" { MoveFocusOrTab = "Right"; })
-              (bind "Ctrl j" { MoveFocusOrTab = "Down"; })
-              (bind "Ctrl k" { MoveFocusOrTab = "Up"; })
-              (bind "Ctrl y" {
-                "LaunchOrFocusPlugin \"file:~/.config/zellij/plugins/zj_forgot.wasm\"" = {
-                  floating = true;
-                };
-              })
-            ]
-          ))
-        ];
       };
 
       fish.shellAbbrs = {
