0c05863c16d2e2798c863ff2fa29d9a64fb0fcbc
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-10 11:29:43 +0200

    fix: add hash for room

--- modules/programs/zellij/plugins/zj-status-bar.nix
+++ modules/programs/zellij/plugins/zj-status-bar.nix
@@ -1,9 +1,8 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 let
   zj-status-bar = pkgs.fetchurl {
     url = "https://github.com/cristiand391/zj-status-bar/releases/download/0.3.0/zj-status-bar.wasm";
-    hash = "";
-
+    hash = "sha256-seiWCtsrkFnDwXrXrAOE6y9EUWzpnb8qgHqRDdMKCeg=";
   };
 in
 {
