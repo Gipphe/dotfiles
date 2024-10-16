f83e646cc8c362897dfdba25074868420cd9abb4
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-16 16:21:48 +0300

    refactor: use Map App instead for Wireguard

--- modules/networking/wireguard/default.nix
+++ modules/networking/wireguard/default.nix
@@ -1,13 +1,5 @@
-{ util, pkgs, ... }:
+{ util, ... }:
 util.mkToggledModule [ "networking" ] {
   name = "wireguard";
-  hm = {
-    home.packages = [ pkgs.wireguard-tools ];
-    sops.secrets.VNB-MB-Pro-wireguard-key = {
-      sopsFile = ../../../secrets/VNB-MB-Pro-wireguard-key.key;
-      mode = "400";
-      format = "binary";
-    };
-  };
   system-darwin.homebrew.masApps.Wireguard = 1451685025;
 }
