21a536e4c07f84893449bfb729662a66741eea39
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-21 01:37:35 +0200

    feat: add avante.nvim

---
+++ modules/programs/nixvim/home-manager/plugins/avante.nix
@@ -0,0 +1,53 @@
+{ pkgs, config, ... }:
+{
+  programs = {
+    nixvim = {
+      extraPlugins = with pkgs.vimPlugins; [
+        nui-nvim
+        img-clip-nvim
+        plenary-nvim
+      ];
+      extraConfigLua = ''
+        require('img-clip').setup({
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        })
+      '';
+      plugins = {
+        avante = {
+          enable = true;
+          settings = {
+            provider = "copilot";
+            auto_suggestions_provider = "copilot";
+          };
+        };
+        copilot-lua.enable = true;
+        dressing.enable = true;
+        render-markdown = {
+          enable = true;
+          settings.file_types = [
+            "markdown"
+            "Avante"
+          ];
+        };
+      };
+    };
+    fish.shellInit = # fish
+      ''
+        set -gx ANTHROPIC_API_KEY "$(cat ${config.sops.secrets.anthropic_api_key.path})"
+      '';
+  };
+
+  sops.secrets.anthropic_api_key = {
+    sopsFile = ../../../../../secrets/claude-nvim-api-key.key;
+    mode = "400";
+    format = "binary";
+  };
+}
