207a7e0d04ca96ecc6ee351d27eb1eab317f901d
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-26 23:17:03 +0200

    fix: godot on darwin

--- modules/programs/godot/default.nix
+++ modules/programs/godot/default.nix
@@ -1,13 +1,22 @@
 {
+  inputs,
+  lib,
   pkgs,
   util,
   ...
 }:
 util.mkProgram {
   name = "godot";
-  hm = {
-    home.packages = [ pkgs.godot_4 ];
-    gipphe.windows.chocolatey.programs = [ "godot" ];
-    gipphe.programs.code-cursor.settings."godotTools.editorPath.godot4" = "godot";
-  };
+  hm = lib.mkMerge [
+    {
+      gipphe.windows.chocolatey.programs = [ "godot" ];
+      gipphe.programs.code-cursor.settings."godotTools.editorPath.godot4" = "godot";
+    }
+    (lib.mkIf (!pkgs.stdenv.isDarwin) {
+      home.packages = [ pkgs.godot_4 ];
+    })
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      home.packages = [ inputs.brew-nix.packages.${pkgs.system}.godot ];
+    })
+  ];
 }
