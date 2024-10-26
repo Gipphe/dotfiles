2507995fd79369cc0313e124ba92d414876eaaa9
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-26 23:46:50 +0200

    feat: add convenience script for godot

--- modules/programs/godot/default.nix
+++ modules/programs/godot/default.nix
@@ -1,22 +1,21 @@
 {
   inputs,
-  lib,
   pkgs,
   util,
   ...
 }:
+let
+  pkg = if pkgs.stdenv.isDarwin then inputs.brew-nix.packages.${pkgs.system}.godot else pkgs.godot_4;
+in
 util.mkProgram {
   name = "godot";
-  hm = lib.mkMerge [
-    {
-      gipphe.windows.chocolatey.programs = [ "godot" ];
-      gipphe.programs.code-cursor.settings."godotTools.editorPath.godot4" = "godot";
-    }
-    (lib.mkIf (!pkgs.stdenv.isDarwin) {
-      home.packages = [ pkgs.godot_4 ];
-    })
-    (lib.mkIf pkgs.stdenv.isDarwin {
-      home.packages = [ inputs.brew-nix.packages.${pkgs.system}.godot ];
-    })
-  ];
+  hm = {
+    gipphe.windows.chocolatey.programs = [ "godot" ];
+    gipphe.programs.code-cursor.settings."godotTools.editorPath.godot4" = "godot";
+    home.packages = [
+      (pkgs.writeShellScriptBin "godot" ''
+        ${pkg}/bin/godot "$@" &>/dev/null &
+      '')
+    ];
+  };
 }
