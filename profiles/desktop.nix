f513d9260008046b5c714ef7890be72bc444b02b
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-31 08:49:29 +0100

    feat: enable firefox for desktop profile

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -7,6 +7,7 @@ util.mkProfile "desktop" {
       code-cursor.enable = true;
       cool-retro-term.enable = true;
       filen.enable = true;
+      firefox.enable = true;
       gimp.enable = true;
       hoppscotch.enable = true;
       logseq.enable = true;
