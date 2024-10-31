5bd0b16378a907d42879b006372e74ec9576edc3
Victor Nascimento Bakke <gipphe@gmail.com>
2024-10-31 08:49:02 +0100

    refactor: move firefox bookmarks into separate files

---
+++ modules/programs/firefox/bookmarks/strise.nix
@@ -0,0 +1,32 @@
+[
+  {
+    name = "Strise";
+    toolbar = true;
+    bookmarks = [
+      {
+        name = "Strise Status";
+        url = "https://status.strise.ai/";
+      }
+      {
+        name = "Grafana";
+        url = "https://grafana.strise.ai/";
+      }
+      {
+        name = "Home - Elastic";
+        url = "http://kibana.log.strise.ai/app/home#/";
+      }
+      {
+        name = "Miro | Visual Workspace for Innovation";
+        url = "https://miro.com/app/dashboard/";
+      }
+      {
+        name = "Dashboard [Jenkins]";
+        url = "https://ci.strise.ai/";
+      }
+      {
+        name = "Welcome – strise-dev – Google Cloud console";
+        url = "https://console.cloud.google.com/welcome?project=strise-dev";
+      }
+    ];
+  }
+]
