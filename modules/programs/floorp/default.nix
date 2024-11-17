51cbccfa6102bdc2227bf0d6583df45de79690b9
Victor Nascimento Bakke <gipphe@gmail.com>
2024-11-17 23:20:57 +0100

    chore(windows): remove Floorp windows files

--- modules/programs/floorp/default.nix
+++ modules/programs/floorp/default.nix
@@ -106,33 +106,33 @@ util.mkModule {
         };
       };
       gipphe.windows = {
-        environment.variables."MOZ_LEGACY_PROFILES" = "1";
+        # environment.variables."MOZ_LEGACY_PROFILES" = "1";
         chocolatey.programs = [ "floorp" ];
-        home.file =
-          let
-            fs = config.home.file;
-            optionalFile =
-              pathTo: pathFrom:
-              if (builtins.hasAttr pathFrom fs) then
-                {
-                  ${pathTo}.source = (builtins.getAttr pathFrom fs).source;
-                }
-              else
-                { };
-          in
-          {
-            "AppData/Roaming/Floorp/profiles.ini".source =
-              pkgs.runCommandNoCC "floorp-profiles" { } # bash
-                ''
-                  cat "${fs.".floorp/profiles.ini".source}" >> $out
-                  echo "" >> $out
-                  echo "${windows.backgroundTasksProfile}" >> $out
-                '';
-          }
-          // optionalFile "AppData/Roaming/Floorp/Profiles/default/search.json.mozlz4" ".floorp/default/search.json.mozlz4"
-          // optionalFile "AppData/Roaming/Floorp/Profiles/default/user.js" ".floorp/default/user.js"
-          // optionalFile "AppData/Roaming/Floorp/Profiles/default/chrome/userContent.css" ".floorp/default/chrome/userContent.css"
-          // optionalFile "AppData/Roaming/Floorp/Profiles/default/chrome/userChrome.css" ".floorp/default/chrome/userChrome.css";
+        # home.file =
+        #   let
+        #     fs = config.home.file;
+        #     optionalFile =
+        #       pathTo: pathFrom:
+        #       if (builtins.hasAttr pathFrom fs) then
+        #         {
+        #           ${pathTo}.source = (builtins.getAttr pathFrom fs).source;
+        #         }
+        #       else
+        #         { };
+        #   in
+        #   {
+        #     "AppData/Roaming/Floorp/profiles.ini".source =
+        #       pkgs.runCommandNoCC "floorp-profiles" { } # bash
+        #         ''
+        #           cat "${fs.".floorp/profiles.ini".source}" >> $out
+        #           echo "" >> $out
+        #           echo "${windows.backgroundTasksProfile}" >> $out
+        #         '';
+        #   }
+        #   // optionalFile "AppData/Roaming/Floorp/Profiles/default/search.json.mozlz4" ".floorp/default/search.json.mozlz4"
+        #   // optionalFile "AppData/Roaming/Floorp/Profiles/default/user.js" ".floorp/default/user.js"
+        #   // optionalFile "AppData/Roaming/Floorp/Profiles/default/chrome/userContent.css" ".floorp/default/chrome/userContent.css"
+        #   // optionalFile "AppData/Roaming/Floorp/Profiles/default/chrome/userChrome.css" ".floorp/default/chrome/userChrome.css";
       };
     }
     (lib.mkIf (cfg.enable && pkgs.stdenv.isDarwin) {
