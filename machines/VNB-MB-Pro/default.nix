d78d437d18cbcbd8bc128921c2d195bcbfdb9395
Victor Nascimento Bakke <gipphe@gmail.com>
2024-11-18 07:57:24 +0100

    chore: remove desktop-darwin profile

--- machines/VNB-MB-Pro/default.nix
+++ machines/VNB-MB-Pro/default.nix
@@ -12,7 +12,6 @@ util.mkToggledModule [ "machines" ] {
       containers.enable = true;
       core.enable = true;
       darwin.enable = true;
-      desktop-darwin.enable = true;
       desktop.enable = true;
       game-dev.enable = true;
       home-vpn.enable = true;
