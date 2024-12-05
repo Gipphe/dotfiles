519b18319151b632d147e2ea6d3736c5d6d8e3a6
Victor Nascimento Bakke <gipphe@gmail.com>
2024-12-05 13:41:54 +0100

    feat: regenerate ssh key for silicon on gitlab

---
+++ secrets/generate-ssh-key.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env nix
+#! nix shell --file ``<nixpkgs>`` openssh sops coreutils --command bash
+
+log() {
+    echo "$@" >&2
+}
+
+help() {
+    log "Usage: generate-ssh-key.sh <key_name>"
+    log ""
+    log "Arguments:"
+    log "  key_name: string, required"
+}
+
+key_name="$1"
+if test -z "$key_name"; then
+    echo "Missing key_name arg".
+    help
+    exit 1
+fi
+
+temp_key=$(mktemp)
+echo "y" | ssh-keygen -t ed25519 -f "$temp_key" -N ''
+
+dir="$(dirname -- "${BASH_SOURCE[0]}")"
+
+dest="$dir/$key_name"
+sops "--filename-override=secrets/$key_name" -e /dev/stdin <"$temp_key" >"$dest"
+sops "--filename-override=secrets/$key_name.pub" -e /dev/stdin <"$temp_key.pub" >"$dest.pub"
+rm "$temp_key" "$temp_key.pub"
+log "Generated and encrypted key in $dest"
