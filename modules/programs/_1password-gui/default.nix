6eee1de7ee047136a1bd7d9b6776ea45b28d6cab
Victor Nascimento Bakke <gipphe@gmail.com>
2024-12-07 00:49:12 +0100

    feat: start up 1password with system

--- modules/programs/_1password-gui/default.nix
+++ modules/programs/_1password-gui/default.nix
@@ -7,5 +7,10 @@
 util.mkProgram {
   name = "_1password-gui";
   hm = lib.mkIf pkgs.stdenv.isLinux { home.packages = with pkgs; [ _1password-gui ]; };
+  system-nixos.systemd.user.services._1password-boot = {
+    wantedBy = [ "default.target" ];
+    description = "Start 1password in the background.";
+    serviceConfig.ExecStop = "${pkgs._1password-gui}/bin/1password --silent";
+  };
   system-darwin.homebrew.casks = [ "1password" ];
 }
