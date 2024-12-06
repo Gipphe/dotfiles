d73eac81ad40e15c83c97bcaa924ed429fdba102
Victor Nascimento Bakke <gipphe@gmail.com>
2024-12-06 21:58:39 +0100

    fix: nerdfonts split into separate separate packages

--- modules/environment/stylix/default.nix
+++ modules/environment/stylix/default.nix
@@ -21,7 +21,7 @@ util.mkEnvironment {
 
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+        package = pkgs.nerd-fonts.fira-code;
         name = "FiraCode Nerd Font Mono";
       };
       sansSerif = {
