805e6b0cab6415c6402798fc0002407644661767
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-14 02:40:29 +0100

    feat: swap nixvim to nvf

--- profiles/cli.nix
+++ profiles/cli.nix
@@ -38,7 +38,7 @@ util.mkProfile "cli" {
     make.enable = true;
     mprocs.enable = true;
     nh.enable = true;
-    nixvim.enable = true;
+    nvf.enable = true;
     procs.enable = true;
     pv.enable = true;
     ripgrep.enable = true;
