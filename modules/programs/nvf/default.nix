415ddf8afceb8effc99129b6fc85987c1ef72f4f
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-17 14:24:06 +0100

    tweak: set modes.nvim line_opacity in visual mode

--- modules/programs/nvf/default.nix
+++ modules/programs/nvf/default.nix
@@ -45,6 +45,7 @@ util.mkProgram {
             };
             illuminate.enable = true;
             modes-nvim.enable = true;
+            modes-nvim.setupOpts.line_opacity.visual = 0.5;
             noice.enable = true;
             nvim-ufo.enable = true;
           };
