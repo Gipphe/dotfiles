8bcef534142b78ee82d2a41da00a1ca9ef7bffcc
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-18 09:22:40 +0100

    chore(windows): remove explicit install of python
    Necessary for sd. This is automatically handled by sd now.

--- profiles/windows-setup.nix
+++ profiles/windows-setup.nix
@@ -44,8 +44,6 @@ util.mkProfile "windows-setup" {
           "paint.net"
           "powershell-core"
           "powertoys"
-          "python310"
-          "python312"
           "qbittorrent"
           "restic"
           "rsync"
