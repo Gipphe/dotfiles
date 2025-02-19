0a80f5a94ae5b4f79f400f26923ab0a8e106e72c
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-19 21:21:22 +0100

    feat: make openai-key a function instead of an abbreviation

--- modules/programs/fish/default.nix
+++ modules/programs/fish/default.nix
@@ -60,6 +60,9 @@ util.mkProgram {
                 set -U fish_color_autosuggestion 5b6078
               '';
           package = pkgs.fish;
+          functions = {
+            openai-key = "cat ${config.sops.secrets.milkyway-translations-openai-key.path}";
+          };
           shellAbbrs = {
             vim = "nvim";
             tf = "terraform";
@@ -74,7 +77,6 @@ util.mkProgram {
             # Taken from https://discourse.nixos.org/t/list-and-delete-nixos-generations/29637/6
             prune-gens = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/sytem --older-than";
             rm = "rm -i";
-            openai-key = "cat ${config.sops.secrets.milkyway-translations-openai-key.path}";
           };
           plugins = [
             {
