0cbcf46bfc7f374501e96fac7a44c7b8efd92d90
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-24 15:58:11 +0100

    tweak: add which-key registers

--- modules/programs/nvf/binds.nix
+++ modules/programs/nvf/binds.nix
@@ -1,6 +1,18 @@
+{ lib, ... }:
+let
+  prefixes = import ./mapping-prefixes.nix;
+in
 {
   programs.nvf.settings.vim.binds = {
     cheatsheet.enable = true;
     whichKey.enable = true;
+    whichKey.register = lib.pipe prefixes [
+      builtins.attrNames
+      (map (k: {
+        name = prefixes.${k}.prefix;
+        value = prefixes.${k}.description;
+      }))
+      builtins.listtoAttrs
+    ];
   };
 }
