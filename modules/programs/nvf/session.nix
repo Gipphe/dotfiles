99cac93f67730262e405c3421de7635470190b7a
Victor Nascimento Bakke <gipphe@gmail.com>
2025-02-27 00:59:11 +0100

    tweak: add mappings for session manager

--- modules/programs/nvf/session.nix
+++ modules/programs/nvf/session.nix
@@ -1,5 +1,17 @@
+let
+  inherit (import ./mapping-prefixes.nix) session;
+  k = x: "${session.prefix}${x}";
+in
 {
-  programs.nvf.settings.vim.session.nvim-session-manager = {
-    enable = true;
+  programs.nvf.settings.vim = {
+    session.nvim-session-manager = {
+      enable = true;
+      mappings = {
+        deleteSession = k "d";
+        loadLastSession = k "s";
+        loadSession = k "l";
+        saveCurrentSession = k "sc";
+      };
+    };
   };
 }
