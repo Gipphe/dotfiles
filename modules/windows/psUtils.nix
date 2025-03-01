85a02d986abf68bb2fa03e296a54b55869351170
Victor Nascimento Bakke <gipphe@gmail.com>
2025-03-01 13:01:04 +0100

    wip

--- modules/windows/psUtils.nix
+++ modules/windows/psUtils.nix
@@ -1,4 +1,8 @@
 { lib, util, ... }:
+let
+  inherit (lib) generators;
+  inherit (gnerators) toPretty;
+in
 util.mkModule {
   hm.gipphe.windows.powershell-script =
     lib.mkOrder (import ./order.nix).utils # powershell
@@ -115,17 +119,59 @@ util.mkModule {
           return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
         }
       '';
-  toPowershell = { multiline ? true, indent ? "", asBindings ? false, }@args: v: let
-  innerIndent = "${indent}  ";
-  introSpace = if multiline then "\n${innerIndent}" else " ";
-  outroSpace = if multiline then "\n${indent}" else " ";
-  innerArgs = args // {
-    indent = if asBindings then indent else innerIndent;
-    asBindings = false;
-  };
-  concatItems = concatStringsSep ",${introSpace}";
-  isPowershellInline = { _type ? null, ... }: _type = "powershell-inline";
+  toPowershell = {
+    multiline ? true,
+    indent ? "",
+    asBindings ? false,
+  }@args: v:
+    let
+      innerIndent = "${indent}  ";
+      introSpace = if multiline then "\n${innerIndent}" else " ";
+      outroSpace = if multiline then "\n${indent}" else " ";
+      innerArgs = args // {
+        indent = if asBindings then indent else innerIndent;
+        asBindings = false;
+      };
+      concatItems = concatStringsSep ",${introSpace}";
+      isPowershellInline = { _type ? null, ... }: _type = "powershell-inline";
 
-  generatedBindings = assert assertMsg
-  in
+      generatedBindings =
+        assert assertMsg (badVarNames == []) "Bad Powershell var names: ${toPretty {} badVarNames}";
+        assert assertMsg (collidingNames == []) "Name collisions in Powershell var names: ${toPretty {} (map (n: n.name) collidingNames)}";
+        concatStrings (
+          mapAttrsToList (key: value: "${indent}${key} = ${toPowershell innerArgs value}\n") v
+        );
+      matchVarName = match "[[:alpha:]_][[:alnum:]_]*(\\.[[:alpha:]_][[:alnum:]_]*)*";
+      badVarNames = filter (name: matchVarName name == null) (attrNames v);
+      loweredNames = map (name: { inherit name; lowered = toLower n; }) (attrNames v);
+      collidingNames = filter ({ name, lowered }: length (filter (x: x.lowered == lowered) loweredNames) > 1) loweredNames;
+    in
+    if asBindings then
+      generatedBindings
+    else if v == null then
+      "$null"
+    else if isInt v || isFloat v || isString v then
+      toJSON v
+    else if isBool v then
+      "$$${toJSON v}"
+    else if isList v then
+      if v = [] then
+        "@()"
+      else
+        "@(${introSpace}${concatItems (map (value: "${toPowershell innerArgs value}") v)}${outroSpace})"
+    else if isAttrs v then
+      if isPowershellInline v then
+        "(${v.expr})"
+      else if v == {} then
+        "@{}"
+      else if isDerivation v then
+        ''"${toString v}"''
+      else
+        "@{${introSpace}${concatItems (
+          mapAttrsToList (key: value: ''"${toJSON key}" = ${toPowershell innerArgs value}'') v
+        )}${outroSpace}}"
+    else
+      abort "toPowershell: type ${typeOf v} is unsupported";
+
+  mkPowershellInline = expr: { _type = "powershell-inline"; inherit expr; };
 }
