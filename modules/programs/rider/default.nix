d77efe97f2beef0e626a55e5b4efc775db90db47
Victor Nascimento Bakke <gipphe@gmail.com>
2025-03-03 20:18:05 +0100

    fix(windows): namespace for rider

--- modules/programs/rider/default.nix
+++ modules/programs/rider/default.nix
@@ -12,6 +12,6 @@ util.mkProgram {
       (lib.mkIf pkgs.stdenv.isDarwin inputs.brew-nix.packages.${pkgs.system}.rider)
       (lib.mkIf pkgs.stdenv.isLinux pkgs.jetbrains.rider)
     ];
-    gipphe.windows.chocolatey.programs = [ "jetbrains-rider" ];
+    gipphe.windows.profiles.titanium.chocolatey.programs = [ "jetbrains-rider" ];
   };
 }
