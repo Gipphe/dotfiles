7d94af9b838f28805300f908a5fc54a54a43ab6e
Victor Nascimento Bakke <gipphe@gmail.com>
2025-04-06 01:15:49 +0200

    fix: stylix issues on nix-on-droid

--- modules/environment/wallpaper/small-memory/default.nix
+++ modules/environment/wallpaper/small-memory/default.nix
@@ -13,12 +13,16 @@ util.mkWallpaper {
     type = lib.types.path;
     default = ./wallpaper/Macchiato-hald8-wall.png;
   };
-  hm = {
-    stylix.image = lib.mkIf (!flags.isNixOnDroid) config.environment.wallpaper.small-memory.image;
-    home.activation = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-      set-wallpaper = ''
-        run /usr/bin/automator -i "${./wallpaper/dynamic.heic}" "${./automator/set_desktop_wallpaper.workflow}" > /dev/null
-      '';
-    };
-  };
+  hm = lib.mkMerge [
+    (lib.optionalAttrs (pkgs.system != "aarch64-linux") {
+      stylix.image = config.environment.wallpaper.small-memory.image;
+    })
+    {
+      home.activation = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+        set-wallpaper = ''
+          run /usr/bin/automator -i "${./wallpaper/dynamic.heic}" "${./automator/set_desktop_wallpaper.workflow}" > /dev/null
+        '';
+      };
+    }
+  ];
 }
