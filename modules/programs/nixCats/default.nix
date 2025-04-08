5f1b396fbaabdeb34d5e63a646d03dfe9fb085ed
Victor Nascimento Bakke <gipphe@gmail.com>
2025-04-08 13:02:39 +0200

    fix: add missing EDITOR var

--- modules/programs/nixCats/default.nix
+++ modules/programs/nixCats/default.nix
@@ -9,15 +9,18 @@ util.mkProgram {
   name = "nixCats";
   hm = {
     imports = [ inputs.nixCats.homeModules.default ];
-    config.nvim = {
-      enable = true;
-      packageDefinitions.merge = {
-        categories.droid = flags.isNixOnDroid;
-        extra.nixd = {
-          nixpkgs = ''import ${pkgs.path} {}'';
-          nixos_options = ''(builtins.getFlake "${inputs.self}").nixosConfigurations.argon.options'';
-          darwin_options = ''(builtins.getFlake "${inputs.self}").darwinConfigurations.silicon.options'';
-          droid_options = ''(builtins.getFlake "${inputs.self}").nixOnDroidConfigurations.helium.options'';
+    config = {
+      home.sessionVariables.EDITOR = "nvim";
+      nvim = {
+        enable = true;
+        packageDefinitions.merge = {
+          categories.droid = flags.isNixOnDroid;
+          extra.nixd = {
+            nixpkgs = ''import ${pkgs.path} {}'';
+            nixos_options = ''(builtins.getFlake "${inputs.self}").nixosConfigurations.argon.options'';
+            darwin_options = ''(builtins.getFlake "${inputs.self}").darwinConfigurations.silicon.options'';
+            droid_options = ''(builtins.getFlake "${inputs.self}").nixOnDroidConfigurations.helium.options'';
+          };
         };
       };
     };
