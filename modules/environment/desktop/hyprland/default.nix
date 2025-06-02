a83f4c0957d65c112f788c9eeffd31d27651d2c1
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-02 20:14:34 +0200

    feat: initial Hyprland config

--- modules/environment/desktop/hyprland/default.nix
+++ modules/environment/desktop/hyprland/default.nix
@@ -1,4 +1,40 @@
-{ util, ... }:
+{ util, pkgs, ... }:
+let
+  grimblast = pkgs.makeExe pkgs.grimblast;
+  inherit (builtins) concatLists genList toString;
+
+  # sioodmy's implementation
+  workspaces = concatLists (
+    genList (
+      x:
+      let
+        ws =
+          let
+            c = (x + 1) / 10;
+          in
+          toString (x + 1 - (c * 10));
+      in
+      [
+        "$mod, ${ws}, workspace, ${toString (x + 1)}"
+        "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+      ]
+    )
+  );
+
+  # Hyprland docs implementation
+  workspaces' = concatLists (
+    genList (
+      i:
+      let
+        ws = i + 1;
+      in
+      [
+        "$mod, code:1${toString i}, workspace, ${toString ws}"
+        "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+      ]
+    )
+  );
+in
 util.mkToggledModule
   [
     "environment"
@@ -6,17 +42,32 @@ util.mkToggledModule
   ]
   {
     name = "hyprland";
-    hm.wayland.windowManager.hyprland = {
-      enable = true;
-    };
-    system-nixos = {
+    hm = {
+      programs.kitty.enable = true;
+      wayland.windowManager.hyprland = {
+        enable = true;
+        # settings = {
+        #   "$mod" = "Super";
+        #   bind = workspaces ++ [
+        #     "$mod, F, exec, floorp"
+        #     ", Print, exec, ${grimblast} copy area"
+        #   ];
+        # };
+        xwayland.enable = true;
+      };
+      home.sessionVariables.NIXOS_OZONE_WL = "1";
       programs = {
-        hyprland.enable = true;
         hyprlock.enable = true;
+        waybar.enable = true;
       };
       services = {
         hypridle.enable = true;
         hyprpaper.enable = true;
+        mako.enable = true;
       };
+    };
+    system-nixos = {
+      programs.hyprland.enable = true;
+      security.pam.services.hyprlock = { };
     };
   }
