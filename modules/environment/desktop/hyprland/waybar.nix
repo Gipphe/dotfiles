d0257c33ba865ed7f2242cd245f123093bf06f50
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-05 00:34:32 +0200

    fix: waybar issues

--- modules/environment/desktop/hyprland/waybar.nix
+++ modules/environment/desktop/hyprland/waybar.nix
@@ -5,13 +5,13 @@
   ...
 }:
 let
-  colors = lib.stylix.colors.withHashtag;
+  colors = config.lib.stylix.colors.withHashtag;
 in
 util.mkModule {
   hm.config = {
     programs.waybar = {
       enable = true;
-      settings = {
+      settings.mainBar = {
         "layer" = "top"; # Waybar at top layer
 
         "position" = "top"; # Waybar position (top|bottom|left|right)
@@ -42,236 +42,237 @@ util.mkModule {
           "tray"
           "custom/exit"
         ];
-        modules = {
-          # Workspaces
-          "hyprland/workspaces" = {
-            on-click = "activate";
-            active-only = false;
-            all-outputs = true;
-            format = "{}";
-            format-icons = {
-              urgent = "";
-              active = "";
-              default = "";
-            };
-            persistent-workspaces = {
-              "*" = 5;
-            };
-          };
 
-          # Hyprland Window
-          "hyprland/window" = {
-            rewrite = {
-              "(.*) - Brave" = "$1";
-              "(.*) - Chromium" = "$1";
-              "(.*) - Brave Search" = "$1";
-              "(.*) - Outlook" = "$1";
-              "(.*) Microsoft Teams" = "$1";
-            };
-            separate-outputs = true;
-          };
+        # Modules
 
-          # Rofi Application Launcher
-          "custom/appmenu" = {
-            format = "Apps";
-            tooltip-format = "Left: Open the application launcher\nRight: Show all keybindings";
-            on-click = "rofi -show drun -replace";
-            on-click-right = "~/.config/ml4w/scripts/keybindings.sh";
-            tooltip = false;
+        # Workspaces
+        "hyprland/workspaces" = {
+          on-click = "activate";
+          active-only = false;
+          all-outputs = true;
+          format = "{}";
+          format-icons = {
+            urgent = "";
+            active = "";
+            default = "";
           };
-
-          # Power Menu
-          "custom/exit" = {
-            format = "";
-            tooltip-format = "Powermenu";
-            on-click = "wlogout -b 4";
-            tooltip = false;
+          persistent-workspaces = {
+            "*" = 5;
           };
+        };
 
-          # Keyboard State
-          keyboard-state = {
-            numlock = true;
-            capslock = true;
-            format = "{name} {icon}";
-            format-icons = {
-              locked = "";
-              unlocked = "";
-            };
+        # Hyprland Window
+        "hyprland/window" = {
+          rewrite = {
+            "(.*) - Brave" = "$1";
+            "(.*) - Chromium" = "$1";
+            "(.*) - Brave Search" = "$1";
+            "(.*) - Outlook" = "$1";
+            "(.*) Microsoft Teams" = "$1";
           };
+          separate-outputs = true;
+        };
 
-          # System tray
-          tray = {
-            # icon-size= 21;
-            spacing = 10;
+        # Rofi Application Launcher
+        "custom/appmenu" = {
+          format = "Apps";
+          tooltip-format = "Left: Open the application launcher\nRight: Show all keybindings";
+          on-click = "rofi -show drun -replace";
+          on-click-right = "~/.config/ml4w/scripts/keybindings.sh";
+          tooltip = false;
+        };
+
+        # Power Menu
+        "custom/exit" = {
+          format = "";
+          tooltip-format = "Powermenu";
+          on-click = "wlogout -b 4";
+          tooltip = false;
+        };
+
+        # Keyboard State
+        keyboard-state = {
+          numlock = true;
+          capslock = true;
+          format = "{name} {icon}";
+          format-icons = {
+            locked = "";
+            unlocked = "";
           };
+        };
 
-          # Clock
-          clock = {
-            # timezone= "America/New_York";
-            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-            format-alt = "{:%Y-%m-%d}";
+        # System tray
+        tray = {
+          # icon-size= 21;
+          spacing = 10;
+        };
+
+        # Clock
+        clock = {
+          # timezone= "America/New_York";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          format-alt = "{:%Y-%m-%d}";
+        };
+
+        # System
+        "custom/system" = {
+          format = "";
+          tooltip = false;
+        };
+
+        # CPU
+        cpu = {
+          format = "/ C {usage}% ";
+          on-click = "alacritty -e htop";
+        };
+
+        # Memory
+        memory = {
+          format = "/ M {}% ";
+          on-click = "alacritty -e htop";
+        };
+
+        # Harddisc space used
+        disk = {
+          interval = 30;
+          format = "D {percentage_used}% ";
+          path = "/";
+          on-click = "alacritty -e htop";
+        };
+
+        "hyprland/language" = {
+          format = "/ K {short}";
+        };
+
+        # Group Hardware
+        "group/hardware" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 300;
+            children-class = "not-memory";
+            transition-left-to-right = false;
           };
+          modules = [
+            "custom/system"
+            "disk"
+            "cpu"
+            "memory"
+            "hyprland/language"
+          ];
+        };
 
-          # System
-          "custom/system" = {
-            format = "";
-            tooltip = false;
+        # Network
+        network = {
+          format = "{ifname}";
+          format-wifi = "   {signalStrength}%";
+          format-ethernet = "  {ipaddr}";
+          format-disconnected = "Not connected"; # An empty format will hide the module.
+          tooltip-format = " {ifname} via {gwaddri}";
+          tooltip-format-wifi = "   {essid} ({signalStrength}%)";
+          tooltip-format-ethernet = "  {ifname} ({ipaddr}/{cidr})";
+          tooltip-format-disconnected = "Disconnected";
+          max-length = 50;
+          on-click = "alacritty -e nmtui";
+        };
+
+        # Battery
+        battery = {
+          states = {
+            # good= 95;
+            warning = 30;
+            critical = 15;
           };
+          format = "{icon}   {capacity}%";
+          format-charging = "  {capacity}%";
+          format-plugged = "  {capacity}%";
+          format-alt = "{icon}  {time}";
+          # format-good= ""; // An empty format will hide the module
+          # format-full= "";
+          format-icons = [
+            " "
+            " "
+            " "
+            " "
+            " "
+          ];
+        };
 
-          # CPU
-          cpu = {
-            format = "/ C {usage}% ";
-            on-click = "alacritty -e htop";
-          };
-
-          # Memory
-          memory = {
-            format = "/ M {}% ";
-            on-click = "alacritty -e htop";
-          };
-
-          # Harddisc space used
-          disk = {
-            interval = 30;
-            format = "D {percentage_used}% ";
-            path = "/";
-            on-click = "alacritty -e htop";
-          };
-
-          "hyprland/language" = {
-            format = "/ K {short}";
-          };
-
-          # Group Hardware
-          "group/hardware" = {
-            orientation = "inherit";
-            drawer = {
-              transition-duration = 300;
-              children-class = "not-memory";
-              transition-left-to-right = false;
-            };
-            modules = [
-              "custom/system"
-              "disk"
-              "cpu"
-              "memory"
-              "hyprland/language"
+        # Pulseaudio
+        pulseaudio = {
+          # "scroll-step"= 1; // %, can be a float
+          format = "{icon}  {volume}%";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [
+              ""
+              " "
+              " "
             ];
           };
+          on-click = "pavucontrol";
+        };
 
-          # Network
-          network = {
-            format = "{ifname}";
-            format-wifi = "   {signalStrength}%";
-            format-ethernet = "  {ipaddr}";
-            format-disconnected = "Not connected"; # An empty format will hide the module.
-            tooltip-format = " {ifname} via {gwaddri}";
-            tooltip-format-wifi = "   {essid} ({signalStrength}%)";
-            tooltip-format-ethernet = "  {ifname} ({ipaddr}/{cidr})";
-            tooltip-format-disconnected = "Disconnected";
-            max-length = 50;
-            on-click = "alacritty -e nmtui";
-          };
+        # Bluetooth
+        bluetooth = {
+          format-disabled = "";
+          format-off = "";
+          interval = 30;
+          on-click = "blueman-manager";
+          format-no-controller = "";
+        };
 
-          # Battery
-          battery = {
-            states = {
-              # good= 95;
-              warning = 30;
-              critical = 15;
-            };
-            format = "{icon}   {capacity}%";
-            format-charging = "  {capacity}%";
-            format-plugged = "  {capacity}%";
-            format-alt = "{icon}  {time}";
-            # format-good= ""; // An empty format will hide the module
-            # format-full= "";
-            format-icons = [
-              " "
-              " "
-              " "
-              " "
-              " "
-            ];
-          };
+        # Other
+        user = {
+          format = "{user}";
+          interval = 60;
+          icon = false;
+        };
 
-          # Pulseaudio
-          pulseaudio = {
-            # "scroll-step"= 1; // %, can be a float
-            format = "{icon}  {volume}%";
-            format-bluetooth = "{volume}% {icon} {format_source}";
-            format-bluetooth-muted = " {icon} {format_source}";
-            format-muted = " {format_source}";
-            format-source = "{volume}% ";
-            format-source-muted = "";
-            format-icons = {
-              headphone = "";
-              hands-free = "";
-              headset = "";
-              phone = "";
-              portable = "";
-              car = "";
-              default = [
-                ""
-                " "
-                " "
-              ];
-            };
-            on-click = "pavucontrol";
+        # Idle Inhibator
+        idle_inhibitor = {
+          format = "{icon}";
+          tooltip = true;
+          format-icons = {
+            activated = "";
+            deactivated = "";
           };
+          on-click-right = "hyprlock";
+        };
 
-          # Bluetooth
-          bluetooth = {
-            format-disabled = "";
-            format-off = "";
-            interval = 30;
-            on-click = "blueman-manager";
-            format-no-controller = "";
-          };
-
-          # Other
-          user = {
-            format = "{user}";
-            interval = 60;
-            icon = false;
-          };
-
-          # Idle Inhibator
-          idle_inhibitor = {
-            format = "{icon}";
-            tooltip = true;
-            format-icons = {
-              activated = "";
-              deactivated = "";
-            };
-            on-click-right = "hyprlock";
-          };
-
-          "custom/quicklink1" = {
-            format = " ";
-            on-click = "~/.config/ml4w/apps/ML4W_Hyprland_Settings-x86_64.AppImage";
-            tooltip-format = "Open Hyprland Settings";
-          };
-          "custom/quicklink2" = {
-            format = " ";
-            on-click = "~/.config/ml4w/settings/browser.sh";
-            tooltip-format = "Open the browser";
-          };
-          "custom/quicklink3" = {
-            format = " ";
-            on-click = "~/.config/ml4w/settings/filemanager.sh";
-            tooltip-format = "Open the filemanager";
-          };
-          "custom/quicklinkempty" = { };
-          "group/quicklinks" = {
-            orientation = "horizontal";
-            modules = [
-              "custom/quicklink1"
-              "custom/quicklink2"
-              "custom/quicklink3"
-              "custom/quicklinkempty"
-            ];
-          };
+        "custom/quicklink1" = {
+          format = " ";
+          on-click = "~/.config/ml4w/apps/ML4W_Hyprland_Settings-x86_64.AppImage";
+          tooltip-format = "Open Hyprland Settings";
+        };
+        "custom/quicklink2" = {
+          format = " ";
+          on-click = "~/.config/ml4w/settings/browser.sh";
+          tooltip-format = "Open the browser";
+        };
+        "custom/quicklink3" = {
+          format = " ";
+          on-click = "~/.config/ml4w/settings/filemanager.sh";
+          tooltip-format = "Open the filemanager";
+        };
+        "custom/quicklinkempty" = { };
+        "group/quicklinks" = {
+          orientation = "horizontal";
+          modules = [
+            "custom/quicklink1"
+            "custom/quicklink2"
+            "custom/quicklink3"
+            "custom/quicklinkempty"
+          ];
         };
       };
       style = ''
