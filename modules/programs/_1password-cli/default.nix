bf824d415bda62d46b73811f4fd57c5b8ae3a4e5
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-05 01:38:32 +0200

    feat: add pgpg shell abbreviation

--- modules/programs/_1password-cli/default.nix
+++ modules/programs/_1password-cli/default.nix
@@ -1,5 +1,8 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "_1password-cli";
-  hm.home.packages = with pkgs; [ _1password-cli ];
+  hm = {
+    home.packages = with pkgs; [ _1password-cli ];
+    programs.fish.shellAbbrs.pgpg = "op item get \"PGP gipphe@gmail.com\" --field label=password --reveal | wl-copy --primary --trim-newline";
+  };
 }
