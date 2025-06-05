1df4162c4cf82030d67f0b499288af3cd651ea8f
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-05 15:17:32 +0200

    feat: add env vars for wayland compatibility

--- modules/environment/desktop/hyprland/default.nix
+++ modules/environment/desktop/hyprland/default.nix
@@ -80,35 +80,33 @@ util.mkToggledModule
             "$mod CTRL, RETURN, exec, rofi -show drun" # Open rofi
           ];
 
-          # Cursor
-          # env = XCURSOR_SIZE,24
+          env = [
+            # Cursor
+            "XCURSOR_SIZE,24"
 
-          # Environments
+            # XDG Desktop Portal
+            "XDG_CURRENT_DESKTOP,Hyprland"
+            "XDG_SESSION_TYPE,wayland"
+            "XDG_SESSION_DESKTOP,Hyprland"
 
-          # XDG Desktop Portal
-          # env = XDG_CURRENT_DESKTOP,Hyprland
-          # env = XDG_SESSION_TYPE,wayland
-          # env = XDG_SESSION_DESKTOP,Hyprland
+            # QT
+            "QT_QPA_PLATFORM,wayland;xcb"
+            "QT_QPA_PLATFORMTHEME,qt6ct"
+            "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+            "QT_AUTO_SCREEN_SCALE_FACTOR,1"
 
-          # QT
-          # env = QT_QPA_PLATFORM,wayland;xcb
-          # env = QT_QPA_PLATFORMTHEME,qt6ct
-          # env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-          # env = QT_AUTO_SCREEN_SCALE_FACTOR,1
-          #
-          # # GTK
-          # env = GDK_SCALE,1
-          #
-          # # Mozilla
-          # env = MOZ_ENABLE_WAYLAND,1
-          #
-          # # Disable appimage launcher by default
-          # env = APPIMAGELAUNCHER_DISABLE,1
-          #
-          # # OZONE
-          # env = OZONE_PLATFORM,wayland
+            # GTK
+            "GDK_SCALE,1"
 
-          # Input
+            # Mozilla
+            "MOZ_ENABLE_WAYLAND,1"
+
+            # Disable appimage launcher by default
+            "APPIMAGELAUNCHER_DISABLE,1"
+
+            # OZONE
+            "OZONE_PLATFORM,wayland"
+          ];
 
           input = {
             kb_layout = "no";
