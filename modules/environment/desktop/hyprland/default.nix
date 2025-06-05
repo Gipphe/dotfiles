339af8f37f97793f2371302736bd5f1f9d2bb4b7
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-05 15:31:27 +0200

    fix: leave colors to stylix

--- modules/environment/desktop/hyprland/default.nix
+++ modules/environment/desktop/hyprland/default.nix
@@ -146,7 +146,7 @@ util.mkToggledModule
               enabled = true;
               range = 4;
               render_power = 3;
-              color = "rgba(1a1a1aee)";
+              # color = "rgba(1a1a1aee)";
             };
           };
 
