75c4f92e2b75e69a6c19fba9c1ac1db3096ff40b
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-05 20:49:37 +0200

    feat: add config for mako

---
+++ modules/environment/desktop/hyprland/mako.nix
@@ -0,0 +1,14 @@
+{ util, ... }:
+util.mkToggledModule [ "environment" "desktop" "hyprland" ] {
+  name = "mako";
+  hm.programs.mako = {
+    enable = true;
+    settings = {
+      # font = "Fira Sans Semibold";
+      border-radius = 8;
+      padding = 8;
+      border-size = 5;
+      default-timeout = 4000;
+    };
+  };
+}
