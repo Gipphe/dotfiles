657ee6acb4ef8f72f464408845691e28045cb6ef
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-07 12:04:16 +0200

    tweak(waybar): reduce space between battery icon and battery level

--- modules/environment/desktop/hyprland/waybar.nix
+++ modules/environment/desktop/hyprland/waybar.nix
@@ -184,7 +184,7 @@ util.mkToggledModule [ "environment" "desktop" "hyprland" ] {
             warning = 30;
             critical = 15;
           };
-          format = "{icon}   {capacity}%";
+          format = "{icon}  {capacity}%";
           format-charging = "󰂄  {capacity}%";
           format-plugged = "  {capacity}%";
           format-alt = "{icon}  {time}";
