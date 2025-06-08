75a75c4a4d76222872f9745375bcac13d5a15f6c
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-08 12:46:33 +0200

    feat: start 1password on boot

--- modules/programs/_1password-gui/default.nix
+++ modules/programs/_1password-gui/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   util,
@@ -11,11 +12,27 @@ let
 in
 util.mkProgram {
   name = "_1password-gui";
+  options.gipphe.programs._1password-gui.startOnBoot =
+    lib.mkEnableOption "start 1password on boot"
+    // {
+      default = pkgs.hostPlatform.isLinux;
+    };
   hm = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
     home.packages = with pkgs; [ _1password-gui ];
     wayland.windowManager.hyprland.settings.bind = [
       "CONTROL SHIFT,SPACE,exec,${quick-access}/bin/1password-quick-access"
     ];
+    systemd.services.boot-start-1password = {
+      Unit = {
+        Description = "Start 1password in silent mode on boot";
+      };
+      Service = {
+        ExecStart = "${pkgs.writeShellScriptBin "1password-boot" ''
+          ${pkgs._1password-gui}/bin/1password --silent"
+        ''}/bin/1password-boot";
+      };
+      Install.wantedBy = "multi-user.target";
+    };
   };
   system-nixos.systemd.user.services._1password-boot = {
     wantedBy = [ "default.target" ];
