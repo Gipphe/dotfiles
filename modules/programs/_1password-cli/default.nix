a22c296b9611095954f68fb958b35ddd9f1bc11d
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-08 16:03:57 +0200

    fix(1password-cli): pgp pw function and other convenience functions

--- modules/programs/_1password-cli/default.nix
+++ modules/programs/_1password-cli/default.nix
@@ -1,8 +1,32 @@
 { util, pkgs, ... }:
+let
+  _1p = pkgs._1password-cli;
+  user_id = "ZRLJD6BOQZFFNF67FTJDPPJUE";
+in
 util.mkProgram {
   name = "_1password-cli";
   hm = {
-    home.packages = with pkgs; [ _1password-cli ];
-    programs.fish.shellAbbrs.pgpg = "op item get \"PGP gipphe@gmail.com\" --field label=password --reveal | wl-copy --primary --trim-newline";
+    home.packages = [ _1p ];
+    programs.fish = {
+      functions = {
+        opon = # fish
+          ''
+            if test -z "$OP_SESSION_${user_id}"
+              eval $(op signin)
+            end
+          '';
+        opoff = # fish
+          ''
+            op signout
+            set -e OP_SESSION_${user_id}
+          '';
+        pgppw = # fish
+          ''
+            op item get "PGP gipphe@gmail.com" --field label=password --reveal \
+            | wl-copy --primary --trim-newline
+          '';
+      };
+    };
+
   };
 }
