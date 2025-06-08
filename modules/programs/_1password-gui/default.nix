2fe0d8c1f76c3b14fd87f86179a01b29fbb41efe
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-08 16:02:43 +0200

    fix(1password): boot systemd service

--- modules/programs/_1password-gui/default.nix
+++ modules/programs/_1password-gui/default.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   pkgs,
   util,
@@ -15,29 +14,28 @@ util.mkProgram {
   options.gipphe.programs._1password-gui.startOnBoot =
     lib.mkEnableOption "start 1password on boot"
     // {
-      default = pkgs.hostPlatform.isLinux;
+      default = pkgs.stdenv.hostPlatform.isLinux;
     };
+
   hm = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
     home.packages = with pkgs; [ _1password-gui ];
+
     wayland.windowManager.hyprland.settings.bind = [
       "CONTROL SHIFT,SPACE,exec,${quick-access}/bin/1password-quick-access"
     ];
-    systemd.services.boot-start-1password = {
+
+    systemd.user.services.boot-start-1password = {
       Unit = {
         Description = "Start 1password in silent mode on boot";
       };
       Service = {
         ExecStart = "${pkgs.writeShellScriptBin "1password-boot" ''
-          ${pkgs._1password-gui}/bin/1password --silent"
+          ${pkgs._1password-gui}/bin/1password --silent
         ''}/bin/1password-boot";
       };
       Install.wantedBy = "multi-user.target";
     };
   };
-  system-nixos.systemd.user.services._1password-boot = {
-    wantedBy = [ "default.target" ];
-    description = "Start 1password in the background.";
-    serviceConfig.ExecStop = "${pkgs._1password-gui}/bin/1password --silent";
-  };
+
   system-darwin.homebrew.casks = [ "1password" ];
 }
