5f5a91b7b77cfedb25635d5597bf9859fd46ba84
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-09 13:26:48 +0200

    fix(rofi): hotkey collision with pseudotiling mode for 1password plugin

--- modules/environment/desktop/hyprland/rofi/1password.nix
+++ modules/environment/desktop/hyprland/rofi/1password.nix
@@ -25,7 +25,7 @@ in
       rofi-1p
     ];
     wayland.windowManager.hyprland.settings.bind = [
-      "$mod, P, exec, ${rofi-1p}/bin/rofi-1pass"
+      "$mod SHIFT, P, exec, ${rofi-1p}/bin/rofi-1pass"
     ];
   };
 }
