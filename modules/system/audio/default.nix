dcdb9f8d4ba826484a767107b8d703629bffb095
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-09 21:08:24 +0200

    fix: attempt to fix audio

--- modules/system/audio/default.nix
+++ modules/system/audio/default.nix
@@ -5,18 +5,31 @@ util.mkToggledModule [ "system" ] {
     # Enable sound with pipewire.
     security.rtkit.enable = true;
     services = {
-      pulseaudio.enable = false;
       pipewire = {
         enable = true;
+        audio.enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
         wireplumber.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
+        wireplumber.extraConfig.bluetoothEnhancement."monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+          ];
+        };
+        extraConfig.pipewire."92-low-latency"."context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 32;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 32;
+        };
       };
     };
   };
