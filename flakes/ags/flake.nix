76ebffd872bfdd6453295f66296ce1ada7230108
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-15 08:20:35 +0200

    wip: ags

--- flakes/ags/flake.nix
+++ flakes/ags/flake.nix
@@ -7,25 +7,69 @@
     };
   };
   outputs =
-    { nixpkgs, ags, ... }:
+    {
+      self,
+      nixpkgs,
+      ags,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       # additional libraries and packages to add to gjs' runtime
-      extraPackages = [
+      extraPackages = with ags.packages.${system}; [
         # ags.packages.${system}.battery
         # pkgs.fzf
+        battery
+        hyprland
+        mpris
+        network
+        tray
+        wireplumber
       ];
     in
     {
-      packages.${system}.default = ags.lib.bundle {
-        inherit pkgs;
-        src = ./.;
-        name = "giphtshell";
-        entry = "app.ts";
-        gtk4 = false;
-        inherit extraPackages;
-      };
+      homeManagerModules.${system}.default =
+        {
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          options.programs.ags = {
+            enable = lib.mkEnableOption "giphtbar (ags bar)";
+          };
+          config = lib.mkIf config.programs.ags.enable {
+            home.packages = [
+              pkgs.upower
+              self.packages.${system}.default
+            ];
+          };
+        };
+      packages.${system}.default =
+        ags.lib.bundle
+          {
+            inherit pkgs;
+            src = ./.;
+            name = "giphtshell";
+            entry = "app.ts";
+            gtk4 = false;
+            inherit extraPackages;
+          }
+          .override
+          (
+            prevAttrs:
+            prevAttrs
+            // {
+              checkPhase = ''
+                ${prevAttrs.checkPhase}
+                if command -v 'upower' &>/dev/null; then
+                  
+                fi
+              '';
+            }
+          );
       devShells.${system}.default = pkgs.mkShell {
         packages = [
           (ags.packages.${system}.default.override {
