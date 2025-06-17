0167ee435f411ab936b011c73f5412016974247c
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-17 12:48:18 +0200

    fix: reencrypt script makes all secrets empty

--- secrets/reencrypt.sh
+++ secrets/reencrypt.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env nix
-#! nix --extra-experimental-features ``flakes nix-command`` shell nixpkgs#parallel nixpkgs#sops nixpkgs#coreutils --command bash
+#! nix --extra-experimental-features ``flakes nix-command`` shell nixpkgs#sops nixpkgs#coreutils --command bash
 
 process_file() {
-    tmp=$(mktemp)
-    sops decrypt "$1" | sops encryupt --filename-override "$1" >"$tmp"
-    mv "$tmp" "$1"
+    echo "Processing $1"
+    backup=$(mktemp)
+    cp "$1" "$backup"
+
+    if sops decrypt --in-place "$1" 2>/dev/null && sops encrypt --in-place "$1" 2>/dev/null; then
+        echo "Re-encrypted $1"
+        rm -f "$backup"
+    else
+        echo "Unable to re-encrypt $1, skipping" >&2
+        mv "$backup" "$1"
+    fi
 }
 export -f process_file
 
 find . -type f ! \( -iname '*.nix' -o -iname '*.sh' \) -print0 |
-    parallel -0 process_file
+    while IFS= read -r -d '' file; do
+        process_file "$file"
+    done
