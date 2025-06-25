d8af85ea4c3f541c50d9ae7ea02b168ef6fc1e9e
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-25 23:02:36 +0200

    fix: incorrect placement of networkmanagerapplet systemd config

--- modules/programs/networkmanagerapplet/default.nix
+++ modules/programs/networkmanagerapplet/default.nix
@@ -1,16 +1,18 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "networkmanagerapplet";
-  hm.home.packages = [ pkgs.networkmanagerapplet ];
-  systemd.user.services.networkmanagerapplet = {
-    Unit = {
-      description = "Networkmanager applet";
-      PartOf = "graphical-session.target";
+  hm = {
+    home.packages = [ pkgs.networkmanagerapplet ];
+    systemd.user.services.networkmanagerapplet = {
+      Unit = {
+        description = "Networkmanager applet";
+        PartOf = "graphical-session.target";
+      };
+      Service = {
+        ExecStart = "${pkgs.networkmanagerapplet}/bin/nm-applet";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
     };
-    Service = {
-      ExecStart = "${pkgs.networkmanagerapplet}/bin/nm-applet";
-      Restart = "on-failure";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
