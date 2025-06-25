cf8cd0e1689cd0c8ab368c36e12869779e8b7362
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-25 15:13:01 +0200

    feat: add shell abbr for vpn

--- modules/programs/openconnect/default.nix
+++ modules/programs/openconnect/default.nix
@@ -1,9 +1,12 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "openconnect";
-  hm.home.packages = with pkgs; [
-    openconnect
-    networkmanager-openconnect
-    openconnect_gnutls
-  ];
+  hm = {
+    home.packages = with pkgs; [
+      openconnect
+      networkmanager-openconnect
+      openconnect_gnutls
+    ];
+    programs.fish.shellAbbrs.vpn = "op item get Lovdata --fields label=password --reveal | openconnect --passwd-on-stdin --user 'vnb@lovdata.no' --protocol anyconnect lovdataazure.ivpn.se";
+  };
 }
