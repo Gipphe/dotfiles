b5ddee6e7db8d034fd49b6548ae01dbf85502b3c
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-25 14:25:18 +0200

    fix: mkSystem does not exist

--- modules/system/devices/default.nix
+++ modules/system/devices/default.nix
@@ -7,7 +7,7 @@
 let
   cfg = config.gipphe.system.devices;
 in
-util.mkSystem {
+util.mkToggledModule [ "system" ] {
   name = "devices";
   options.gipphe.system.devices.logitech.enable = lib.mkEnableOption "logitech unifying defices";
   system-nixos.hardware.logitech.wireless = cfg.logitech.enable;
