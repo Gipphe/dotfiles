1144b7f3be9cbeb9035708a4b45dc548ae227249
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-28 19:06:42 +0200

    tweak(hyprland): reduce gaps

--- modules/environment/desktop/hyprland/default.nix
+++ modules/environment/desktop/hyprland/default.nix
@@ -159,8 +159,8 @@ util.mkToggledModule
           };
 
           general = {
-            gaps_in = 5;
-            gaps_out = 5;
+            gaps_in = 2;
+            gaps_out = 2;
             border_size = 1;
             # "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
             # "col.inactive_border" = "rgba(595959aa)";
