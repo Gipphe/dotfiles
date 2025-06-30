592e537a8896ff9559c332a180582a0389462c37
Victor Nascimento Bakke <gipphe@gmail.com>
2025-06-30 16:11:07 +0200

    refactor(eww): move mode-toggle script to mode-toggle module

--- modules/environment/desktop/hyprland/eww/eww/modules/mode-toggle/theme.fish
+++ modules/environment/desktop/hyprland/eww/eww/modules/mode-toggle/theme.fish
@@ -2,12 +2,13 @@
 
 set -l theme_file "$HOME/.cache/gipphe/theme"
 set -l dir $(dirname -- "$(status filename)")
+set -l color_dir = "$dir/../../colors"
 
 mkdir -p $(dirname -- $theme_file)
 if test -e "$theme_file" && test "$(cat "$theme_file")" = dark
-    cp -f "$dir/../colors/light.scss" "$dir/../colors/current.scss"
+    cp -f "$color_dir/light.scss" "$color_dir/current.scss"
     echo light >"$theme_file"
 else
-    cp -f "$dir/../colors/dark.scss" "$dir/../colors/current.scss"
+    cp -f "$color_dir/dark.scss" "$color_dir/current.scss"
     echo dark >"$theme_file"
 end
