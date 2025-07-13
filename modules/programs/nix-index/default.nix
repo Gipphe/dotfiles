145ba463a43123881cac20236142c15e57ea0e22
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-14 00:28:35 +0200

    chore: update nix-index homeModules set

--- modules/programs/nix-index/default.nix
+++ modules/programs/nix-index/default.nix
@@ -1,7 +1,7 @@
 { util, inputs, ... }:
 util.mkModule {
   hm = {
-    imports = [ inputs.nix-index-db.hmModules.nix-index ];
+    imports = [ inputs.nix-index-db.homeModules.nix-index ];
     programs.nix-index-database.comma.enable = true;
   };
 }
