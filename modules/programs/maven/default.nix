0dfdb56fea551bdd00e04b0965ca1a2ff10c5414
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-22 13:34:26 +0200

    feat: add maven master password

--- modules/programs/maven/default.nix
+++ modules/programs/maven/default.nix
@@ -1,5 +1,18 @@
-{ util, pkgs, ... }:
+{
+  util,
+  pkgs,
+  config,
+  ...
+}:
 util.mkProgram {
   name = "maven";
-  hm.home.packages = [ pkgs.maven ];
+  hm = {
+    home.packages = [ pkgs.maven ];
+    sops.secrets.lovdata-maven-settings-security = {
+      format = "binary";
+      sopsFile = ../../../secrets/utv-vnb-lt-maven-settings-security.xml;
+      path = "${config.home.homeDirectory}/projects/lovdata/settings-security.xml";
+      mode = "0400";
+    };
+  };
 }
