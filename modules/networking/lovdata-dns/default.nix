be871d85cb7922d1da2f606ff13fb624da132d1f
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-22 12:06:05 +0200

    feat: add lovdata stage hostname and ssh key

--- modules/networking/lovdata-dns/default.nix
+++ modules/networking/lovdata-dns/default.nix
@@ -1,10 +1,19 @@
 { util, ... }:
 util.mkToggledModule [ "networking" ] {
   name = "lovdata-dns";
+  hm.sops.secrets.lovdata-ssh-key = {
+    format = "binary";
+    sopsFile = ../../../secrets/utv-vnb-lt-lovdata-stage.ssh;
+  };
   system-nixos = {
-    networking.search = [
-      "lovdata.no"
-      "lovdata.c.bitbit.net"
-    ];
+    networking = {
+      search = [
+        "lovdata.no"
+        "lovdata.c.bitbit.net"
+      ];
+      extraHosts = ''
+        stage02.lovdata.c.bitbit.net stage
+      '';
+    };
   };
 }
