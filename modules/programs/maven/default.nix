d2657523fe0d36cd8d1b3359165028e91e88f91c
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-22 13:59:30 +0200

    feat: add maven settings-security.xml

--- modules/programs/maven/default.nix
+++ modules/programs/maven/default.nix
@@ -7,12 +7,19 @@
 util.mkProgram {
   name = "maven";
   hm = {
-    home.packages = [ pkgs.maven ];
+    home = {
+      packages = [ pkgs.maven ];
+      file.".m2/settings-security.xml".text = ''
+        <settingsSecurity>
+          <relocation>
+            ${config.sops.secrets.lovdata-maven-settings-security.path}
+          </relocation>
+        </settingsSecurity>
+      '';
+    };
     sops.secrets.lovdata-maven-settings-security = {
       format = "binary";
       sopsFile = ../../../secrets/utv-vnb-lt-maven-settings-security.xml;
-      path = "${config.home.homeDirectory}/projects/lovdata/settings-security.xml";
-      mode = "0400";
     };
   };
 }
