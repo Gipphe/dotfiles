3ccc9f7200df8b4f746ea583ec19d8e87d2387bc
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-30 10:12:48 +0200

    tweak: simplify audio setup

--- modules/system/audio/default.nix
+++ modules/system/audio/default.nix
@@ -5,37 +5,12 @@ util.mkToggledModule [ "system" ] {
     # Enable sound with pipewire.
     security.rtkit.enable = true;
     services = {
-      pulseaudio.support32Bit = true;
       pipewire = {
         enable = true;
         audio.enable = true;
-        alsa = {
-          enable = true;
-          support32Bit = true;
-        };
+        alsa.enable = true;
         pulse.enable = true;
-        wireplumber = {
-          enable = true;
-          extraConfig.bluetoothEnhancement."monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.codecs" = [
-              "sbc"
-              "sbc_xq"
-              "aac"
-              "ldac"
-              "aptx"
-              "aptx_hd"
-            ];
-          };
-        };
-        extraConfig.pipewire."92-low-latency"."context.properties" = {
-          "default.clock.rate" = 48000;
-          "default.clock.quantum" = 32;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
-        };
+        wireplumber.enable = true;
       };
     };
   };
