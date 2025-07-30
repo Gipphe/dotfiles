fe24234820719f5805af3b61de6b0089efbbc98e
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-30 09:18:59 +0200

    feat: add playerctld

--- modules/programs/mpris/default.nix
+++ modules/programs/mpris/default.nix
@@ -3,6 +3,9 @@ util.mkProgram {
   name = "mpris";
   hm = {
     home.packages = [ pkgs.playerctl ];
-    services.mpris-proxy.enable = true;
+    services = {
+      mpris-proxy.enable = true;
+      playerctld.enable = true;
+    };
   };
 }
