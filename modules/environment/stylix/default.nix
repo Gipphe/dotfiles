ad1afc303cc38e13759d9135cbf470c12670742b
Victor Nascimento Bakke <gipphe@gmail.com>
2025-07-31 10:33:39 +0200

    tweak: replace Bibata cursor with Vimix cursor

--- modules/environment/stylix/default.nix
+++ modules/environment/stylix/default.nix
@@ -51,8 +51,8 @@ util.mkEnvironment {
       stylix
       {
         cursor = {
-          package = pkgs.bibata-cursors;
-          name = "Bibata-Modern-Ice";
+          package = pkgs.vimix-cursors;
+          name = "Vimix-white-cursors";
           size = 24;
         };
       }
