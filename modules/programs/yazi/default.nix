3887a26f14dbe0899b2ac427087d8fc8f1e3c72e
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-01 12:25:32 +0200

    feat: add default app for yazi

--- modules/programs/yazi/default.nix
+++ modules/programs/yazi/default.nix
@@ -13,11 +13,19 @@ let
 in
 util.mkProgram {
   name = "yazi";
-  options.gipphe.programs.yazi.hyprland.enable = lib.mkEnableOption "hyprland integration";
+  options.gipphe.programs.yazi = {
+    hyprland.enable = lib.mkEnableOption "hyprland integration";
+    default = lib.mkEnableOption "default file manager";
+  };
   hm = {
     programs.yazi.enable = true;
     wayland.windowManager.hyprland.settings.bind = lib.mkIf cfg.hyprland.enable [
       "$mod, E, exec, ${file_manager_script}/bin/file-manager-script.sh"
     ];
+    gipphe.default.filemanager = lib.mkIf cfg.default {
+      name = "yazi";
+      package = config.programs.yazi.package;
+      actions.open = "${config.programs.yazi.package}/bin/yazi";
+    };
   };
 }
