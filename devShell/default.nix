4411ba4a2c7cff07f381244419994b98043cc289
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-01 13:03:43 +0200

    refactor: tidy up devShell

--- devShell/default.nix
+++ devShell/default.nix
@@ -1,22 +1,63 @@
 { pkgs, lib, ... }:
 let
-  shellApplicationAsCommand =
-    {
-      name,
-      runtimeInputs ? [ ],
-      runtimeEnv ? { },
-      text,
-    }:
-    lib.getExe (
-      pkgs.writeShellApplication {
-        inherit
-          name
-          runtimeInputs
-          runtimeEnv
-          text
-          ;
-      }
-    );
+  util = pkgs.callPackage ../util.nix { };
+  cmd = opts: lib.getExe (util.writeFishApplication opts);
+  build =
+    { name, ask }:
+    cmd {
+      inherit name;
+      runtimeInputs = with pkgs; [
+        nh
+        jq
+      ];
+      text =
+        # fish
+        ''
+          if command -q nixos-rebuild || command -q darwin-rebuild
+            set -l args
+            if test '${builtins.toString ask}' = 'true'
+              set -a args '--ask'
+            end
+
+            if command -q nixos-rebuild
+              nh os switch $args
+            else
+              nh darwin switch $args
+            end
+          else if command -v nix-on-droid &>/dev/null
+            set -l hostname $(jq '.hostname' env.json)
+            if test -z "$hostname"
+              echo 'Found no hostname in env.json' >&2
+              exit 1
+            end
+
+            nix-on-droid build --flake "$(pwd)#""$hostname"
+
+            echo
+            set -l nixOnDroidPkg $(nix path-info --impure "$NH_FLAKE#nixOnDroidConfigurations.$hostname.activationPackage")
+            nvd diff "$nixOnDroidPkg" result
+            echo
+
+            set -l REPLY
+            if test '${builtins.toString ask}' = 'true'
+              echo "Apply the config?"
+              read -r -p "[y/N]" -n 1 REPLY
+              echo
+            else
+              set REPLY y
+            end
+
+            switch "$(string lower $REPLY)"
+              case y
+                nix-on-droid switch --flake "$(pwd)#""$hostname"
+            end
+            rm -f result
+          else
+            echo "This is not a NixOS, nix-darwin or nix-on-droid system" >&2
+            exit 1
+          end
+        '';
+    };
 in
 {
   shellCommands = [
@@ -24,116 +65,36 @@ in
     rec {
       help = "Rebuild the system using nh, darwin-rebuild or home-manager, whichever is applicable";
       name = "sw";
-      command = shellApplicationAsCommand {
+      command = build {
         inherit name;
-        runtimeInputs = with pkgs; [
-          nh
-          jq
-        ];
-        text =
-          # bash
-          ''
-            if command -v nixos-rebuild &>/dev/null; then
-              nh os switch
-            elif command -v nix-on-droid &>/dev/null; then
-              hostname=$(jq '.hostname' env.json)
-              if test -z "$hostname"; then
-                echo 'Found no hostname in env.json' >&2
-                exit 1
-              fi
-              nix-on-droid build --flake "$(pwd)#''${hostname}"
-              echo
-              nixOnDroidPkg=$(nix path-info --impure "$NH_FLAKE#nixOnDroidConfigurations.$hostname.activationPackage")
-              nvd diff "$nixOnDroidPkg" result
-              echo
-              nix-on-droid switch --flake "$(pwd)#''${hostname}"
-            elif command -v darwin-rebuild &>/dev/null; then
-              darwin-rebuild build --flake "$(pwd)"
-              echo
-              nvd diff /run/current-system result
-              echo
-              darwin-rebuild switch --flake "$(pwd)"
-              rm -f result
-            elif command -v home-manager &>/dev/null; then
-              nh home switch
-            fi
-          '';
+        ask = false;
       };
       category = "build";
     }
     rec {
       help = "Rebuild the system using nh os switch --ask";
       name = "swa";
-      command = shellApplicationAsCommand {
+      command = build {
         inherit name;
-        runtimeInputs = with pkgs; [
-          nh
-          nvd
-        ];
-        text = # bash
-          ''
-            if command -v nixos-rebuild &> /dev/null; then
-              nh os switch --ask
-            elif command -v nix-on-droid &>/dev/null; then
-              hostname=$(jq '.hostname' env.json)
-              if test -z "$hostname"; then
-                echo 'Found no hostname in env.json' >&2
-                exit 1
-              fi
-
-              nix-on-droid build --flake "$(pwd)#''${hostname}"
-
-              echo
-              nixOnDroidPkg=$(nix path-info --impure "$NH_FLAKE#nixOnDroidConfigurations.$hostname.activationPackage")
-              nvd diff "$nixOnDroidPkg" result
-              echo
-
-              echo "Apply the config?"
-              read -r -p "[y/N]" -n 1 REPLY
-              echo
-
-              case "$REPLY" in
-                y|Y) nix-on-droid switch --flake "$(pwd)#''${hostname}" ;;
-              esac
-              rm -f result
-            elif command -v darwin-rebuild &>/dev/null; then
-              darwin-rebuild build --flake "$(pwd)"
-
-              echo
-              nvd diff /run/current-system result
-              echo
-
-              echo "Apply the config?"
-              read -r -p "[y/N]" -n 1 REPLY
-              echo
-
-              case "$REPLY" in
-                y|Y) darwin-rebuild switch --flake "$(pwd)" ;;
-              esac
-              rm -f result
-            else
-              echo "This is not a NixOS system" >&2
-              exit 1
-            fi
-          '';
+        ask = true;
       };
       category = "build";
     }
     rec {
       help = "Rebuild the system using nh os boot";
       name = "boot";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.nh ];
         text =
-          # bash
+          # fish
           ''
-            if command -v nixos-rebuild &> /dev/null; then
+            if command -q nixos-rebuild
               nh os boot
             else
               echo "This is not a NixOS system" >&2
               exit 1
-            fi
+            end
           '';
       };
       category = "build";
@@ -143,11 +104,11 @@ in
     rec {
       help = "Format the source tree with treefmt";
       name = "fmt";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.treefmt ];
         text =
-          # bash
+          # fish
           "treefmt";
       };
       category = "formatter";
@@ -157,12 +118,14 @@ in
     rec {
       help = "Update flake inputs and commit changes";
       name = "update";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.git ];
         text =
-          # bash
-          ''nix flake update && git commit flake.lock -m "chore: update flake inputs"'';
+          # fish
+          ''
+            nix flake update && git commit flake.lock -m "chore: update flake inputs"
+          '';
       };
       category = "utils";
     }
@@ -171,18 +134,18 @@ in
     rec {
       help = "Track distribution of PR";
       name = "nix:pr";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.xdg-utils ];
         text =
-          # bash
+          # fish
           ''
-            if test "$#" = "0" || test "$1" = "--help"; then
+            if test "$(count $argv)" = "0" || test $argv[1] = "--help"
               echo "Usage: nix:pr <pr number>"
               exit 1
-            fi
-            prg=$(command -v open || command -v xdg-open)
-            eval "$prg 'https://nixpk.gs/pr-tracker.html?pr=$1'"
+            end
+            set -l prg $(command -s open || command -s xdg-open)
+            eval "$prg 'https://nixpk.gs/pr-tracker.html?pr="$argv[1]\'
           '';
       };
       category = "nix utils";
@@ -190,11 +153,13 @@ in
     rec {
       help = "View store path sizes";
       name = "nix:du";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         text =
-          # bash
-          "nix path-info -rS /run/current-system | sort -nk2";
+          # fish
+          ''
+            nix path-info -rS /run/current-system | sort -nk2
+          '';
       };
       category = "nix utils";
     }
@@ -203,11 +168,11 @@ in
     rec {
       help = "Check .nix files with statix";
       name = "lint:statix";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.statix ];
         text =
-          # bash
+          # fish
           "statix check";
       };
       category = "lint";
@@ -215,11 +180,11 @@ in
     rec {
       help = "Check .nix files with statix, watching";
       name = "lint:statix:watch";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.entr ];
         text =
-          # bash
+          # fish
           "find . -type f | entr statix check";
       };
       category = "lint";
@@ -227,11 +192,11 @@ in
     rec {
       help = "Check .nix files with deadnix";
       name = "lint:deadnix";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = [ pkgs.deadnix ];
         text =
-          # bash
+          # fish
           "deadnix --exclude ./modules/system/hardware-configuration/*.nix";
       };
       category = "lint";
@@ -239,14 +204,14 @@ in
     rec {
       help = "Check .nix files with deadnix, watching";
       name = "lint:deadnix:watch";
-      command = shellApplicationAsCommand {
+      command = cmd {
         inherit name;
         runtimeInputs = with pkgs; [
           entr
           findutils
         ];
         text =
-          # bash
+          # fish
           "find . -type f | entr deadnix --exclude ./modules/system/hardware-configuration/*.nix";
       };
       category = "lint";
