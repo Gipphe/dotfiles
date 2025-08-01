7bce132ccc4f1a225cdbccef52efc412b5b88c23
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-01 16:12:36 +0200

    revert: revert: fix: nixos deprecations
    This reverts commit ee695ac983a5500674c33fae1db92b98809ed2ec.

--- modules/system/systemd/default.nix
+++ modules/system/systemd/default.nix
@@ -1,16 +1,20 @@
 { util, ... }:
 let
-  extraConfig = ''
-    DefaultTimeoutStopSec=16s
-  '';
+  settings = {
+    Manager = {
+      DefaultTimeoutStopSec = "16s";
+    };
+  };
 in
 util.mkToggledModule [ "system" ] {
   name = "systemd";
   system-nixos = {
     systemd = {
-      inherit extraConfig;
+      inherit settings;
       user = {
-        inherit extraConfig;
+        extraConfig = ''
+          DefaultTimeoutStopSec=16s
+        '';
       };
       services = {
         "getty@tty1".enable = false;
@@ -23,9 +27,6 @@ util.mkToggledModule [ "system" ] {
       # Fedora enables these options by deafult. See the 10-oomd-* files here:
       # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
       oomd.enableRootSlice = true;
-
-      # TODO: channels-to-flakes
-      tmpfiles.rules = [ "D /nix/var/nix/profiles/per-user/root 755 root root - -" ];
     };
   };
 }
