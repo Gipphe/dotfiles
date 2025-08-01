649b7be28dc6164d86a58dc5f90b0ace15aabe5f
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-01 13:39:05 +0200

    refactor: remove redundant systemd service configs

--- modules/system/systemd/default.nix
+++ modules/system/systemd/default.nix
@@ -3,13 +3,6 @@ util.mkToggledModule [ "system" ] {
   name = "systemd";
   system-nixos = {
     systemd = {
-      services = {
-        "getty@tty1".enable = false;
-        "autovt@tty1".enable = false;
-        "getty@tty7".enable = false;
-        "autovt@tty7".enable = false;
-      };
-
       # Systemd OOMd
       # Fedora enables these options by deafult. See the 10-oomd-* files here:
       # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
