2c726a953ec26b04c5a80f16a4bbc13d21f432fe
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-04 12:46:28 +0200

    refactor: disable editor in systemd-boot

--- modules/boot/systemd-boot/default.nix
+++ modules/boot/systemd-boot/default.nix
@@ -2,7 +2,10 @@
 util.mkToggledModule [ "boot" ] {
   name = "systemd-boot";
   system-nixos.boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      editor = false;
+    };
     efi.canTouchEfiVariables = true;
   };
 }
