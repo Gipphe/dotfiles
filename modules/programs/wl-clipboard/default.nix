a58f8217463f6f4a1b848a0b7db9e7e5b5e113e2
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-04 15:14:04 +0200

    feat: add wl-clipboard syncing service

--- modules/programs/wl-clipboard/default.nix
+++ modules/programs/wl-clipboard/default.nix
@@ -11,5 +11,20 @@ in
 util.mkProgram {
   name = "wl-clipboard";
   options.gipphe.programs.wl-clipboard.package = lib.mkPackageOption pkgs "wl-clipboard" { };
-  hm.home.packages = [ cfg.package ];
+  hm = {
+    home.packages = [ cfg.package ];
+    systemd.user.services.wl-clipboard = {
+      Unit = {
+        Description = "Synchronizes clipboard contents between applications";
+        After = [ config.wayland.systemd.target ];
+        PartOf = [ config.wayland.systemd.target ];
+      };
+      Service = {
+        Type = "exec";
+        ExecStart = "${cfg.package}/bin/wl-paste --primary --watch ${cfg.package}/bin/wl-copy";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ config.wayland.systemd.target ];
+    };
+  };
 }
