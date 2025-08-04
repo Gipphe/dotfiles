3272b512987b81baeda7d7251e9ceffc1d046860
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-04 19:03:43 +0200

    refactor: extract ttf-to-psf from minecraftia implementation

--- packages/minecraftia.nix
+++ packages/minecraftia.nix
@@ -1,31 +1,8 @@
 {
-  runCommandNoCC,
   minecraftia,
-  gnused,
-  otf2bdf,
-  bdf2psf,
-  gzip,
+  callPackage,
 }:
-let
-  bdf2psf-share = "${bdf2psf}/share/bdf2psf";
-in
-runCommandNoCC "minecraftia-psf" { } ''
-  echo "otf2bdf"
-  (set +e; set +o pipefail; ${otf2bdf}/bin/otf2bdf -v -r 72 -p 12 -c C "${minecraftia}/share/fonts/truetype/Minecraftia.ttf" |
-    ${gnused}/bin/sed -e "s/AVERAGE_WIDTH.*/AVERAGE_WIDTH 80/" > minecraftia.bdf
-  )
-
-  echo "bdf2psf"
-  ${bdf2psf}/bin/bdf2psf minecraftia.bdf \
-    ${bdf2psf-share}/standard.equivalents \
-    ${bdf2psf-share}/ascii.set+${bdf2psf-share}/linux.set+${bdf2psf-share}/useful.set \
-    256 \
-    minecraftia.psf
-
-  echo "gzip"
-  ${gzip}/bin/gzip minecraftia.psf
-
-  echo "dir"
-  mkdir -p $out/share/consolefonts
-  mv minecraftia.psf.gz $out/share/consolefonts
-''
+callPackage ./ttf-to-psf.nix {
+  name = "Minecraftia.ttf";
+  pkg = minecraftia;
+}
---
+++ packages/ttf-to-psf.nix
@@ -0,0 +1,44 @@
+{
+  runCommandNoCC,
+  gnused,
+  otf2bdf,
+  bdf2psf,
+  gzip,
+  lib,
+
+  pkg,
+  name,
+  pointSize ? "24",
+  resolution ? "72",
+}:
+let
+  bdf2psf-share = "${bdf2psf}/share/bdf2psf";
+  cleanName = lib.pipe name [
+    builtins.baseNameOf
+    (lib.splitStringBy (_: x: x == ".") false)
+    lib.reverseList
+    builtins.tail
+    lib.reverseList
+    (lib.concatStringsSep ".")
+  ];
+in
+runCommandNoCC "minecraftia-psf" { } ''
+  echo "otf2bdf"
+  (set +e; set +o pipefail; ${otf2bdf}/bin/otf2bdf -v -r ${resolution} -p ${pointSize} -c C "${pkg}/share/fonts/truetype/${name}" |
+    ${gnused}/bin/sed -e "s/AVERAGE_WIDTH.*/AVERAGE_WIDTH 80/" > font.bdf
+  )
+
+  echo "bdf2psf"
+  ${bdf2psf}/bin/bdf2psf font.bdf \
+    ${bdf2psf-share}/standard.equivalents \
+    ${bdf2psf-share}/ascii.set+${bdf2psf-share}/linux.set+${bdf2psf-share}/useful.set \
+    256 \
+    font.psf
+
+  echo "gzip"
+  ${gzip}/bin/gzip font.psf
+
+  echo "dir"
+  mkdir -p $out/share/consolefonts
+  mv font.psf.gz $out/share/consolefonts/${cleanName}.psf.gz
+''
