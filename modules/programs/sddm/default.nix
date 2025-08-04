7a1a8a5618ec299dba42ff3f58e024ec04b7785e
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-04 14:20:29 +0200

    feat: add catppuccin theme to sddm

--- modules/programs/sddm/default.nix
+++ modules/programs/sddm/default.nix
@@ -1,15 +1,38 @@
-{ config, util, ... }:
+{
+  pkgs,
+  util,
+  ...
+}:
 util.mkProgram {
   name = "sddm";
-  system-nixos.services.displayManager = {
-    sddm = {
-      enable = true;
-      autoNumlock = true;
-      wayland.enable = true;
+  system-nixos = {
+    environment.systemPackages = [
+      pkgs.catppuccin-sddm.override
+      {
+        flavor = "macchiato";
+        font = "Noto Sans";
+        fontSize = "12";
+        background = "${
+          pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "sddm";
+            rev = "master";
+            hash = "";
+          }
+        }/src/backgrounds/wall.png";
+        loginBackground = true;
+      }
+    ];
+    services.displayManager = {
+      sddm = {
+        enable = true;
+        autoNumlock = true;
+        wayland.enable = true;
+      };
+      # autoLogin = {
+      #   enable = true;
+      #   user = config.gipphe.username;
+      # };
     };
-    # autoLogin = {
-    #   enable = true;
-    #   user = config.gipphe.username;
-    # };
   };
 }
