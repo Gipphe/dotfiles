188bfad578351ee3a0041c184e2fe2c759317cd2
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-04 19:07:54 +0200

    chore: wl-paste --watch must run in a terminal

--- modules/programs/wl-clipboard/default.nix
+++ modules/programs/wl-clipboard/default.nix
@@ -13,18 +13,5 @@ util.mkProgram {
   options.gipphe.programs.wl-clipboard.package = lib.mkPackageOption pkgs "wl-clipboard" { };
   hm = {
     home.packages = [ cfg.package ];
-    systemd.user.services.wl-clipboard = {
-      Unit = {
-        Description = "Synchronizes clipboard contents between applications";
-        After = [ config.wayland.systemd.target ];
-        PartOf = [ config.wayland.systemd.target ];
-      };
-      Service = {
-        Type = "exec";
-        ExecStart = "${cfg.package}/bin/wl-paste --primary --watch ${cfg.package}/bin/wl-copy";
-        Restart = "on-failure";
-      };
-      Install.WantedBy = [ config.wayland.systemd.target ];
-    };
   };
 }
