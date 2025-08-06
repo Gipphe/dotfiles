f3001c3bf2c38b108554230f17637fde37b78e9d
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-06 14:36:06 +0200

    feat: split cli profile into cli-slim and cli

--- machines/helium/default.nix
+++ machines/helium/default.nix
@@ -7,7 +7,7 @@ util.mkToggledModule [ "machines" ] {
       homeDirectory = "/data/data/com.termux.nix/files/home";
       hostName = "helium";
       profiles = {
-        cli.enable = true;
+        cli-slim.enable = true;
         core.enable = true;
         fonts.enable = true;
         gc.enable = true;
