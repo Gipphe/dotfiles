4d7c0d4ddd3b6e01ca7844820a1053f7cd2019a7
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-07 08:50:33 +0200

    fix: sddm theme does not apply

--- modules/programs/sddm/default.nix
+++ modules/programs/sddm/default.nix
@@ -4,6 +4,14 @@
   config,
   ...
 }:
+let
+  background = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "sddm";
+    rev = "master";
+    hash = "sha256-J2DkKptVjWFcA2R71Vv7e0DCZJKeIl5TwjbnzI1kYmw=";
+  };
+in
 util.mkProgram {
   name = "sddm";
   system-nixos = {
@@ -11,21 +19,15 @@ util.mkProgram {
       (pkgs.catppuccin-sddm.override {
         flavor = "macchiato";
         font = "Noto Sans";
-        fontSize = "12";
-        background = "${
-          pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "sddm";
-            rev = "master";
-            hash = "sha256-J2DkKptVjWFcA2R71Vv7e0DCZJKeIl5TwjbnzI1kYmw=";
-          }
-        }/src/backgrounds/wall.png";
+        fontSize = "14";
+        background = "${background}/src/backgrounds/wall.png";
         loginBackground = true;
       })
     ];
     services.displayManager = {
       sddm = {
         enable = true;
+        package = pkgs.kdePackages.sddm;
         autoNumlock = true;
         wayland.enable = true;
         theme = "catppuccin-macchiato";
