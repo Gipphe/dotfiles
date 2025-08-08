b230d5353b1a850ed6c568f5d92785accb88afee
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-08 18:40:46 +0200

    feat: add windowrule for runelite menus

--- modules/programs/runelite/default.nix
+++ modules/programs/runelite/default.nix
@@ -1,5 +1,10 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "runelite";
-  hm.home.packages = [ pkgs.runelite ];
+  hm = {
+    home.packages = [ pkgs.runelite ];
+    wayland.windowManager.hyprland.settings.windowrule = [
+      "nofocus,floating:1,class:net-runelite-client-RuneLite,title:^(win\d+)$"
+    ];
+  };
 }
