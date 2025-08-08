dcc2b21cc387a83997d9e24716abe84f749ba3b3
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-08 15:53:35 +0200

    feat: use new catppuccin-sddm theme for sddm

--- modules/programs/sddm/default.nix
+++ modules/programs/sddm/default.nix
@@ -1,26 +1,22 @@
 {
+  self,
   pkgs,
   util,
   config,
   ...
 }:
 let
-  background = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "sddm";
-    rev = "master";
-    hash = "sha256-J2DkKptVjWFcA2R71Vv7e0DCZJKeIl5TwjbnzI1kYmw=";
-  };
+  flavor = "macchiato";
+  accent = "mauve";
 in
 util.mkProgram {
   name = "sddm";
   system-nixos = {
     environment.systemPackages = [
-      (pkgs.catppuccin-sddm.override {
-        flavor = "macchiato";
+      (self.packages.${pkgs.system}.catppuccin-sddm.override {
+        inherit flavor accent;
         font = "Noto Sans";
         fontSize = "14";
-        background = "${background}/src/backgrounds/wall.png";
         loginBackground = true;
       })
     ];
@@ -30,7 +26,7 @@ util.mkProgram {
         package = pkgs.kdePackages.sddm;
         autoNumlock = true;
         wayland.enable = true;
-        theme = "catppuccin-macchiato";
+        theme = "catppuccin-${flavor}-${accent}";
       };
       autoLogin = {
         enable = false;
