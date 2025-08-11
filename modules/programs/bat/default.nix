71e9284539d3314f99d9e80fe4df6edd5cb74d00
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-11 10:10:41 +0200

    feat: add batman man page viewer

--- modules/programs/bat/default.nix
+++ modules/programs/bat/default.nix
@@ -1,8 +1,20 @@
-{ util, ... }:
+{
+  util,
+  pkgs,
+  config,
+  ...
+}:
 util.mkProgram {
   name = "bat";
-  hm.programs = {
-    fish.shellAbbrs.cat = "bat";
-    bat.enable = true;
+  hm = {
+    home.packages = [
+      (pkgs.writeShellScriptBin "batman" ''
+        ${pkgs.man-db}/bin/man "$@" | ${pkgs.unixtools.col}/bin/col -bx | ${config.programs.bat.package}/bin/bat -l man -p
+      '')
+    ];
+    programs = {
+      fish.shellAbbrs.cat = "bat";
+      bat.enable = true;
+    };
   };
 }
