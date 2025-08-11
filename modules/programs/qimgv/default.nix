bcd940b2897e658a51772e0ad87ea9819925db68
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-11 08:28:10 +0200

    feat: add desktop entry for qimgv

--- modules/programs/qimgv/default.nix
+++ modules/programs/qimgv/default.nix
@@ -1,5 +1,21 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "qimgv";
-  hm.home.packages = [ pkgs.qimgv ];
+  hm = {
+    home.packages = [ pkgs.qimgv ];
+    xdg.desktopEntries.qimgv = {
+      exec = "${pkgs.qimgv}/bin/qimgv";
+      name = "qimgv";
+      type = "Application";
+      mimeType = [
+        "image/avif"
+        "image/gif"
+        "image/jpeg"
+        "image/jpg"
+        "image/png"
+        "image/svg+xml"
+        "image/webp"
+      ];
+    };
+  };
 }
