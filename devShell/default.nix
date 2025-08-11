6c7587a12bbfb94c19dc020fc4ba88110cd8fa98
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-11 11:20:26 +0200

    feat: add devshell command to generate fastfetch output

--- devShell/default.nix
+++ devShell/default.nix
@@ -299,32 +299,81 @@ in
     }
 
     rec {
-      help = "Generate neofetch output for the current host";
-      name = "md:neofetch";
+      help = "Generate fastfetch output for the current host";
+      name = "md:fastfetch";
+      category = "docs";
       command = cmd {
         inherit name;
         runtimeInputs = with pkgs; [
+          wezterm
           coreutils
           hyprland
           grim
-          neofetch
+          fastfetch
+          unclutter
         ];
-        text = ''
-          ${pkgs.wezterm}/bin/wezterm start --class neofetch --always-new-process -- bash -c 'sleep 1s && neofetch && read -p ""' &
-          sleep 5s
+        text =
+          let
+            cfg = pkgs.writeText "fastfetch-config" (
+              builtins.toJSON {
+                "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+                modules = [
+                  "title"
+                  "separator"
+                  "os"
+                  "host"
+                  "kernel"
+                  # "uptime"
+                  "packages"
+                  "shell"
+                  "display"
+                  "de"
+                  "wm"
+                  "wmtheme"
+                  "theme"
+                  "icons"
+                  "font"
+                  "cursor"
+                  "terminal"
+                  "terminalfont"
+                  "cpu"
+                  "gpu"
+                  "memory"
+                  "swap"
+                  "disk"
+                  # "localip"
+                  "battery"
+                  "poweradapter"
+                  "locale"
+                  "break"
+                  "colors"
+                ];
+              }
+            );
+          in
+          ''
+            wezterm start --class neofetch --always-new-process -- bash -c 'sleep 1s && fastfetch --config "${cfg}" && read -p ""' &
+            sleep 5s
 
-          set -l window (hyprctl clients -j | jq -r '.[] | select(.class == "neofetch")')
+            set -l window (hyprctl clients -j | jq -r '.[] | select(.class == "neofetch")')
 
-          set -l window_pid (echo $window | jq '.pid')
-          set -l pos (echo $window | jq -r '.at | (.[0] | tostring) + "," + (.[1] | tostring)')
-          set -l dim (echo $window | jq -r '.at | (.[0] | tostring) + "," + (.[1] | tostring)')
-          set -l g "$pos $dim"
+            set -l window_pid (echo $window | jq '.pid')
+            set -l pos (echo $window | jq -r '.at | (.[0] | tostring) + "," + (.[1] | . + 8 | tostring)')
+            # set -l dim (echo $window | jq -r '.size | (.[0] | tostring) + "x" + (.[1] | tostring)')
+            set -l dim "900x410"
+            set -l g "$pos $dim"
 
-          mkdir -p assets/neofetch
-          grim -g "$g" "assets/neofetch/$(hostname).png"
+            # Hide the cursor with unclutter
+            unclutter -idle 0.1 -root &
+            set -l unclutter_pid (jobs --last --pid | tail -n +1)
+            sleep 0.5s
 
-          kill $window_pid
-        '';
+            mkdir -p assets/neofetch
+            grim -g "$g" "assets/neofetch/$(hostname).png"
+
+            kill $window_pid
+            kill $unclutter_pid
+          '';
       };
     }
   ];
