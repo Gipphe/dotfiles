9a8462dd475e17a30f00a731cbac869cca7bc537
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-12 08:54:07 +0200

    feat(mattermost): add hotkey in Hyprland

--- modules/programs/mattermost/default.nix
+++ modules/programs/mattermost/default.nix
@@ -1,5 +1,18 @@
-{ util, pkgs, ... }:
+{
+  util,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  pkg = pkgs.mattermost-desktop;
+in
 util.mkProgram {
   name = "mattermost";
-  hm.home.packages = [ pkgs.mattermost-desktop ];
+  hm = {
+    home.packages = [ pkgs.mattermost-desktop ];
+    wayland.windowManager.hyprland.settings.bind = [
+      "$mod, M, exec, ${lib.getExe pkg}"
+    ];
+  };
 }
