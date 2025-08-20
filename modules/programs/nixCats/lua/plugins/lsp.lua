e4b0545a896482ee78a2f72276816d1872a29d7f
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-20 10:47:44 +0200

    fix(nixCats): remove remaining nvim-ufo code

--- modules/programs/nixCats/lua/plugins/lsp.lua
+++ modules/programs/nixCats/lua/plugins/lsp.lua
@@ -433,12 +433,6 @@ return {
           },
         }
       end
-
-      vim.o.foldcolumn = '1'
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      require('ufo').setup()
     end,
   },
 
