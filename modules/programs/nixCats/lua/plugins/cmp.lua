ce2eb4ee4f8dae31cd892dcec41e5b32a810f1e8
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-20 21:37:01 +0200

    refactor(nixCats): separate cmp deps from blink.cmp

--- modules/programs/nixCats/lua/plugins/cmp.lua
+++ modules/programs/nixCats/lua/plugins/cmp.lua
@@ -1,62 +1,4 @@
-return { -- Autocompletion
-  {
-    'L3MON4D3/LuaSnip',
-    -- NOTE: nixCats: nix downloads it with a different file name.
-    -- tell lazy about that.
-    name = 'luasnip',
-    build = require('nixCatsUtils').lazyAdd((function()
-      -- Build Step is needed for regex support in snippets.
-      -- This step is not supported in many windows environments.
-      -- Remove the below condition to re-enable on windows.
-      if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-        return
-      end
-      return 'make install_jsregexp'
-    end)()),
-    opts = {
-      keep_roots = true,
-      link_roots = true,
-      link_children = true,
-      exit_roots = false,
-      delete_check_events = 'TextChanged',
-    },
-    dependencies = {
-      -- `friendly-snippets` contains a variety of premade snippets.
-      --    See the README about individual language/framework/plugin snippets:
-      --    https://github.com/rafamadriz/friendly-snippets
-      -- {
-      --   'rafamadriz/friendly-snippets',
-      --   config = function()
-      --     require('luasnip.loaders.from_vscode').lazy_load()
-      --   end,
-      -- },
-    },
-    keys = {
-      {
-        '<tab>',
-        function()
-          return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
-        end,
-        mode = 'i',
-        expr = true,
-        silent = true,
-      },
-      {
-        '<tab>',
-        function()
-          return require('luasnip').jump(1)
-        end,
-        mode = 's',
-      },
-      {
-        '<S-tab>',
-        function()
-          return require('luasnip').jump(-1)
-        end,
-        mode = 's',
-      },
-    },
-  },
+return {
   {
     'saghen/blink.cmp',
     event = 'VimEnter',
@@ -123,6 +65,65 @@ return { -- Autocompletion
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
+    },
+  },
+
+  {
+    'L3MON4D3/LuaSnip',
+    -- NOTE: nixCats: nix downloads it with a different file name.
+    -- tell lazy about that.
+    name = 'luasnip',
+    build = require('nixCatsUtils').lazyAdd((function()
+      -- Build Step is needed for regex support in snippets.
+      -- This step is not supported in many windows environments.
+      -- Remove the below condition to re-enable on windows.
+      if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+        return
+      end
+      return 'make install_jsregexp'
+    end)()),
+    opts = {
+      keep_roots = true,
+      link_roots = true,
+      link_children = true,
+      exit_roots = false,
+      delete_check_events = 'TextChanged',
+    },
+    dependencies = {
+      -- `friendly-snippets` contains a variety of premade snippets.
+      --    See the README about individual language/framework/plugin snippets:
+      --    https://github.com/rafamadriz/friendly-snippets
+      -- {
+      --   'rafamadriz/friendly-snippets',
+      --   config = function()
+      --     require('luasnip.loaders.from_vscode').lazy_load()
+      --   end,
+      -- },
+    },
+    keys = {
+      {
+        '<tab>',
+        function()
+          return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
+        end,
+        mode = 'i',
+        expr = true,
+        silent = true,
+      },
+      {
+        '<tab>',
+        function()
+          return require('luasnip').jump(1)
+        end,
+        mode = 's',
+      },
+      {
+        '<S-tab>',
+        function()
+          return require('luasnip').jump(-1)
+        end,
+        mode = 's',
+      },
     },
   },
 }
