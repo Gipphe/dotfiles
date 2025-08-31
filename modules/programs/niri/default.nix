6022f51f17e08c204b738d9b3e2d22adb30817ab
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-31 23:28:35 +0000

    fix: niri missing on non-NixOS

--- modules/programs/niri/default.nix
+++ modules/programs/niri/default.nix
@@ -1,4 +1,5 @@
 {
+  flags,
   pkgs,
   util,
   config,
@@ -26,12 +27,15 @@ in
 util.mkProgram {
   name = "niri";
   hm = {
-    imports = [
-      {
+    imports =
+      lib.optional (!flags.isNixOnDroid) {
         stylix.targets.niri.enable = lib.mkDefault false;
       }
-    ];
-    stylix.targets.niri.enable = true;
+      ++ lib.optional flags.isNixOnDroid inputs.niri.homeModules.config
+      ++ lib.optional (!flags.isNixOnDroid && !flags.isNixos) inputs.niri.homeModules.stylix
+      ++ lib.optional (!flags.isNixOnDroid && config.gipphe.programs.niri.enable) {
+        stylix.targets.niri.enable = true;
+      };
     programs.niri = {
       settings = {
         binds = coreBinds;
