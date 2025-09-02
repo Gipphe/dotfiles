64922fbb3ae421a90e7d98136f6561f012a8b3a0
Victor Nascimento Bakke <gipphe@gmail.com>
2025-09-02 09:24:12 +0200

    fix(nixCats): replace old telescope pnpm implementation

--- modules/programs/nixCats/lsp/ts_ls.lua
+++ modules/programs/nixCats/lsp/ts_ls.lua
@@ -1,8 +1,5 @@
 return {
   on_attach = function()
-    -- TODO Replace telescope here
-    -- vim.keymap.set('n', '<leader>cw', function()
-    --   require('telescope').extensions.pnpm.workspace()
-    -- end, { buffer = true, desc = 'Select pnpm workspace', silent = true })
+    vim.keymap.set('n', '<leader>cw', require('pickers').pnpm_workspaces, { buffer = true, desc = 'Select pnpm workspace', silent = true })
   end,
 }
