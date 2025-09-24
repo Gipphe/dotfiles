7eb36a99cfd10373b84076afebd583109f0ddc37
Victor Nascimento Bakke <gipphe@gmail.com>
2025-09-24 08:49:14 +0200

    feat(hyprland): add smart gaps

--- modules/programs/hyprland/default.nix
+++ modules/programs/hyprland/default.nix
@@ -281,6 +281,17 @@ util.mkProgram {
           disable_hyprland_logo = true;
           disable_splash_rendering = true;
         };
+
+        workspace = [
+          "w[tv1]s[false], gapsout:0, gapsin:0"
+          "f[1]s[false], gapsout:0, gapsin:0"
+        ];
+        windowrule = [
+          "bordersize 0, floating:0, onworkspace:w[tv1]s[false]"
+          "rounding 0, floating:0, onworkspace:w[tv1]s[false]"
+          "bordersize 0, floating:0, onworkspace:f[1]s[false]"
+          "rounding 0, floating:0, onworkspace:f[1]s[false]"
+        ];
       };
       xwayland.enable = true;
     };
