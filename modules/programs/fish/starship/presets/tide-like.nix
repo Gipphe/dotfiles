e668005861deec72af55706e90f3480e62b24f15
Victor Nascimento Bakke <gipphe@gmail.com>
2025-09-26 09:17:24 +0200

    tweak(jj): truncate bookmarks in shell prompt

--- modules/programs/fish/starship/presets/tide-like.nix
+++ modules/programs/fish/starship/presets/tide-like.nix
@@ -179,7 +179,7 @@ in
             ${jj} log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
               separate(" ",
                 change_id.shortest(4),
-                bookmarks,
+                truncate_end(15, bookmarks),
                 "|",
                 concat(
                   if(conflict, "💥"),
