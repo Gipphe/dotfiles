6a4d5a865c398eb89f1f9eb8bf8694824c799346
Victor Nascimento Bakke <gipphe@gmail.com>
2025-10-06 08:22:25 +0200

    fix: Mattermost config is malformed

--- modules/lovdata/mattermost/default.nix
+++ modules/lovdata/mattermost/default.nix
@@ -3,7 +3,6 @@
   util,
   pkgs,
   lib,
-  config,
   ...
 }:
 let
@@ -14,34 +13,7 @@ util.mkToggledModule [ "lovdata" ] {
   hm = {
     imports = [ inputs.lovdata.homeModules.mattermost ];
     config = {
-      lovdata.mattermost = {
-        enable = true;
-        settings = {
-          showTrayIcon = true;
-          notifications = {
-            flashWindow = 0;
-            bounceIcon = true;
-            bounceIconType = "informational";
-          };
-          showUnreadBadge = true;
-          useSpellChecker = true;
-          enableHardwareAcceleration = true;
-          autostart = false;
-          hideOnStart = false;
-          spellCheckerLocales = [
-            "en-GB"
-            "nb"
-          ];
-          darkMode = false;
-          lastActiveTeam = 0;
-          downloadLocation = "${config.home.homeDirectory}/Downloads";
-          startInFullscreen = false;
-          lovLevel = "info";
-          enableMetrics = false;
-          trayIconTheme = "use_system";
-          alwaysMinimize = true;
-        };
-      };
+      lovdata.mattermost.enable = true;
       gipphe.core.wm.binds = [
         {
           mod = "$mod";
