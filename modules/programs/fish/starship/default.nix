73c7cd6aa3505347abb6aa67ea6298e76b647700
Victor Nascimento Bakke <gipphe@gmail.com>
2025-10-15 14:16:39 +0000

    chore(windows): fix starship config

--- modules/programs/fish/starship/default.nix
+++ modules/programs/fish/starship/default.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.gipphe.programs.fish;
+  tomlFormat = pkgs.formats.toml { };
 in
 {
   config = lib.mkIf (cfg.enable && cfg.prompt == "starship") {
@@ -20,6 +21,20 @@ in
 
     stylix.targets.starship.enable = false;
     gipphe.windows.home.file.".config/starship.toml".source =
-      config.home.file.${config.programs.starship.configPath}.source;
+      tomlFormat.generate "starship-config-windows"
+        (
+          lib.recursiveUpdate config.programs.starship.settings {
+            custom = {
+              git_branch.when = "! jj --ignore-working-copy root";
+              git_status.when = "! jj --ignore-working-copy root";
+              jj.when = "jj --ignore-working-copy root";
+              jj.command =
+                let
+                  stringParts = lib.splitString " " config.programs.starship.settings.custom.jj.command;
+                in
+                lib.concatStringsSep " " ([ "jj" ] ++ builtins.tail stringParts);
+            };
+          }
+        );
   };
 }
