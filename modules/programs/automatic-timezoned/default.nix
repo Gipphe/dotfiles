3f073f5e17eee0b0025533338b7d822805ab2afd
Victor Nascimento Bakke <gipphe@gmail.com>
2025-11-03 08:29:37 +0000

    fix(automatic-timezoned): missing permissions to use geoclue2

--- modules/programs/automatic-timezoned/default.nix
+++ modules/programs/automatic-timezoned/default.nix
@@ -1,5 +1,10 @@
 { util, ... }:
 util.mkProgram {
   name = "automatic-timezoned";
-  system-nixos.services.automatic-timezoned.enable = true;
+  system-nixos = {
+    services.automatic-timezoned.enable = true;
+
+    # Enable polkit for authorization (required by automatic-timezoned)
+    security.polkit.enable = true;
+  };
 }
