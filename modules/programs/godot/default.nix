d760ae9f0782723b21284b00511852415f374287
Victor Nascimento Bakke <gipphe@gmail.com>
2025-11-03 08:05:58 +0000

    chore: remove unused module code-cursor

--- modules/programs/godot/default.nix
+++ modules/programs/godot/default.nix
@@ -1,36 +1,15 @@
-{
-  config,
-  lib,
-  pkgs,
-  util,
-  ...
-}:
-util.mkModule {
-  options.gipphe.programs.godot.enable = lib.mkEnableOption "godot";
-  hm.config = lib.mkMerge [
-    {
-      gipphe.programs.code-cursor.windowsSettings = {
-        "godotTools.editorPath.godot4" = lib.mkDefault "godot";
-        "godotTools.lsp.serverPort" = lib.mkDefault 6005;
-      };
-    }
-    (lib.mkIf config.gipphe.programs.godot.enable (
-      lib.mkMerge [
-        {
-          gipphe = {
-            windows.chocolatey.programs = [
-              "godot-mono"
-              "dotnet-8.0-sdk"
-            ];
-            programs.code-cursor.settings."godotTools.editorPath.godot4" = "${pkgs.godot_4-mono}/bin/godot";
-          };
-          home.packages = [
-            (pkgs.writeShellScriptBin "gd" ''
-              ${pkgs.godot_4-mono}/bin/godot-mono "$@" &>/dev/null &
-            '')
-          ];
-        }
-      ]
-    ))
-  ];
+{ pkgs, util, ... }:
+util.mkProgram {
+  name = "godot";
+  hm = {
+    home.packages = [
+      (pkgs.writeShellScriptBin "gd" ''
+        ${pkgs.godot_4-mono}/bin/godot-mono "$@" &>/dev/null &
+      '')
+    ];
+    gipphe.windows.chocolatey.programs = [
+      "godot-mono"
+      "dotnet-8.0-sdk"
+    ];
+  };
 }
