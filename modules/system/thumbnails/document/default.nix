67d25b6dd502acd6c5045ea66f15c896e092940d
Victor Nascimento Bakke <gipphe@gmail.com>
2025-11-22 22:04:03 +0100

    fix(thumbnails): invalid function

--- modules/system/thumbnails/document/default.nix
+++ modules/system/thumbnails/document/default.nix
@@ -2,7 +2,7 @@
 util.mkToggledModule [ "system" "thumbnails" ] {
   name = "document";
   system-nixos = {
-    environment.systemPackages = builtins.attrVals {
+    environment.systemPackages = builtins.attrValues {
       inherit (pkgs)
         poppler # PDF
         f3d # 3D files
