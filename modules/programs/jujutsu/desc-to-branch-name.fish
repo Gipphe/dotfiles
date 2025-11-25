337d10c27d6b0301518dca8e9a3293ee955d76f8
Victor Nascimento Bakke <gipphe@gmail.com>
2025-11-25 16:03:27 +0100

    fix(jj): pub fails when desc contains quotes

--- modules/programs/jujutsu/desc-to-branch-name.fish
+++ modules/programs/jujutsu/desc-to-branch-name.fish
@@ -12,7 +12,7 @@ set -l desc (jj show --template description --no-patch "$rev" | head -n 1)
 or exit 1
 
 if test -n "$desc" && test "$(string replace -ra '[^:]+' "" "$desc")" = ":"
-    set desc (string replace -r '\\)?: ' '/' $desc | string replace -ra '[^\\w/]' '-' | string lower)
+    set desc (string replace -r '\\)?: ' '/' $desc | string replace -ra '[^\\w/]' '-' | string replace -ra '\'"`' '' | string lower)
     set desc (echo -n "$desc" | tr -sc '/:[:alnum:]' '-' | tr -s ':' '/')
 else
     # Fall back to "gipphe/push-<short_id>" if no description
