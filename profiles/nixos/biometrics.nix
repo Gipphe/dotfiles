f5d544de0ee5a58efbde9db096011bbdeaa43f8b
Victor Nascimento Bakke <gipphe@gmail.com>
2025-12-14 17:52:54 +0100

    docs: add comment on enrolling finger prints

--- profiles/nixos/biometrics.nix
+++ profiles/nixos/biometrics.nix
@@ -1,5 +1,6 @@
 { util, ... }:
 util.mkToggledModule [ "profiles" "nixos" ] {
   name = "biometrics";
+  # Use fprintd-enroll to enroll finger prints
   shared.gipphe.system.fprint.enable = true;
 }
