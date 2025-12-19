59252b716590855706106dcfd92b8eaca5180524
Victor Nascimento Bakke <gipphe@gmail.com>
2025-12-19 09:34:00 +0100

    fix: build for rbpi

--- machines/sodium/default.nix
+++ machines/sodium/default.nix
@@ -3,87 +3,49 @@
   util,
   inputs,
   lib,
-  pkgs,
   ...
 }:
 let
   host = import ./host.nix;
+  username = "gipphe";
 in
 util.mkToggledModule [ "machines" ] {
   inherit (host) name;
 
   shared.gipphe = {
-    username = "gipphe";
-    homeDirectory = "/home/gipphe";
+    inherit username;
+    homeDirectory = "/home/${username}";
     hostName = host.name;
     profiles = {
       nixos = {
-        audio.enable = true;
-        bluetooth.enable = true;
-        boot-efi.enable = true;
-        camera.enable = true;
-        devices.enable = true;
         networking.enable = true;
         power.enable = true;
         system.enable = true;
-        thumbnails.enable = true;
         time.enable = true;
+        wifi.enable = true;
       };
-      cli.enable = true;
+      cli-slim.enable = true;
       core.enable = true;
-      desktop.plasma.enable = true;
-      fonts.enable = true;
       gc.enable = true;
       keyring.enable = true;
       secrets.enable = true;
-    };
-    # system.cpu.enable = true;
-  };
-
-  hm = {
-    wayland.windowManager.hyprland.settings = {
-      monitor = [
-        ", preferred, auto-down, auto"
-      ];
     };
   };
 
   system-nixos = {
     imports = lib.optionals (hostname == host.name) [
+      ./hardware-configuration.nix
       inputs.nixos-hardware.nixosModules.raspberry-pi-4
     ];
 
-    boot.kernelParams = [ "kunit.enable=0" ];
-    hardware.deviceTree.filter = "bcm2711-rpi-4*.dtb";
-    boot = {
-      initrd.systemd.tpm2.enable = false;
-      initrd.availableKernelModules = [
-        "usbhid"
-        "usb_storage"
-        "vc4"
-        "pcie_brcmstb" # required for the pcie bus to work
-        "reset-raspberrypi" # required for vl805 firmware to load
+    users.users.${username} = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1qNZ75DNoJXpoAnEXKQzrLYWw6WyQIcnW0c6E1+9Sa gipphe@argon"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABZJhUC80F6MMN4++EiNuWdnlVMQBuXGxRM1t2Hz4Og gipphe@boron"
       ];
     };
-
-    hardware.enableRedistributableFirmware = true;
-
-    # networking.useDHCP = true;
-    networking.interfaces.eth0.useDHCP = true;
-    networking.interfaces.wlan0.useDHCP = true;
-
-    powerManagement.cpuFreqGovernor = "ondemand";
-
-    environment.systemPackages = with pkgs; [
-      raspberrypi-eeprom
-    ];
-
-    environment = {
-      # This allows alacritty to run
-      extraInit = ''
-        export LIBGL_ALWAYS_SOFTWARE=1
-      '';
-    };
+    services.openssh.enable = true;
+    networking.networkmanager.enable = lib.mkForce false;
 
     system.stateVersion = "26.05";
   };
