b8fd47c0b445d0c86790fcb5034d22c3f32df9af
Victor Nascimento Bakke <gipphe@gmail.com>
2025-12-19 09:34:00 +0100

    fix: build for rbpi

--- sd-image/sd-image.nix
+++ sd-image/sd-image.nix
@@ -20,11 +20,23 @@
     isNormalUser = true;
     home = "/home/gipphe";
     group = "gipphe";
+    initialPassword = "gipphe";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1qNZ75DNoJXpoAnEXKQzrLYWw6WyQIcnW0c6E1+9Sa gipphe@argon"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABZJhUC80F6MMN4++EiNuWdnlVMQBuXGxRM1t2Hz4Og gipphe@boron"
     ];
   };
   users.groups.gipphe = { };
+  nix.settings = {
+    allowed-users = [
+      "@wheel"
+      "root"
+    ];
+    trusted-users = [
+      "@wheel"
+      "root"
+    ];
+  };
   services.openssh.enable = true;
   system.stateVersion = "26.05";
 }
