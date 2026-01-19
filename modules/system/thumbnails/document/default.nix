4eac589ee59b8d5c2d163261ccd82273fe5db723
Victor Nascimento Bakke <gipphe@gmail.com>
2026-01-19 18:56:40 +0100

    chore: temporarily disable f3d while openturns is broken

--- modules/system/thumbnails/document/default.nix
+++ modules/system/thumbnails/document/default.nix
@@ -5,7 +5,12 @@ util.mkToggledModule [ "system" "thumbnails" ] {
     environment.systemPackages = builtins.attrValues {
       inherit (pkgs)
         poppler # PDF
-        f3d # 3D files
+        # TODO: Broken dependency openturns.
+        # See:
+        # - https://github.com/NixOS/nixpkgs/pull/480871
+        # - https://github.com/NixOS/nixpkgs/issues/480860
+        # - https://github.com/NixOS/nixpkgs/issues/481019
+        # f3d # 3D files
         ;
     };
   };
