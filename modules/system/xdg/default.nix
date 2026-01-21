80ed432deca6dc2ad74b6d24b2ef87226524eb83
Victor Nascimento Bakke <gipphe@gmail.com>
2026-01-21 08:59:35 +0100

    fix: missing extraPortals for xdg.portal

--- modules/system/xdg/default.nix
+++ modules/system/xdg/default.nix
@@ -1,12 +1,25 @@
-{ util, ... }:
+{ util, pkgs, ... }:
 util.mkSystem {
   name = "xdg";
   hm = {
     xdg = {
       enable = true;
       mimeApps.enable = true;
-      portal.enable = true;
-      portal.xdgOpenUsePortal = true;
+      portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+        config = {
+          common = {
+            default = [ "gtk" ];
+          };
+        };
+        extraPortals = builtins.attrValues {
+          inherit (pkgs)
+            xdg-desktop-portal
+            xdg-desktop-portal-gtk
+            ;
+        };
+      };
       terminal-exec.enable = true;
       userDirs = {
         enable = true;
