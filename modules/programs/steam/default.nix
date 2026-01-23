c9dfdff6d8b86873914473f752012a9459a7489b
Victor Nascimento Bakke <gipphe@gmail.com>
2026-01-23 18:17:38 +0100

    fix(steam): cannot find graphics device

--- modules/programs/steam/default.nix
+++ modules/programs/steam/default.nix
@@ -2,19 +2,45 @@
   util,
   pkgs,
   lib,
-  config,
   ...
 }:
 let
-  cfg = config.gipphe.programs.steam;
+  pkg = pkgs.steam.override {
+    extraArgs = "-system-composer";
+  };
 in
 util.mkProgram {
   name = "steam";
-  options.gipphe.programs.steam.package = lib.mkPackageOption pkgs "steam" { };
+  options.gipphe.programs.steam.package = lib.mkPackageOption pkgs "steam" { } // {
+    default = pkg;
+  };
   hm = {
-    home.packages = [ cfg.package ];
     wayland.windowManager.hyprland.settings.windowrule = [
       "float true, match:class steam, match:title (Friends List)"
     ];
+    programs.niri.settings.window-rules = [
+      {
+        matches = [
+          {
+            app-id = "steam";
+            title = "^notificationtoasts_\\d+_desktop$";
+          }
+        ];
+        default-floating-position = {
+          relative-to = "bottom-right";
+          x = 10;
+          y = 10;
+        };
+      }
+    ];
+  };
+  system-nixos = {
+    programs.steam = {
+      enable = true;
+      package = pkg;
+      protontricks.enable = true;
+      gamescopeSession.enable = true;
+    };
+    programs.gamescope.enable = true;
   };
 }
