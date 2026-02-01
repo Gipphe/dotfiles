2811bc8b1396881ea87b713555e3d29b53eb1575
Victor Nascimento Bakke <gipphe@gmail.com>
2026-02-01 16:28:50 +0100

    fix(runelite): keep env vars to runelite

--- modules/programs/runelite/default.nix
+++ modules/programs/runelite/default.nix
@@ -13,14 +13,11 @@ util.mkProgram {
           buildInputs = [ pkgs.makeWrapper ];
           postBuild = ''
             wrapProgram $out/bin/runelite \
-              --set GDK_SCALE 2
+              --set GDK_SCALE 2 \
+              --set _JAVA_AWT_WM_NONREPARENTING 1
           '';
         })
       ];
-      sessionVariables = {
-        _JAVA_AWT_WM_NONREPARENTING = 1;
-        GDK_SCALE = 2;
-      };
     };
     wayland.windowManager.hyprland.settings.windowrule = [
       "no_focus true, match:float yes, match:class net-runelite-client-RuneLite, match:title ^(win\d+)$"
