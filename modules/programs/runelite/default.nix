a42fe3002389765baf0b648c2632226a6aec3939
Victor Nascimento Bakke <gipphe@gmail.com>
2026-02-01 14:07:52 +0100

    fix(runelite): scaling and resolution issues

--- modules/programs/runelite/default.nix
+++ modules/programs/runelite/default.nix
@@ -1,8 +1,27 @@
 { util, pkgs, ... }:
+let
+  pkg = pkgs.runelite;
+in
 util.mkProgram {
   name = "runelite";
   hm = {
-    home.packages = [ pkgs.runelite ];
+    home = {
+      packages = [
+        (pkgs.symlinkJoin {
+          inherit (pkg) name version;
+          paths = [ pkg ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/runelite \
+              --set GDK_SCALE 2
+          '';
+        })
+      ];
+      sessionVariables = {
+        _JAVA_AWT_WM_NONREPARENTING = 1;
+        GDK_SCALE = 2;
+      };
+    };
     wayland.windowManager.hyprland.settings.windowrule = [
       "no_focus true, match:float yes, match:class net-runelite-client-RuneLite, match:title ^(win\d+)$"
     ];
