107bccd56dc5e1436790259370e7e20cb1b1bffc
Victor Nascimento Bakke <gipphe@gmail.com>
2026-02-03 18:51:49 +0100

    wip: swap boron to plasma

--- machines/boron/default.nix
+++ machines/boron/default.nix
@@ -35,7 +35,8 @@ util.mkToggledModule [ "machines" ] {
       audio.enable = true;
       cli.enable = true;
       core.enable = true;
-      desktop.niri.enable = true;
+      desktop.plasma.enable = true;
+      desktop.niri.enable = false;
       desktop.hyprland.enable = false;
       desktop.hyprland.noctalia.enable = true;
       fonts.enable = true;
--- profiles/desktop/plasma.nix
+++ profiles/desktop/plasma.nix
@@ -6,8 +6,12 @@ util.mkToggledModule [ "profiles" "desktop" ] {
     programs = {
       plasma6.enable = true;
       sddm.enable = true;
+      plymouth.enable = true;
+      cliphist.enable = true;
+      clipse.enable = true;
       pinentry-curses.enable = true;
       wl-clipboard.enable = true;
+      wezterm.enable = true;
     };
   };
 }
