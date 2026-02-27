57b2e76d6d1e172a5148578eae45eac78bf8d056
Victor Nascimento Bakke <gipphe@gmail.com>
2026-02-27 09:38:09 +0100

    feat(jj): prefix generated branch names with "gipphe"

--- modules/programs/jujutsu/desc-to-branch-name.fish
+++ modules/programs/jujutsu/desc-to-branch-name.fish
@@ -14,6 +14,7 @@ or exit 1
 if test -n "$desc" && test "$(string replace -ra '[^:]+' "" "$desc")" = ":"
     set desc (string replace -r '\\)?: ' '/' $desc | string replace -ra '[^\\w/]' '-' | string replace -ra '\'"`' '' | string lower)
     set desc (echo -n "$desc" | tr -sc '/:[:alnum:]' '-' | tr -s ':' '/')
+    set desc "gipphe/$desc"
 else
     # Fall back to "gipphe/push-<short_id>" if no description
     # is set, or description does not comply with
