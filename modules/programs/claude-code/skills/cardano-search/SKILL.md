1d510df31e6fcf689991cc534a8f9c25257bb293
Victor Nascimento Bakke <gipphe@gmail.com>
2026-03-12 11:00:57 +0100

    refactor(claude): use niv to set source versions for cardano repos

--- modules/programs/claude-code/skills/cardano-search/SKILL.md
+++ modules/programs/claude-code/skills/cardano-search/SKILL.md
@@ -4,13 +4,17 @@ description: |
   Search through the Cardano repositories in this directory. Use when looking
   through the local Cardano repositories for implementations and reference
   code, and when asked about how to do things with Cardano or Ouroboros.
-allowed-tools: Read, Grep, Glob
+allowed-tools: Read, Grep, Glob, Bash(ls *), Bash(grep *), Agent(Explore)
 ---
 
 All the repositories in the [repos](repos) directory are Haskell repositories
-pertaining to the Cardano and Ouroboros ecosystem of Haskell packages. Most, if
-not all, of them are multi-package repositories. Each package has
-a corresponding `<name>.cabal` file that describes the package, including
+pertaining to the Cardano and Ouroboros ecosystem of Haskell packages. Some of
+the directories for the repositories might be symlinks to the repository
+elsewhere in the system, which should be taken into consideration when using
+bash tools like `find`.
+
+Most, if not all, of them are multi-package repositories. Each package has a
+corresponding `<name>.cabal` file that describes the package, including
 pointing to where the source code is. Each `<name>.cabal` file specified one or
 more libraries for applications or testing.
 
--- modules/programs/claude-code/skills/cardano-search/default.nix
+++ modules/programs/claude-code/skills/cardano-search/default.nix
@@ -6,7 +6,12 @@
   writeTextFile,
 }:
 let
-  inherit (builtins) listToAttrs readFile;
+  inherit (builtins)
+    listToAttrs
+    readFile
+    fromJSON
+    attrValues
+    ;
   inherit (lib) pipe;
 
   skill = writeTextFile {
@@ -14,56 +19,23 @@ let
     text = readFile ./SKILL.md;
     destination = "/SKILL.md";
   };
-  repoDefs = [
-    {
-      owner = "IntersectMBO";
-      repo = "cardano-api";
-      rev = "034d1d7eee86be409a51309ba5fa1a788879928a";
-      hash = "sha256-/xFrz/qTtkJUFfyZ5wSJ99TBj0xAFhuICmjVhqFe6k0=";
-    }
-    {
-      owner = "IntersectMBO";
-      repo = "cardano-base";
-      rev = "db52f43b38ba5d8927feb2199d4913fe6c0f974d";
-      hash = "sha256-/xFrz/qTtkJUFfyZ5wSJ99TBj0xAFhuICmjVhqFe6k0=";
-    }
-    {
-      owner = "IntersectMBO";
-      repo = "cardano-ledger";
-      rev = "b5d3b4b1638af928830a629fdcd950640fac3334";
-      hash = "sha256-B1eFCvT9K25XdTaLc3pLvnywR93GgjR8Z8B7T2SRttw=";
-    }
-    {
-      owner = "IntersectMBO";
-      repo = "cardano-node";
-      rev = "10051dec74b21ff042b00e25239eb00fcbfc5556";
-      hash = "sha256-NY7ki0xkgD7W86NKuqwundTPpNmRh7u9px5eUb6FerU=";
-    }
-    {
-      owner = "IntersectMBO";
-      repo = "ouroboros-consensus";
-      rev = "3d32643b7fa46db63ff82fdac2c50c23584e839e";
-      hash = "sha256-lhBS68tbV0yFtrCTsW3Jk0xbnDUq90+WI0nsEHSH+Q0=";
-    }
-    {
-      owner = "IntersectMBO";
-      repo = "ouroboros-network";
-      rev = "38894b286110f5f4548c960d6a2652c2b9cdf53d";
-      hash = "sha256-YLQxPAxX2esr5v471+rTS8MA2eoE4nUFEgt6/r2Zlmw=";
-    }
-  ];
-  repoDrvs = map (
-    r:
-    fetchFromGitHub {
-      inherit (r)
-        owner
-        repo
-        rev
-        hash
-        ;
-    }
-  ) repoDefs;
-  repos = pipe repoDrvs [
+  # Update this file with
+  # , niv -s modules/programs/claude-code/skills/cardano-search/sources.json update
+  repos = pipe ./sources.json [
+    readFile
+    fromJSON
+    attrValues
+    (map (
+      r:
+      fetchFromGitHub {
+        inherit (r)
+          owner
+          repo
+          rev
+          sha256
+          ;
+      }
+    ))
     (map (d: {
       name = "repos/${d.repo}";
       value = d;
