5581c24c78a28d4363d8553e9ebd09d560410a94
Victor Nascimento Bakke <gipphe@gmail.com>
2026-03-30 17:00:44 +0200

    chore(noctalia): include cache regardless whether noctalia is enabled

--- modules/programs/noctalia-shell/default.nix
+++ modules/programs/noctalia-shell/default.nix
@@ -15,96 +15,100 @@ let
   noctalia-diff-settings = pkgs.callPackage ./scripts/noctalia-diff-settings.nix {
     noctalia-shell = config.programs.noctalia-shell.package;
   };
-in
-util.mkProgram {
-  name = "noctalia-shell";
-  hm = {
-    imports = [ inputs.noctalia.homeModules.default ];
-    programs.noctalia-shell = lib.mkMerge [
-      {
-        enable = true;
-        settings = {
-          bar = {
-            backgroundOpacity = lib.mkForce 1;
-            capsuleOpacity = lib.mkForce 1;
+
+  main = util.mkProgram {
+    name = "noctalia-shell";
+    hm = {
+      imports = [ inputs.noctalia.homeModules.default ];
+      programs.noctalia-shell = lib.mkMerge [
+        {
+          enable = true;
+          settings = {
+            bar = {
+              backgroundOpacity = lib.mkForce 1;
+              capsuleOpacity = lib.mkForce 1;
+            };
+            dock = {
+              backgroundOpacity = lib.mkForce 1;
+            };
+            general = {
+              avatarImage = lib.mkForce "${config.home.homeDirectory}/${config.home.file.".face".target}";
+            };
+            notifications = {
+              backgroundOpacity = lib.mkForce 1;
+            };
+            osd = {
+              backgroundOpacity = lib.mkForce 1;
+            };
+            ui = {
+              panelBackgroundOpacity = lib.mkForce 1;
+            };
+            wallpaper = {
+              directory = lib.mkForce "${config.home.homeDirectory}/${wallpaperDir}";
+            };
           };
-          dock = {
-            backgroundOpacity = lib.mkForce 1;
-          };
-          general = {
-            avatarImage = lib.mkForce "${config.home.homeDirectory}/${config.home.file.".face".target}";
-          };
-          notifications = {
-            backgroundOpacity = lib.mkForce 1;
-          };
-          osd = {
-            backgroundOpacity = lib.mkForce 1;
-          };
-          ui = {
-            panelBackgroundOpacity = lib.mkForce 1;
-          };
-          wallpaper = {
-            directory = lib.mkForce "${config.home.homeDirectory}/${wallpaperDir}";
-          };
+        }
+        {
+          settings = import ./settings.nix;
+        }
+      ];
+      home = {
+        file = {
+          ".face".source = ../../../assets/profile.png;
+          "${wallpaperDir}/small-memory.png".source = config.stylix.image;
         };
-      }
-      {
-        settings = import ./settings.nix;
-      }
-    ];
-    home = {
-      file = {
-        ".face".source = ../../../assets/profile.png;
-        "${wallpaperDir}/small-memory.png".source = config.stylix.image;
+        packages = [
+          noctalia-copy-gui-settings
+          noctalia-diff-settings
+        ];
       };
-      packages = [
-        noctalia-copy-gui-settings
-        noctalia-diff-settings
-      ];
-    };
-    gipphe.core.wm = {
-      binds = [
-        {
-          mod = "Mod";
-          key = "space";
-          action.spawn = "${ipc} launcher toggle";
-        }
-        {
-          mod = "Mod";
-          key = "C";
-          action.spawn = "${ipc} launcher clipboard";
-        }
-      ];
-      triggers.on-load =
-        let
-          startup = pkgs.writeShellApplication {
-            name = "noctalia-startup";
-            runtimeInputs = [ config.programs.noctalia-shell.package ];
-            text = ''
-              noctalia-shell kill || true
-              sleep 2s
-              noctalia-shell
-            '';
+      gipphe.core.wm = {
+        binds = [
+          {
+            mod = "Mod";
+            key = "space";
+            action.spawn = "${ipc} launcher toggle";
+          }
+          {
+            mod = "Mod";
+            key = "C";
+            action.spawn = "${ipc} launcher clipboard";
+          }
+        ];
+        triggers.on-load =
+          let
+            startup = pkgs.writeShellApplication {
+              name = "noctalia-startup";
+              runtimeInputs = [ config.programs.noctalia-shell.package ];
+              text = ''
+                noctalia-shell kill || true
+                sleep 2s
+                noctalia-shell
+              '';
+            };
+          in
+          {
+            noctalia-startup.command = lib.getExe startup;
           };
-        in
-        {
-          noctalia-startup.command = lib.getExe startup;
-        };
+      };
+    };
+    system-nixos = {
+      networking.networkmanager.enable = true;
+      hardware.bluetooth.enable = true;
+      services = {
+        auto-cpufreq.enable = lib.mkForce false;
+        power-profiles-daemon.enable = true;
+        upower.enable = true;
+      };
     };
   };
-  system-nixos = {
-    networking.networkmanager.enable = true;
-    hardware.bluetooth.enable = true;
-    services = {
-      auto-cpufreq.enable = lib.mkForce false;
-      power-profiles-daemon.enable = true;
-      upower.enable = true;
-    };
-    nix.settings = {
-      extra-substituters = [ "https://noctalia.cachix.org" ];
-      extra-trusted-public-keys = [
-        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      ];
-    };
+in
+util.mkModule {
+  shared.imports = [ main ];
+  system-nixos.nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
   };
 }
