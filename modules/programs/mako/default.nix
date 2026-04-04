639a4d2946ce887f8af2f43e4bfca1dfb4c06563
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-04 17:15:13 +0200

    refactor: use nix-wrapper-modules for mako

--- modules/programs/mako/default.nix
+++ modules/programs/mako/default.nix
@@ -1,14 +1,37 @@
-{ util, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  util,
+  ...
+}:
 util.mkProgram {
   name = "mako";
-  hm.services.mako = {
-    enable = true;
-    settings = {
-      # font = "Fira Sans Semibold";
-      border-radius = 8;
-      padding = 8;
-      border-size = 5;
-      default-timeout = 4000;
+  hm = {
+    imports = [
+      (inputs.wlib.lib.mkInstallModule {
+        loc = [
+          "home"
+          "packages"
+        ];
+        name = "mako";
+        value = inputs.wlib.lib.wrapperModules.mako;
+      })
+    ];
+    wrappers.mako = {
+      enable = true;
+      settings = {
+        # font = "Fira Sans Semibold";
+        border-radius = 8;
+        padding = 8;
+        border-size = 5;
+        default-timeout = 4000;
+      };
+      constructFiles.generatedConfig.builder = /* bash */ ''
+        mkdir -p "$(dirname "$2")" && cp "$1" "$2" && (${lib.getExe' pkgs.mako "makoctl"} reload || true)
+      '';
     };
+    dbus.packages = [ config.wrappers.mako.package ];
   };
 }
