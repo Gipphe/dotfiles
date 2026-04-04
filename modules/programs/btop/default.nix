891d25ee65d0e00349c4e810f39201d2caf8b8e5
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-04 08:23:02 +0200

    refactor: use nix-wrapper-modules for btop

--- modules/programs/btop/default.nix
+++ modules/programs/btop/default.nix
@@ -1,5 +1,30 @@
-{ util, ... }:
+{
+  util,
+  inputs,
+  config,
+  ...
+}:
 util.mkProgram {
   name = "btop";
-  hm.programs.btop.enable = true;
+  hm = {
+    imports = [
+      (inputs.wlib.lib.mkInstallModule {
+        loc = [
+          "home"
+          "packages"
+        ];
+        name = "btop";
+        value = inputs.wlib.lib.wrapperModules.btop;
+      })
+    ];
+    wrappers.btop = {
+      enable = true;
+      settings = {
+        color_theme = "stylix";
+      };
+      themes = {
+        stylix = config.programs.btop.themes.stylix;
+      };
+    };
+  };
 }
