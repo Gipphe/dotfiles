cf50b39d0feae4233f7015fdb2ea5e011805fa38
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-08 16:54:35 +0200

    chore: remove darwin programs

--- modules/programs/spotify-player/default.nix
+++ modules/programs/spotify-player/default.nix
@@ -1,6 +1,7 @@
 { util, ... }:
 util.mkProgram {
   name = "spotify-player";
-  # brew-nix cask hash for spotify: "sha256-JfrxFFYXrFWCqt1fxm79+hqHaiWETeXZ2cQzO59qiv4="
-  hm.programs.spotify-player.enable = true;
+  hm = {
+    programs.spotify-player.enable = true;
+  };
 }
