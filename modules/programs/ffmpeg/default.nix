76c0342341e9f0488484f6c23a2469ba745d731a
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-10 00:03:02 +0200

    feat(ffmpeg): add normalize-video for compressing videos

--- modules/programs/ffmpeg/default.nix
+++ modules/programs/ffmpeg/default.nix
@@ -1,5 +1,28 @@
 { util, pkgs, ... }:
 util.mkProgram {
   name = "ffmpeg";
-  hm.home.packages = [ pkgs.ffmpeg ];
+  hm = {
+    home.packages = [
+      pkgs.ffmpeg
+      (pkgs.writeShellApplication {
+        name = "normalize-video";
+        runtimeInputs = [ pkgs.ffmpeg ];
+        text = /* bash */ ''
+          info() {
+            echo "$@" >&2
+          }
+          print_help() {
+            info "Usage: normalize-video <input-path> <output-path>"
+          }
+          if test $# -ne 2; then
+            info "Missing an argument"
+            print_help
+            exit 1
+          fi
+
+          ffmpeg -i "$1" -vf scale=-2:720,fps=25 -c:v libx264 -crf 23 -preset slow -c:a aac -b:a 128k -movflags +faststart "$2"
+        '';
+      })
+    ];
+  };
 }
