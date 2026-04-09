5ac95b103898711a0e7a9a7a6a96f06165a24ca3
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-09 16:13:26 +0200

    fix: invalid nvidia.open setting

--- machines/titanium/default.nix
+++ machines/titanium/default.nix
@@ -60,6 +60,7 @@ util.mkToggledModule [ "machines" ] {
       [
         ./hardware-configuration.nix
         ./disk-config.nix
+        "${inputs.nixos-hardware}/common/gpu/nvidia/ampere"
       ]
       ++ builtins.attrValues {
         inherit (inputs.nixos-hardware.nixosModules)
