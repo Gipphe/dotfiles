c2afbd9de868cb348099f5bf5316ac897e124e12
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-10 10:10:00 +0200

    chore: remove outdated section of README and outdated scripts

--- setup/install-home-manager.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v home-manager >/dev/null; then
-  echo "Home-Manager already installed"
-  exit 0
-fi
-
-nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/home-manager/master -- init --switch "$(pwd)"
--- setup/vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IS_NIXOS="$(if test -d "/etc/nixos"; then echo "yes"; else echo ""; fi)"
-export IS_NIXOS
-IS_WSL="$(if test ! -z "$(grep -E 'microsoft|WSL' /proc/sys/kernel/osrelease 2>/dev/null)"; then echo "yes"; else echo ""; fi)"
-export IS_WSL
