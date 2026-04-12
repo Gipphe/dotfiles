33f601aff731c6cf500a91128d6e602e313105fe
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-12 11:48:17 +0200

    feat(filen): start on boot

--- modules/programs/filen/default.nix
+++ modules/programs/filen/default.nix
@@ -1,5 +1,13 @@
 { pkgs, util, ... }:
 util.mkProgram {
   name = "filen-desktop";
-  hm.home.packages = [ pkgs.filen-desktop ];
+  hm = {
+    home.packages = [ pkgs.filen-desktop ];
+    systemd.user.services.filen-startup = {
+      Unit.Description = "Start Filen on boot";
+      Service.Type = "oneshot";
+      Service.ExecStart = "${pkgs.filen-desktop}/bin/filen-desktop";
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
