21d7ef2aa68d481befb2a7b405ab2dcdefcc17d3
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-13 10:05:17 +0200

    fix(filen): unable to start on boot

--- modules/programs/filen/default.nix
+++ modules/programs/filen/default.nix
@@ -3,11 +3,6 @@ util.mkProgram {
   name = "filen-desktop";
   hm = {
     home.packages = [ pkgs.filen-desktop ];
-    systemd.user.services.filen-startup = {
-      Unit.Description = "Start Filen on boot";
-      Service.Type = "oneshot";
-      Service.ExecStart = "${pkgs.filen-desktop}/bin/filen-desktop";
-      Install.WantedBy = [ "graphical-session.target" ];
-    };
+    wayland.windowManager.hyprland.settings.exec-once = "filen-desktop";
   };
 }
