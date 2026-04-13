174f11759d0382ca982aea2486ff0fb24ab13e0c
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-13 11:14:48 +0200

    fix(filen): missing noctalia-shell icon

--- modules/programs/filen/default.nix
+++ modules/programs/filen/default.nix
@@ -1,8 +1,20 @@
 { pkgs, util, ... }:
+let
+  pkg = pkgs.symlinkJoin {
+    inherit (pkgs.filen-desktop) name pname version;
+    paths = [
+      pkgs.filen-desktop
+      (pkgs.linkFarm "filen-icon" {
+        "share/pixmaps/filen-desktop.png" =
+          "${pkgs.filen-desktop}/share/icons/hicolor/128x128/apps/filen-desktop.png";
+      })
+    ];
+  };
+in
 util.mkProgram {
   name = "filen-desktop";
   hm = {
-    home.packages = [ pkgs.filen-desktop ];
+    home.packages = [ pkg ];
     wayland.windowManager.hyprland.settings.exec-once = [ "filen-desktop" ];
   };
 }
