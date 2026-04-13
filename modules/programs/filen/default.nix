cc94aece171322732dfcac8d59693abfe0313761
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-13 10:24:52 +0200

    fix(hyprland): exec-once must be lists

--- modules/programs/filen/default.nix
+++ modules/programs/filen/default.nix
@@ -3,6 +3,6 @@ util.mkProgram {
   name = "filen-desktop";
   hm = {
     home.packages = [ pkgs.filen-desktop ];
-    wayland.windowManager.hyprland.settings.exec-once = "filen-desktop";
+    wayland.windowManager.hyprland.settings.exec-once = [ "filen-desktop" ];
   };
 }
