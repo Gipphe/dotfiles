029047eeab09a7a4eea6c359f6e6736f9d69b580
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-15 10:12:42 +0200

    fix(flatpak): invalid bash option

--- modules/programs/flatpak/default.nix
+++ modules/programs/flatpak/default.nix
@@ -3,7 +3,7 @@ util.mkProgram {
   name = "flatpak";
   system-nixos = {
     services.flatpak.enable = true;
-    programs.bash.profileExtra = ''
+    programs.bash.shellInit = ''
       export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
     '';
   };
