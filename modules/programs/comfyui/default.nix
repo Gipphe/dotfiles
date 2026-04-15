5fcbda14aebe1fdc6b0106d477210f8bd37980a6
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-16 00:16:37 +0200

    chore: add cai secret to nix-daemon environment

--- modules/programs/comfyui/default.nix
+++ modules/programs/comfyui/default.nix
@@ -1,5 +1,26 @@
-{ util, ... }:
+{ config, util, ... }:
+let
+  nixDaemonEnv = "/run/secrets/nix-daemon-env";
+in
 util.mkProgram {
   name = "comfyui";
-  shared.imports = [ ./proxy.nix ];
+  system-nixos = {
+    programs.comfyui = {
+      enable = true;
+    };
+    sops.secrets.cai_api_key = {
+      format = "binary";
+      sopsFile = ../../../secrets/pub-cai-api-key.txt;
+    };
+    system.activationScripts.nix-daemon-env = {
+      deps = [ "setupSecrets" ];
+      text = ''
+        printf 'CAI_API_KEY=%s\n' "$(cat ${config.sops.secrets.cai_api_key.path})" \
+          > ${nixDaemonEnv}
+        chmod 600 ${nixDaemonEnv}
+      '';
+    };
+
+    systemd.services.nix-daemon.serviceConfig.EnvironmentFile = [ nixDaemonEnv ];
+  };
 }
