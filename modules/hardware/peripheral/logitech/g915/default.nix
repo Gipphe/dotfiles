a78538f0bfdcc7717be7282e08f015593a780c05
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-16 00:16:37 +0200

    feat(g915): map media keys to noctalia-shell

--- modules/hardware/peripheral/logitech/g915/default.nix
+++ modules/hardware/peripheral/logitech/g915/default.nix
@@ -1,6 +1,30 @@
-{ util, ... }:
+{
+  util,
+  lib,
+  config,
+  ...
+}:
+let
+  noctalia-shell = "${lib.getExe' config.programs.noctalia-shell.package "noctalia-shell"} ipc call";
+in
 util.mkToggledModule [ "hardware" "peripheral" "logitech" ] {
   name = "g915";
+  hm = {
+    gipphe.core.wm.binds = [
+      {
+        key = "XF86AudioPlay";
+        action.spawn = "${noctalia-shell} media playPause";
+      }
+      {
+        key = "XF86AudioPrev";
+        action.spawn = "${noctalia-shell} media previous";
+      }
+      {
+        key = "XF86AudioNext";
+        action.spawn = "${noctalia-shell} media next";
+      }
+    ];
+  };
   system-nixos = {
     services.hardware.openrgb = {
       enable = true;
