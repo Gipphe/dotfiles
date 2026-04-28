51fd04928d452210be6d40edd79ebc3e39811253
Victor Nascimento Bakke <gipphe@gmail.com>
2026-04-28 09:46:33 +0200

    feat(logseq): add config

--- modules/programs/logseq/default.nix
+++ modules/programs/logseq/default.nix
@@ -1,9 +1,61 @@
 {
+  config,
+  lib,
   util,
   pkgs,
   ...
 }:
+let
+  cfg = config.gipphe.programs.logseq;
+  json = pkgs.formats.json { };
+in
 util.mkProgram {
   name = "logseq";
-  hm.home.packages = [ pkgs.logseq ];
+  options.gipphe.programs.logseq = {
+    settings = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+      description = "Global settings for the global config.end file. Written as edn.";
+    };
+    preferences = lib.mkOption {
+      type = json.type;
+      default = { };
+      description = "General preferences";
+    };
+  };
+  hm = {
+    gipphe.programs.logseq = {
+      preferences = {
+        theme = null;
+        themes = {
+          mode = "dark";
+          light = null;
+          dark = null;
+        };
+        externals = [ ];
+      };
+      settings = /* clojure */ ''
+        {:meta/version 1
+         :preferred-workflow :todo
+         :start-of-week 0
+         :feature/enable-block-timestamps? true
+         :file/name-format :triple-lowbar
+         :journal/page-title-format "yyyy-MM-dd EEEE"
+         :editor/logical-outdenting? true
+         :editor/preferred-pasting-file? true}
+      '';
+    };
+
+    home = {
+      packages = [ pkgs.logseq ];
+      file.".logseq/config/config.edn" = {
+        enable = cfg.settings != "";
+        text = cfg.settings;
+      };
+      file.".logseq/preferences.json" = {
+        enable = cfg.preferences != { };
+        source = json.generate "logseq-preferences.json" cfg.preferences;
+      };
+    };
+  };
 }
