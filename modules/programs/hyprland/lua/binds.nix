8d6bb2026f7bd19aea13ea5da82ef710ad17ba2c
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-04 20:39:33 +0200

    fix(hyprland): SUPER + Q does not close window

--- modules/programs/hyprland/lua/binds.nix
+++ modules/programs/hyprland/lua/binds.nix
@@ -20,7 +20,7 @@ let
         if test "$(hyprctl activewindow -j | jq -r '.class')" = 'Steam'; then
           xdotool getactivewindow windowunmap
         else
-          hyprctl dispatch 'hl.dsp.close()'
+          hyprctl dispatch 'hl.dsp.window.close()'
         fi
       '';
     }
