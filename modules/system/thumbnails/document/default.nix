c43b939becac7721f374929dc883e201e681b201
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-07 12:21:30 +0200

    chore: enable f3d for thumbnails

--- modules/system/thumbnails/document/default.nix
+++ modules/system/thumbnails/document/default.nix
@@ -5,12 +5,7 @@ util.mkToggledModule [ "system" "thumbnails" ] {
     environment.systemPackages = builtins.attrValues {
       inherit (pkgs)
         poppler # PDF
-        # TODO: Broken dependency openturns.
-        # See:
-        # - https://github.com/NixOS/nixpkgs/pull/480871
-        # - https://github.com/NixOS/nixpkgs/issues/480860
-        # - https://github.com/NixOS/nixpkgs/issues/481019
-        # f3d # 3D files
+        f3d # 3D files
         ;
     };
   };
