d684a0c64193dd1e1b5b66d4557a8ef4a1490c18
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-12 12:37:18 +0200

    feat(qalculate): float window

--- modules/programs/qalculate/default.nix
+++ modules/programs/qalculate/default.nix
@@ -3,5 +3,13 @@ util.mkProgram {
   name = "qalculate";
   home-manager = {
     home.packages = [ pkgs.qalculate-qt ];
+    gipphe.programs.hyprland.settings.windowRules = [
+      {
+        match.initialClass = "io.github.Qalculate.qalculate-qt";
+        float = true;
+        size = "(monitor_w * 0.5) (monitor_h * 0.5)";
+        center = true;
+      }
+    ];
   };
 }
