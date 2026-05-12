da5abbd9c488512f29ca135eda072bf429bda200
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-12 12:39:08 +0200

    fix(qalculate): invalid option name

--- modules/programs/qalculate/default.nix
+++ modules/programs/qalculate/default.nix
@@ -5,7 +5,7 @@ util.mkProgram {
     home.packages = [ pkgs.qalculate-qt ];
     gipphe.programs.hyprland.settings.windowRules = [
       {
-        match.initialClass = "io.github.Qalculate.qalculate-qt";
+        match.initial_class = "io.github.Qalculate.qalculate-qt";
         float = true;
         size = "(monitor_w * 0.5) (monitor_h * 0.5)";
         center = true;
