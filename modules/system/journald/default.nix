3430750f6f636ad2b8ca16be4dff4cc3d0d63e9d
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-12 12:37:18 +0200

    feat(journald): increase storage limits

--- modules/system/journald/default.nix
+++ modules/system/journald/default.nix
@@ -2,7 +2,7 @@
 util.mkToggledModule [ "system" ] {
   name = "journald";
   system-nixos.services.journald.extraConfig = ''
-    SystemMaxUse=50M
-    RuntimeMaxUse=10M
+    SystemMaxUse=500M
+    RuntimeMaxUse=100M
   '';
 }
