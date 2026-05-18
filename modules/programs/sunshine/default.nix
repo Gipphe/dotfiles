6902a762e8b44d53a419609170803bc738251cae
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-18 13:47:11 +0200

    tweak(sunshine): disable auto start

--- modules/programs/sunshine/default.nix
+++ modules/programs/sunshine/default.nix
@@ -4,7 +4,7 @@ util.mkProgram {
   system-nixos = {
     services.sunshine = {
       enable = true;
-      autoStart = true;
+      autoStart = false;
       capSysAdmin = true;
       openFirewall = true;
     };
