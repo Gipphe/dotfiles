8b9be9c5afdc2ea0472a3cfb474bd7421d0cf49b
Victor Nascimento Bakke <gipphe@gmail.com>
2026-05-27 08:09:29 +0200

    feat(prismlauncher): add extra JDKs

--- modules/gaming/prismlauncher.nix
+++ modules/gaming/prismlauncher.nix
@@ -1,5 +1,16 @@
 { util, pkgs, ... }:
 util.mkGaming {
   name = "prismlauncher";
-  homeManager.home.packages = [ pkgs.prismlauncher ];
+  homeManager.home.packages = [
+    (pkgs.prismlauncher.override {
+      jdks = builtins.attrValues {
+        inherit (pkgs)
+          jdk21
+          jdk17
+          jdk8
+          jdk25
+          ;
+      };
+    })
+  ];
 }
