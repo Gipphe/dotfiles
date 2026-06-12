ee59434be4b8fea4f62f0bebcdc2816f8711aac5
Victor Nascimento Bakke <gipphe@gmail.com>
2026-06-12 08:01:15 +0200

    tweak(sunshine): set applications

--- modules/programs/sunshine/default.nix
+++ modules/programs/sunshine/default.nix
@@ -1,9 +1,13 @@
 {
+  config,
   util,
   inputs,
   pkgs,
   ...
 }:
+let
+  steam = config.programs.steam.package;
+in
 util.mkProgram {
   name = "sunshine";
   nixos = {
@@ -15,6 +19,30 @@ util.mkProgram {
       autoStart = false;
       capSysAdmin = true;
       openFirewall = true;
+      applications = {
+        env = {
+          PATH = "$(PATH):$(HOME)/.local/bin";
+        };
+        apps = [
+          {
+            name = "Desktop";
+            image-path = "desktop.png";
+          }
+          {
+            name = "Steam Big Picture";
+            detached = [
+              "${pkgs.util-linux.bin}/bin/setsid ${steam}/bin/steam steam://open/bigpicture"
+            ];
+            prep-cmd = [
+              {
+                do = "";
+                undo = "${pkgs.util-linux.bin}/bin/setsid ${steam}/bin/steam steam://close/bigpicture";
+              }
+            ];
+            image-path = "steam.png";
+          }
+        ];
+      };
     };
   };
 }
