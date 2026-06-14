49bbf3cb61a883dc3e1e05d9d4ed462b428dbd98
Victor Nascimento Bakke <gipphe@gmail.com>
2026-06-14 20:37:24 +0200

    chore: comment

--- modules/gaming/cache.nix
+++ modules/gaming/cache.nix
@@ -3,6 +3,7 @@ let
   subs = {
     "https://nix-gaming.cachix.org" =
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
+    # Used by `nix-gaming-edge`
     "https://nix-cache.tokidoki.dev/tokidoki" = "tokidoki:MD4VWt3kK8Fmz3jkiGoNRJIW31/QAm7l1Dcgz2Xa4hk=";
   };
 in
