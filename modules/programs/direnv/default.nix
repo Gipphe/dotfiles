8fee1098ae995b354c7c81305c6e5581063ad19e
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-06-29 10:04:48 +0200

    tweak(direnv): disable timeout warning (#29)

--- modules/programs/direnv/default.nix
+++ modules/programs/direnv/default.nix
@@ -5,7 +5,9 @@ util.mkProgram {
   homeManager.programs.direnv = {
     enable = true;
     config = {
+      strict_env = true;
       hide_env_diff = true;
+      warn_timeout = "0s";
     };
     nix-direnv.enable = true;
   };
