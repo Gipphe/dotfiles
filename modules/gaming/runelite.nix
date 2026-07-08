70609a3e8cfbdb01c63196559fb352a0efa5a2d7
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-07-08 13:21:58 +0200

    chore: add disclaimer about runelite window

--- modules/gaming/runelite.nix
+++ modules/gaming/runelite.nix
@@ -2,6 +2,18 @@
 let
   pkg = pkgs.runelite;
 in
+# Runelite has some issues with scaling, but setting the following property
+# in the currently selected profile under
+# ~/.local/share/bold-launcher/.runelite/profiles2 helps:
+#
+# runelite.clientBounds=0\:37\:1920\:1043\:c
+#
+# This ensures that the Runelite client itself stretches and scales to the
+# window, while keeping the game window within the same rendered pixel size.
+# This does however mean that resizing the Runelite client causes the game
+# window to stretch, and also causes mouse clicks to become misaligned, but
+# that is the price to pay for a game window that scales to the monitor
+# resolution.
 util.mkGaming {
   name = "runelite";
   homeManager = {
