607e2066b689249954a1aa6490366ceeed72bfe9
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-07-08 14:12:50 +0200

    fix(minecraft): inconsistencies (#59)
    * fix(minecraft): geyser supports up to version 26.1.2
    
    * fix(minecraft): change seed to prevent spawning in bedrock
    
    * feat(minecraft): add hurricane
    
    * fix(minecraft): geyser config is not writable
    
    * fix(minecraft): wrong path to floodgate cert
    
    * chore(minecraft): document plugins

--- modules/gaming/minecraft/worlds/poketards.nix
+++ modules/gaming/minecraft/worlds/poketards.nix
@@ -20,7 +20,7 @@ util.mkToggledModule [ "gaming" "minecraft" "servers" "worlds" ] {
         enable = true;
         autoStart = false;
         package =
-          inputs.nix-minecraft.legacyPackages.${pkgs.stdenv.hostPlatform.system}.paperServers.paper-26_2;
+          inputs.nix-minecraft.legacyPackages.${pkgs.stdenv.hostPlatform.system}.paperServers.paper-26_1_2;
         jvmOpts = "-Xms4G -Xmx4G -XX:+UseG1GC";
         serverProperties = {
           server-port = javaPort;
@@ -28,7 +28,7 @@ util.mkToggledModule [ "gaming" "minecraft" "servers" "worlds" ] {
           difficulty = "peaceful";
           white-list = false;
           online-mode = true;
-          level-seed = "i.definitely.backed.this.one.up";
+          level-seed = "i.backed.this.one.up";
         };
         operators = {
           Gipphe = {
@@ -37,19 +37,35 @@ util.mkToggledModule [ "gaming" "minecraft" "servers" "worlds" ] {
           };
         };
         symlinks = {
-          # NOTE: Update hashes on first run failure
+          # Allows bedrock players to join Java server
           "plugins/Geyser-Spigot.jar" = pkgs.fetchurl {
             pname = "Geyser-Spigot.jar";
             version = "2026-29-6-1177";
             url = "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot";
             hash = "sha256-UqBOIsSHajV7V6kFiMXl4plrfWfF2Rn6yQkaCSNSq8I=";
           };
+          # Authenticates Bedrock players as Java players using their Bedrock
+          # account (otherwise they'd need a paid Java account)
           "plugins/Floodgate-Spigot.jar" = pkgs.fetchurl {
             pname = "Floodgate-Spigot.jar";
             version = "2026-29-6-138";
             url = "https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot";
             hash = "sha256-RL25COL7T/G5dNUxPQSKYlohVVqYRM+4Ylapjo4ca9E=";
           };
+          # Fixes some inconsistencies between Bedrock and Java in ways that
+          # Geyser developers don't want to include in the base Geyser mod
+          # because of potential exploitation.
+          # At this time of writing, all it does is disable server-side
+          # collision for bamboo and dripstone, since those have offset
+          # collisions between Bedrock and Java.
+          "plugins/hurricane-spigot.jar" = pkgs.fetchurl {
+            pname = "hurricane-spigot.jar";
+            version = "2026-05-30-4";
+            url = "https://download.geysermc.org/v2/projects/hurricane/versions/latest/builds/latest/downloads/spigot";
+            hash = "sha256-VhWJaY+znUxtRpiRmD7bb/u6Q+zJgkSJUNyaVRdqav4=";
+          };
+        };
+        files = {
           "plugins/Geyser-Spigot/config.yml" = pkgs.writeText "config.yml" ''
             bedrock:
               address: 0.0.0.0
@@ -58,7 +74,7 @@ util.mkToggledModule [ "gaming" "minecraft" "servers" "worlds" ] {
               address: auto
               port: ${toString javaPort}
               auth-type: floodgate
-            floodgate-key-file: ../Floodgate-Spigot/key.pem
+            floodgate-key-file: ../floodgate/key.pem
           '';
         };
       };
