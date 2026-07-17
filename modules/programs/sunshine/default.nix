9058d090f2d760639de309924e07224817f109e8
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-07-17 22:41:17 +0200

    chore: remove workaround for sunshine (#80)

--- modules/programs/sunshine/default.nix
+++ modules/programs/sunshine/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   util,
-  inputs,
   pkgs,
   ...
 }:
@@ -13,9 +12,6 @@ util.mkProgram {
   nixos = {
     services.sunshine = {
       enable = true;
-      # TODO: Remove once https://github.com/NixOS/nixpkgs/pull/521906 is in
-      # nixos-unstable.
-      package = inputs.nixpkgs-sunshine.legacyPackages.${pkgs.stdenv.hostPlatform.system}.sunshine;
       autoStart = false;
       capSysAdmin = true;
       openFirewall = true;
