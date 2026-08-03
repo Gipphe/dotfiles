610f643559820e5c75f4432288297db770614437
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-03 15:59:40 +0200

    feat(carapace): add bridges (#131)

--- modules/programs/carapace/default.nix
+++ modules/programs/carapace/default.nix
@@ -1,7 +1,34 @@
-{ util, ... }:
+{
+  lib,
+  config,
+  util,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.programs.carapace;
+in
 util.mkProgram {
   name = "carapace";
   homeManager = {
-    programs.carapace.enable = true;
+    config = {
+      programs.carapace = {
+        enable = true;
+        package = pkgs.symlinkJoin {
+          pname = "${pkgs.carapace.pname}-wrapped";
+          inherit (pkgs.carapace) version;
+          paths = [ pkgs.carapace ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/carapace \
+              --set CARAPACE_BRIDGES fish
+          '';
+          inherit (pkgs.carapace) meta;
+        };
+      };
+      home.activation.clearCarapaceCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        run ${cfg.package}/bin/carapace --clear-cache
+      '';
+    };
   };
 }
