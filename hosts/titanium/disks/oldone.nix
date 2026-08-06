f1a0f9e79e436558326591bc25251c753894f51f
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-06 09:48:16 +0200

    chore: deduplicate keys (#136)

--- hosts/titanium/disks/oldone.nix
+++ hosts/titanium/disks/oldone.nix
@@ -2,19 +2,21 @@
   disko.devices.disk.oldone = {
     type = "disk";
     device = "/dev/disk/by-id/ata-ST2000DX002-2DV164_Z4ZA11VT";
-    content.type = "gpt";
-    content.partitions.main = {
-      name = "oldone";
-      size = "100%";
-      content = {
-        type = "btrfs";
-        extraArgs = [ "-f" ];
-        subvolumes."/main" = {
-          mountpoint = "/mnt/oldone";
-          mountOptions = [
-            "compress=zstd"
-            "noatime"
-          ];
+    content = {
+      type = "gpt";
+      partitions.main = {
+        name = "oldone";
+        size = "100%";
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ];
+          subvolumes."/main" = {
+            mountpoint = "/mnt/oldone";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+            ];
+          };
         };
       };
     };
