ca5d027c67b04513459cfc1ba621884620c82384
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-06 09:22:25 +0200

    chore: deduplicate keys

--- hosts/titanium/disks/slowone.nix
+++ hosts/titanium/disks/slowone.nix
@@ -2,19 +2,21 @@
   disko.devices.disk.slowone = {
     type = "disk";
     device = "/dev/disk/by-id/ata-ST2000DX001-1CM164_Z1E7FPDD";
-    content.type = "gpt";
-    content.partitions.main = {
-      name = "slowone";
-      size = "100%";
-      content = {
-        type = "btrfs";
-        extraArgs = [ "-f" ];
-        subvolumes."/main" = {
-          mountpoint = "/mnt/slowone";
-          mountOptions = [
-            "compress=zstd"
-            "noatime"
-          ];
+    content = {
+      type = "gpt";
+      partitions.main = {
+        name = "slowone";
+        size = "100%";
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ];
+          subvolumes."/main" = {
+            mountpoint = "/mnt/slowone";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+            ];
+          };
         };
       };
     };
