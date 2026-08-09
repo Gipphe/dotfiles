121e3ccdd436d7960c67e6b81fcb11f063bfde17
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-09 18:55:51 +0200

    feat(carapace): add fish and zsh shells for extra completions

--- modules/programs/carapace/default.nix
+++ modules/programs/carapace/default.nix
@@ -12,19 +12,26 @@ util.mkProgram {
   name = "carapace";
   homeManager = {
     config = {
-      programs.carapace = {
-        enable = true;
-        package = pkgs.symlinkJoin {
-          pname = "${pkgs.carapace.pname}-wrapped";
-          inherit (pkgs.carapace) version;
-          paths = [ pkgs.carapace ];
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/carapace \
-              --set CARAPACE_BRIDGES fish
-          '';
-          inherit (pkgs.carapace) meta;
+      programs = {
+        carapace = {
+          enable = true;
+          package = pkgs.symlinkJoin {
+            pname = "${pkgs.carapace.pname}-wrapped";
+            inherit (pkgs.carapace) version;
+            paths = [ pkgs.carapace ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/carapace \
+                --set CARAPACE_BRIDGES fish,zsh
+            '';
+            inherit (pkgs.carapace) meta;
+          };
         };
+        # Keep these for extra completions for programs that don't have native
+        # nushell completions.
+        fish.enable = true;
+        zsh.enable = true;
+        zsh.dotDir = "${config.xdg.configHome}/zsh";
       };
       home.activation.clearCarapaceCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         run ${cfg.package}/bin/carapace --clear-cache
--- modules/programs/zellij/default.nix
+++ modules/programs/zellij/default.nix
@@ -3,7 +3,7 @@ util.mkProgram {
   name = "zellij";
   homeManager = {
     imports = [ ./config.nix ];
-    config.programs = {
+    programs = {
       zellij.enable = true;
       nushell.shellAliases = {
         zq = "zellij kill-session $env.ZELLIJ_SESSION_NAME";
