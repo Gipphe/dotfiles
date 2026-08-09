ab53fde7786bbf99491f9523e805066f83b56295
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-09 20:23:18 +0200

    tweak(librewolf): add settings

---
+++ modules/programs/librewolf/settings.nix
@@ -0,0 +1,47 @@
+{
+  "browser.contentblocking.category" = "strict";
+  "browser.download.viewableInternally.typeWasRegistered.avif" = true;
+  "browser.download.viewableInternally.typeWasRegistered.webp" = true;
+  "browser.eme.ui.firstContentShown" = true;
+  "browser.policies.applied" = true;
+  "browser.policies.runOncePerModification.removeSearchEngines" =
+    "[\"Google\",\"Bing\",\"Amazon.com\",\"eBay\",\"Twitter\",\"Perplexity\"]";
+  "browser.policies.runOncePerModification.setDefaultSearchEngine" = "DuckDuckGo";
+  "browser.region.network.url" = "";
+  "browser.region.update.enabled" = false;
+  "browser.safebrowsing.downloads.remote.block_potentially_unwanted" = false;
+  "browser.safebrowsing.downloads.remote.block_uncommon" = false;
+  "browser.safebrowsing.downloads.remote.enabled" = false;
+  "browser.safebrowsing.downloads.remote.url" = "";
+  "browser.safebrowsing.provider.google4.dataSharingURL" = "";
+  "browser.startup.couldRestoreSession.count" = 2;
+  "browser.startup.page" = 3;
+  "browser.toolbars.bookmarks.visibility" = "never";
+  "browser.translations.neverTranslateLanguages" = "nb";
+  "browser.uiCustomization.state" =
+    "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"_testpilot-containers-browser-action\",\"_3c6bf0cc-3ae2-42fb-9993-0d33104fdcaf_-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\",\"dearrow_ajay_app-browser-action\"],\"nav-bar\":[\"sidebar-button\",\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"vertical-spacer\",\"urlbar-container\",\"customizableui-special-spring2\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"ublock0_raymondhill_net-browser-action\",\"_d634138d-c276-4fc8-924b-40a0ea21d284_-browser-action\",\"alltabs-button\",\"reset-pbm-toolbar-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[],\"vertical-tabs\":[\"tabbrowser-tabs\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"screenshot-button\",\"_testpilot-containers-browser-action\",\"_3c6bf0cc-3ae2-42fb-9993-0d33104fdcaf_-browser-action\",\"sponsorblocker_ajay_app-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\",\"dearrow_ajay_app-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_d634138d-c276-4fc8-924b-40a0ea21d284_-browser-action\",\"reset-pbm-toolbar-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"unified-extensions-area\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":24,\"newElementCount\":2}";
+  "browser.urlbar.placeholderName" = "DuckDuckGo";
+  "dom.forms.autocomplete.formautofill" = true;
+  "dom.security.https_only_mode_ever_enabled" = true;
+  "extensions.pictureinpicture.enable_picture_in_picture_overrides" = true;
+  "network.captive-portal-service.enabled" = false;
+  "network.connectivity-service.enabled" = false;
+  "network.http.http3.enable_0rtt" = false;
+  "network.http.referer.disallowCrossSiteRelaxingDefault.top_navigation" = true;
+  "pref.privacy.disable_button.tracking_protection_exceptions" = false;
+  "privacy.annotate_channels.strict_list.enabled" = true;
+  "privacy.bounceTrackingProtection.mode" = 1;
+  "privacy.fingerprintingProtection" = true;
+  "privacy.history.custom" = true;
+  "privacy.query_stripping.enabled" = true;
+  "privacy.query_stripping.enabled.pbmode" = true;
+  "privacy.trackingprotection.allow_list.baseline.enabled" = false;
+  "privacy.trackingprotection.consentmanager.skip.pbmode.enabled" = false;
+  "privacy.trackingprotection.emailtracking.enabled" = true;
+  "privacy.trackingprotection.enabled" = true;
+  "privacy.trackingprotection.socialtracking.enabled" = true;
+  "security.tls.enable_0rtt_data" = false;
+  "services.sync.engine.addresses.available" = true;
+  "sidebar.revamp" = true;
+  "sidebar.verticalTabs" = true;
+}
