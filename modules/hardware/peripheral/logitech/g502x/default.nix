9401cf2cfa8438edc737a412967011a8632cc9a4
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-08-19 21:44:08 +0200

    feat: add g502x

---
+++ modules/hardware/peripheral/logitech/g502x/default.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  util,
+  lib,
+  pkgs,
+  ...
+}:
+util.mkToggledModule [ "hardware" "peripheral" "logitech" ] {
+  name = "g502x";
+  options.gipphe.hardware.peripheral.logitech.g502x = {
+    id = lib.mkOption {
+      type = lib.types.str;
+      description = "ID of the device as reported by lsusb";
+      example = "046d:c539";
+    };
+  };
+  homeManager = {
+    home.packages = [ pkgs.piper ];
+  };
+  nixos = {
+    programs.solaar.enable = true;
+    services = {
+      ratbagd.enable = true;
+      libinput.enable = true;
+      hardware.openrgb.enable = true;
+    };
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "reset-mouse-kernel-module" ''
+        echo "Rebinding the logitech-djreceiver driver..." >&2
+        echo "0003:046D:C539.0003" | sudo ${pkgs.coreutils}/bin/tee /sys/bus/hid/drivers/logitech-djreceiver/unbind
+        sleep 0.5
+        echo "0003:046D:C539.0003" | sudo ${pkgs.coreutils}/bin/tee /sys/bus/hid/drivers/logitech-djreceiver/bind
+        echo "Rebound. Hoping the scrollwheel works now!" >&2
+      '')
+    ];
+    security.sudo.extraRules = [
+      {
+        users = [ config.gipphe.username ];
+        commands = [
+          {
+            command = "${pkgs.coreutils}/bin/tee /sys/bus/hid/drivers/logitech-djreceiver/unbind";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.coreutils}/bin/tee /sys/bus/hid/drivers/logitech-djreceiver/bind";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
+}
