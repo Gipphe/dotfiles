34cdc2cda78e7da87f15113db2c1e27b5e26ddef
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-09-18 00:39:31 +0200

    flake: fix incorrect packages

--- flake/packages/default.nix
+++ flake/packages/default.nix
@@ -3,15 +3,15 @@
   perSystem = { pkgs, ... }: {
     packages =
       let
-        util = pkgs.callPackage ./util.nix { };
+        util = pkgs.callPackage ../../util.nix { };
       in
       {
-        md-fastfetch = pkgs.callPackage ./packages/md-fastfetch.nix {
+        md-fastfetch = pkgs.callPackage ./md-fastfetch.nix {
           inherit (util) writeNushellApplication;
         };
-        md-icons = pkgs.callPackage ./packages/md-icons.nix { inherit (util) writeNushellApplication; };
-        mo2installer = pkgs.callPackage ./packages/mo2installer.nix { };
-        fluorine-manager = pkgs.callPackage ./packages/fluorine-manager.nix { };
+        md-icons = pkgs.callPackage ./md-icons.nix { inherit (util) writeNushellApplication; };
+        mo2installer = pkgs.callPackage ./mo2installer.nix { };
+        fluorine-manager = pkgs.callPackage ./fluorine-manager.nix { };
       }
       // (
         let
