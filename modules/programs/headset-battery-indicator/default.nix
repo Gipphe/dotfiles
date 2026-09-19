1ba135a1e76622cfa338ab961db08c22d8b70137
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-09-19 21:01:45 +0200

    a50gen5: enable headset-battery-indicator

--- modules/programs/headset-battery-indicator/default.nix
+++ modules/programs/headset-battery-indicator/default.nix
@@ -1,4 +1,15 @@
-{ util, ... }:
+{
+  self,
+  util,
+  pkgs,
+  ...
+}:
 util.mkProgram {
   name = "headset-battery-indicator";
+  homeManager.home.packages = [
+    self.packages.${pkgs.stdenv.hostPlatform.system}.headset-battery-indicator
+  ];
+  nixos = {
+    environment.systemPackages = [ pkgs.headsetcontrol ];
+  };
 }
