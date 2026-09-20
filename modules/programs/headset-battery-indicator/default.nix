e8fc495c72a248593f310e5118c86065f8e6f67a
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-09-21 00:38:36 +0200

    headset-battery-indicator: run on hyprland start

--- modules/programs/headset-battery-indicator/default.nix
+++ modules/programs/headset-battery-indicator/default.nix
@@ -1,14 +1,19 @@
 {
   self,
+  lib,
   util,
   pkgs,
   ...
 }:
+let
+  pkg = self.packages.${pkgs.stdenv.hostPlatform.system}.headset-battery-indicator;
+in
 util.mkProgram {
   name = "headset-battery-indicator";
-  homeManager.home.packages = [
-    self.packages.${pkgs.stdenv.hostPlatform.system}.headset-battery-indicator
-  ];
+  homeManager = {
+    home.packages = [ pkg ];
+    gipphe.core.wm.triggers.on-startup.headset-battery-indicator.command = lib.getExe pkg;
+  };
   nixos = {
     environment.systemPackages = [ pkgs.headsetcontrol ];
   };
